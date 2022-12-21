051d7a425efb1656ef4a773eb249f690535a3e40
Naomi Plasterer <naomi@xmtp.com>
2022-12-21 14:05:19 -0800

    Setup Android Project with XMTP proto import (#3)
    * initial commit
    
    * get the xmtp protos imported into the library
    
    * add the library module for the future sdk
    
    * rename to message library
    
    * add github properties
    
    * dont commit this
    
    * Update build.gradle
    
    Co-authored-by: Elise Alix <elise@xmtp.com>
    
    * setup the yml file to run the gradle tests
    
    * update the icon
    
    * add some simple theming
    
    Co-authored-by: Elise Alix <elise@xmtp.com>

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 XMTP client SDK for Android applications written in Kotlin.
 
 **Coming soon!** This is a work in progress and should not be used yet. Follow along in the [tracking issue](https://github.com/xmtp/xmtp-android/issues/1) for updates on progress.
+
+### Setup
+
+```bash
+cp github.properties.example github.properties
+```
+
+Fill in with your GitHub credentials. Token must have package read access.
---
+++ example/src/androidTest/java/org/xmtp/android/example/MainActivityInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package org.xmtp.android.example
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("org.xmtp.android.example", appContext.packageName)
+    }
+}
