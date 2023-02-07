f10e62ba6bb36fa9f9868db604b036447bed5174
Michael Xu <michaelx@xmtp.com>
2023-02-07 12:03:31 -0800

    return a string as error

--- crates/xmtp-keystore/src/lib.rs
+++ crates/xmtp-keystore/src/lib.rs
@@ -39,12 +39,14 @@ impl Keystore {
      *   )
      * }
      */
-    fn hkdf(secret: &[u8], salt: &[u8]) -> Result<[u8; 32], ()> {
+    fn hkdf(secret: &[u8], salt: &[u8]) -> Result<[u8; 32], String> {
         let hk = Hkdf::<Sha256>::new(Some(&salt[..]), &secret);
         let mut okm = [0u8; 42];
-        hk.expand(&[], &mut okm)
-            .expect("42 is a valid length for Sha256 to output");
-        Ok(okm[0..32].try_into().expect("slice with incorrect length"))
+        let res = hk.expand(&[], &mut okm);
+        if res.is_err() {
+            return Err(res.err().unwrap().to_string());
+        }
+        okm[0..32].try_into().map_err(|_| "hkdf failed to fit in 32 bytes".to_string())
     }
 
     // Set private identity key from protobuf bytes
