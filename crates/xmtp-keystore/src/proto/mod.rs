20c38b4e572398c50c47704c275b7b0782b21e23
Michael Xu <michaelx@xmtp.com>
2023-03-07 15:40:17 -0800

    feat: update protobuf with new keystore V2 convo changes

--- crates/xmtp-keystore/src/proto/mod.rs
+++ crates/xmtp-keystore/src/proto/mod.rs
@@ -5,9 +5,8 @@ pub mod composite;
 pub mod contact;
 pub mod content;
 pub mod invitation;
+pub mod keystore;
 pub mod message;
 pub mod private_key;
 pub mod public_key;
 pub mod signature;
-// Added manually
-pub mod keystore;
