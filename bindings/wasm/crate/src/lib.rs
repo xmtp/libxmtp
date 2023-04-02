ed40c79dabb40f1153ba2e2ae341af8f639f3ef9
Michael Xu <michaeljxu11@gmail.com>
2023-04-02 14:43:48 -0700

    fix: change up xmtpv3 api to match

--- bindings/wasm/crate/src/lib.rs
+++ bindings/wasm/crate/src/lib.rs
@@ -4,7 +4,7 @@ use std::sync::Mutex;
 
 use wasm_bindgen::prelude::*;
 
-use xmtpv3::VoodooInstance;
+use xmtpv3::manager::VoodooInstance;
 
 #[macro_use]
 extern crate lazy_static;
@@ -36,14 +36,16 @@ pub fn create_outbound_session(
         .get(sending_handle)
         .ok_or("sending_handle not found")?
         .take();
-    let mut receiving_instance = instances
+    let receiving_instance = instances
         .get(receiving_handle)
         .ok_or("receiving_handle not found")?
         .take();
 
+    // Get other party's public situation
+    let receiving_public = receiving_instance.public_account();
+
     // Create the session
-    let result = sending_instance
-        .create_outbound_session_serialized(&mut receiving_instance.account, message);
+    let result = sending_instance.create_outbound_session_serialized(&receiving_public, message);
 
     // Put the sending_instance and receiving_instance back, we know the cells exist
     instances.get(sending_handle).unwrap().set(sending_instance);
@@ -65,7 +67,7 @@ pub fn create_inbound_session(
 ) -> Result<String, JsValue> {
     // Look up both handles in INSTANCE_MAP
     let instances = INSTANCE_MAP.lock().unwrap();
-    let mut sending_instance = instances
+    let sending_instance = instances
         .get(sending_handle)
         .ok_or("sending_handle not found")?
         .take();
@@ -74,9 +76,11 @@ pub fn create_inbound_session(
         .ok_or("receiving_handle not found")?
         .take();
 
+    // Get sender party public
+    let sending_public = sending_instance.public_account();
+
     // Create the session
-    let result = receiving_instance
-        .create_inbound_session_serialized(&mut sending_instance.account, message);
+    let result = receiving_instance.create_inbound_session_serialized(&sending_public, message);
 
     // Put the instances back in their cells
     instances.get(sending_handle).unwrap().set(sending_instance);
@@ -93,7 +97,7 @@ pub fn create_inbound_session(
 
 #[wasm_bindgen]
 pub fn e2e_selftest() -> Result<bool, JsValue> {
-    xmtpv3::e2e_selftest()
+    xmtpv3::manager::e2e_selftest()
         .map(|x| x == "Self test successful")
         .map_err(|e| JsValue::from_str(&e.to_string()))
 }
