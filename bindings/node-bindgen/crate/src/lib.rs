78c247f6636b72273a692922ef1832ec7769825a
Michael Xu <michaeljxu11@gmail.com>
2023-04-03 08:30:39 -0700

    test: fix node-bindgen e2e test

--- bindings/node-bindgen/crate/src/lib.rs
+++ bindings/node-bindgen/crate/src/lib.rs
@@ -1,7 +1,9 @@
 use node_bindgen::derive::node_bindgen;
-use xmtpv3;
+
 
 #[node_bindgen]
 fn test() -> String {
-    xmtpv3::e2e_selftest().map_err(|x| x.to_string()).unwrap()
+    xmtpv3::manager::e2e_selftest()
+        .map_err(|x| x.to_string())
+        .unwrap()
 }
