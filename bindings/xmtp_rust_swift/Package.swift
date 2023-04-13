185a52f1cbe4a9d998dff5119004daa6eb049d35
Michael Xu <michaelx@xmtp.com>
2023-04-13 12:51:00 -0700

    feat: add a new binding for iOS -> Swift package (#48)
    * feat: initial PR for crate that builds off libxmtp
    
    * feat: attempt to package cryptography logic in
    
    * feat: added functionality to push to deriveddata, and README.md
    
    * feat: remove example functions and leave encryption selftest

---
+++ bindings/xmtp_rust_swift/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+import Foundation
+let package = Package(
+        name: "XMTPRustSwift",
+        platforms: [
+            .iOS(.v13), 
+            .macOS(.v11)
+        ],
+        products: [
+            .library(
+                name: "XMTPRustSwift",
+                targets: ["XMTPRustSwift"]),
+        ],
+        targets: [
+            .binaryTarget(
+                name: "XMTPRustSwift",
+                path: "XMTPRustSwift.xcframework"
+        ]
+)
