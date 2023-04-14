a32d95b10f68e0e71a17d998f4bca7faf9cec557
Michael Xu <michaeljxu11@gmail.com>
2023-04-13 18:30:19 -0700

    fix: actually got it right

--- crates/xmtp-networking/src/proto_helper.rs
+++ crates/xmtp-networking/src/proto_helper.rs
@@ -1,17 +1,15 @@
-mod xmtp {
-    mod keystore_api {
-        mod v1 {
+pub mod xmtp {
+    pub mod keystore_api {
+        pub mod v1 {
             include!("xmtp.keystore_api.v1.rs");
         }
     }
-    mod message_api {
-        mod v1 {
+    pub mod message_api {
+        pub mod v1 {
             include!("xmtp.message_api.v1.rs");
         }
     }
-    mod message_contents {
-        mod v1 {
-            include!("xmtp.message_contents.rs");
-        }
+    pub mod message_contents {
+        include!("xmtp.message_contents.rs");
     }
 }
