3c1c6eb1f985179f92ed9218baa377bd7b32cbca
Nicholas Molnar <nicholas@xmtp.com>
2023-04-15 23:28:32 -0700

    Automatically install required dependency

--- crates/xmtp-proto/build.rs
+++ crates/xmtp-proto/build.rs
@@ -1,7 +1,32 @@
 use std::error::Error;
-use std::process::{exit, Command};
+use std::process::{exit, Command, Stdio};
+
+fn ensure_crate(crate_name: &str) {
+    let check_crate = Command::new("cargo")
+        .arg("install")
+        .arg("--list")
+        .output()
+        .expect("Failed to list installed crates");
+
+    let installed_crates = String::from_utf8_lossy(&check_crate.stdout);
+    if !installed_crates.contains(crate_name) {
+        let mut install_crate = Command::new("cargo")
+            .arg("install")
+            .arg(crate_name)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()
+            .expect("Failed to install crate");
+
+        install_crate
+            .wait()
+            .expect("Failed to wait on crate installation process");
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    ensure_crate("protoc-gen-prost-crate");
+
     let status = Command::new("buf")
         .arg("generate")
         .arg("buf.build/xmtp/proto")
