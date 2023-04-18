c71541a136f8d0d92923deafc4177625ee4643bb
Richard Hua <rich@rkhua.com>
2023-04-18 17:17:54 +1000

    refactor: remove unused code from wasm binding

--- bindings/wasm/crate/src/lib.rs
+++ bindings/wasm/crate/src/lib.rs
@@ -1,33 +1,6 @@
-use once_cell::sync::Lazy;
-use std::sync::Mutex;
-
 use wasm_bindgen::prelude::*;
-use xmtp_keystore::Keystore;
-
-// Keep the keystore class in memory
-static KEYSTORE: Lazy<Mutex<Keystore>> = Lazy::new(|| Mutex::new(Keystore::new()));
 
 #[wasm_bindgen]
 pub fn add_two_numbers(left: usize, right: usize) -> usize {
     libxmtp_core::add(left, right)
-}
-
-#[wasm_bindgen]
-pub fn set_private_key_bundle(key_bytes: &[u8]) -> Result<bool, JsValue> {
-    KEYSTORE
-        .lock()
-        .unwrap()
-        .set_private_key_bundle(key_bytes)
-        .map_err(|e| JsValue::from_str(&e.to_string()));
-    Ok(true)
-}
-
-#[wasm_bindgen]
-pub fn save_invitation(invite_bytes: &[u8]) -> Result<bool, JsValue> {
-    KEYSTORE
-        .lock()
-        .unwrap()
-        .save_invitation(invite_bytes)
-        .map_err(|e| JsValue::from_str(&e.to_string()));
-    Ok(true)
 }
