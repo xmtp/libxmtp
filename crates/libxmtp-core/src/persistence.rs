9e282e8d20e0dbb7b954db973b22de189c69e4f2
Richard Hua <rich@rkhua.com>
2023-04-20 18:02:11 +1000

    refactor: address lint

--- crates/libxmtp-core/src/persistence.rs
+++ crates/libxmtp-core/src/persistence.rs
@@ -16,6 +16,12 @@ impl InMemoryPersistence {
     }
 }
 
+impl Default for InMemoryPersistence {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Persistence for InMemoryPersistence {
     fn write(&mut self, key: String, value: &[u8]) -> Result<(), String> {
         self.data.insert(key, value.to_vec());
