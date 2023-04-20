67a476a2588054bee8c3403e69f00a14e0263d1a
Michael Xu <michaelx@xmtp.com>
2023-04-20 14:00:26 -0700

    feat: added hashing and k256 utilities for iOS to consume (#65)
    * feat: added hashing and k256 utilities for iOS to consume
    
    * fix: remove unnecessary sha256 public function

---
+++ crates/corecrypto/src/hashes.rs
@@ -0,0 +1,11 @@
+use sha2::Digest;
+use sha3::Keccak256;
+
+pub fn keccak256(data: &[u8]) -> [u8; 32] {
+    let mut hasher = Keccak256::new();
+    hasher.update(data);
+    let result = hasher.finalize();
+    let mut digest = [0u8; 32];
+    digest.copy_from_slice(&result);
+    digest
+}
