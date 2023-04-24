2609736a8dc1d3552724cb0065c10f30cfca7a3e
Michael Xu <michaelx@xmtp.com>
2023-04-24 14:53:38 -0700

    feat: use xmtpv3 as the branch

--- crates/xmtp-proto/build.rs
+++ crates/xmtp-proto/build.rs
@@ -29,7 +29,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let status = Command::new("buf")
         .arg("generate")
-        .arg("https://github.com/xmtp/proto.git#branch=michaelx-rust_v3_protos,subdir=proto")
+        .arg("https://github.com/xmtp/proto.git#branch=xmtpv3,subdir=proto")
         .current_dir(env!("CARGO_MANIFEST_DIR"))
         .status()
         .unwrap();
