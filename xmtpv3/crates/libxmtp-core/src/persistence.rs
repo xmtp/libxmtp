f8b99e1cf3f3ed8457136ae4fffad1ca0c64c42c
Richard Hua <rich@rkhua.com>
2023-04-24 19:51:26 +1000

    feat: round-trip in-memory persistence through JS

--- xmtpv3/crates/libxmtp-core/src/persistence.rs
+++ xmtpv3/crates/libxmtp-core/src/persistence.rs
@@ -1,4 +1,7 @@
-pub trait Persistence {
+// WARNING: Send and Sync are unsafe traits. We add them here to make the compiler happy,
+// knowing that the entry-point to this library should be single-threaded. This code will
+// FAIL in unexpected ways in a multi-threaded environment.
+pub trait Persistence: Send + Sync {
     fn write(&mut self, key: String, value: &[u8]) -> Result<(), String>;
     fn read(&self, key: String) -> Result<Option<Vec<u8>>, String>;
 }
