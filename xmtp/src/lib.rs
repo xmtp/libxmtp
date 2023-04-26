75abcb37176dbfe61b7f16e1767d43fe070adcf6
Michael Xu <michaelx@xmtp.com>
2023-04-26 10:25:21 -0700

    feat: contact bundle proto populated from vodozemac account

--- xmtp/src/lib.rs
+++ xmtp/src/lib.rs
@@ -1,9 +1,14 @@
 pub mod client;
 pub mod persistence;
+pub mod vmac;
 
 #[cfg(test)]
 mod tests {
-    use crate::{client::Client, persistence::InMemoryPersistence};
+    use crate::{
+        client::Client,
+        persistence::InMemoryPersistence,
+        vmac::{generate_outbound_session, generate_test_contact_bundle},
+    };
 
     #[test]
     fn can_pass_persistence_methods() {
@@ -19,5 +24,16 @@ mod tests {
             client.read_from_persistence("foo".to_string()).unwrap(),
             Some(b"bar".to_vec())
         );
+    }
+
+    #[test]
+    fn test_can_generate_test_contact_bundle_and_session() {
+        let bundle = generate_test_contact_bundle();
+        assert!(bundle.identity_key.is_some());
+        assert!(bundle.prekey.is_some());
+
+        // Generate an outbound session (Olm Prekey Message) given a VmacContactBundle
+        let session = generate_outbound_session(bundle);
+        assert!(!session.is_empty());
     }
 }
