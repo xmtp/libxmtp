5725c7cdf683a11d739eb24c8d1fb0b01e618637
Nicholas Molnar <nicholas@xmtp.com>
2023-04-26 13:24:01 -0700

    Use builder interface
    Does not work

--- xmtp/src/builder.rs
+++ xmtp/src/builder.rs
@@ -1,4 +1,7 @@
+use std::borrow::Borrow;
+
 use crate::{
+    account::VmacAccount,
     client::{Client, Network},
     persistence::Persistence,
 };
@@ -10,6 +13,7 @@ where
 {
     network: Network,
     persistence: Option<P>,
+    account: Option<VmacAccount>,
 }
 
 impl<P> ClientBuilder<P>
@@ -20,6 +24,7 @@ where
         Self {
             network: Network::Dev,
             persistence: None,
+            account: None,
         }
     }
 
@@ -33,12 +38,46 @@ where
         self
     }
 
+    pub fn account(mut self, wallet_address: String) -> Result<Self, String> {
+        let key = get_account_storage_key(wallet_address);
+        let persistence = self.persistence.as_mut().ok_or_else(|| {
+            "Persistence engine must be set before setting the account".to_string()
+        })?;
+
+        let existing = persistence.read(key.clone());
+        match existing {
+            Ok(Some(data)) => {
+                println!("Found data in key {}", key.clone());
+                let data_string = std::str::from_utf8(&data).map_err(|e| format!("{}", e))?;
+                let account: VmacAccount =
+                    serde_json::from_str(data_string).map_err(|e| format!("{}", e))?;
+                self.account = Some(account)
+            }
+            Ok(None) => {
+                let account = VmacAccount::generate();
+                let data = serde_json::to_string(&account).map_err(|e| format!("{}", e))?;
+
+                persistence.write(key.clone(), data.as_bytes())?;
+
+                self.account = Some(account)
+            }
+            Err(e) => return Err(format!("Failed to read from persistence: {}", e)),
+        }
+
+        Ok(self)
+    }
+
     pub fn build(self) -> Client<P> {
         Client {
             network: self.network,
             persistence: self.persistence.expect("A persistence engine must be set"),
+            account: self.account.expect("An account must be set"),
         }
     }
+}
+
+pub fn get_account_storage_key(wallet_address: String) -> String {
+    format!("account_{}", wallet_address)
 }
 
 #[cfg(test)]
@@ -50,13 +89,45 @@ mod tests {
 
     impl ClientBuilder<InMemoryPersistence> {
         pub fn new_test() -> Self {
-            Self::new().persistence(InMemoryPersistence::new())
+            Self::new()
+                .persistence(InMemoryPersistence::new())
+                .account("unknown".to_string())
+                .unwrap()
         }
     }
 
     #[test]
     fn builder_test() {
-        ClientBuilder::new_test().build();
+        let client = ClientBuilder::new_test().build();
+        assert!(!client
+            .account
+            .account
+            .identity_keys()
+            .curve25519
+            .to_bytes()
+            .is_empty())
+    }
+
+    #[test]
+    fn persistence_test() {
+        let persistence = InMemoryPersistence::new();
+        let client_a = ClientBuilder::new()
+            .persistence(persistence)
+            .account("foo".to_string())
+            .unwrap()
+            .build();
+
+        let client_b = ClientBuilder::new()
+            .persistence(persistence)
+            .account("foo".to_string())
+            .unwrap()
+            .build();
+
+        // Ensure the persistence was used to store the generated keys
+        assert_eq!(
+            client_a.account.account.curve25519_key().to_bytes(),
+            client_b.account.account.curve25519_key().to_bytes()
+        )
     }
 
     #[test]
--- xmtp/src/client.rs
+++ xmtp/src/client.rs
@@ -1,6 +1,5 @@
 use crate::{account::VmacAccount, persistence::Persistence};
 use serde_json;
-use vodozemac::olm::Account;
 
 #[derive(Clone, Copy)]
 pub enum Network {
@@ -21,6 +20,7 @@ where
 {
     pub network: Network,
     pub persistence: P,
+    pub account: VmacAccount,
 }
 
 impl<P: Persistence> Client<P> {
@@ -28,35 +28,7 @@ impl<P: Persistence> Client<P> {
         self.persistence.write(s, b)
     }
 
-    pub fn get_or_create_account(
-        mut persistence: P,
-        wallet_address: String,
-    ) -> Result<VmacAccount, String> {
-        let key = get_account_storage_key(wallet_address);
-        let existing = persistence.read(key.clone());
-        match existing {
-            Ok(Some(data)) => {
-                let data_string = std::str::from_utf8(&data).map_err(|e| format!("{}", e))?;
-                let account: VmacAccount =
-                    serde_json::from_str(data_string).map_err(|e| format!("{}", e))?;
-                Ok(account)
-            }
-            Ok(None) => {
-                let account = VmacAccount::generate();
-                let data = serde_json::to_string(&account).map_err(|e| format!("{}", e))?;
-                persistence.write(key, data.as_bytes())?;
-
-                Ok(account)
-            }
-            Err(e) => Err(format!("Failed to read from persistence: {}", e)),
-        }
-    }
-
     pub fn read_from_persistence(&self, s: String) -> Result<Option<Vec<u8>>, String> {
         self.persistence.read(s)
     }
-}
-
-pub fn get_account_storage_key(wallet_address: String) -> String {
-    format!("account_{}", wallet_address)
 }
