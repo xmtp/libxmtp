54afd418da8f9e14b73a9c2faf0ed3daf4a1e24e
Richard Hua <rich@rkhua.com>
2023-05-01 15:39:55 +1000

    refactor: move local storage persistence to separate file

--- bindings_wasm/src/lib.rs
+++ bindings_wasm/src/lib.rs
@@ -1,57 +1,11 @@
-use std::sync::Mutex;
+mod local_storage_persistence;
 
+use local_storage_persistence::LocalStoragePersistence;
+use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
-use web_sys;
-use xmtp::persistence::Persistence;
 use xmtp::{Client, ClientBuilder};
 
 static CLIENT_LIST: Mutex<Vec<Client<LocalStoragePersistence>>> = Mutex::new(Vec::new());
-
-pub struct LocalStoragePersistence {}
-
-impl LocalStoragePersistence {
-    pub fn new() -> Self {
-        LocalStoragePersistence {}
-    }
-
-    fn storage(&self) -> web_sys::Storage {
-        web_sys::window()
-            .expect("Global Window not found - are you running in a browser?")
-            .local_storage()
-            .expect("Local Storage not found - are you running in a browser?")
-            .expect("Window.localStorage not found - are you running in a browser?")
-    }
-}
-
-impl Default for LocalStoragePersistence {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl Persistence for LocalStoragePersistence {
-    fn write(&mut self, key: String, value: &[u8]) -> Result<(), String> {
-        let value = String::from_utf8(value.to_vec()).unwrap();
-        let key = format!("xmtp_{}", key);
-        self.storage()
-            .set_item(&key, &value)
-            .expect("Failed to write to local storage");
-        Ok(())
-    }
-
-    fn read(&self, key: String) -> Result<Option<Vec<u8>>, String> {
-        let key = format!("xmtp_{}", key);
-        let value = self
-            .storage()
-            .get_item(&key)
-            .expect("Failed to read from local storage");
-        if value.is_none() {
-            return Ok(None);
-        }
-        let value = value.unwrap();
-        Ok(Some(value.as_bytes().to_vec()))
-    }
-}
 
 #[wasm_bindgen]
 pub fn client_create() -> usize {
