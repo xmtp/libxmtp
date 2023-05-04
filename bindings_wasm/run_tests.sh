9e3be183dfa7c20e0fbb8a643b46280615c5a69b
Richard Hua <rich@rkhua.com>
2023-05-04 16:42:42 +1000

    doc: add instructions for fixing browser tests

--- bindings_wasm/run_tests.sh
+++ bindings_wasm/run_tests.sh
@@ -1,1 +1,5 @@
 wasm-pack test --chrome --headless
+
+# If you get the error "invalid session id", make sure your Chrome has been updated to the latest
+# https://github.com/rustwasm/wasm-bindgen/issues/2151
+
