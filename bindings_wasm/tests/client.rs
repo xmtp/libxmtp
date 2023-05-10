60a00a2fd00ee483807da90df2838d1160687431
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2023-05-10 13:06:32 -0700

    update tests

--- bindings_wasm/tests/client.rs
+++ bindings_wasm/tests/client.rs
@@ -1,4 +1,6 @@
-use bindings_wasm::{client_create, client_read_from_persistence, client_write_to_persistence};
+use bindings_wasm::{
+    client_create, client_read_from_persistence, client_write_to_persistence, register,
+};
 use wasm_bindgen::JsError;
 use wasm_bindgen_test::*;
 
@@ -11,10 +13,16 @@ fn unwrap_js_error<T>(result: Result<T, JsError>) -> T {
     }
 }
 
+fn create_js_signer() -> js_sys::Function {
+    js_sys::Function::new_with_args("str", " return 'SGVsbG8gV29ybGQh'") // Returns base64 string
+}
+
 wasm_bindgen_test_configure!(run_in_browser);
 #[wasm_bindgen_test]
 fn can_pass_persistence_methods() {
-    let client_id = unwrap_js_error(client_create());
+    // TODO: Update test with valid signature
+    let account_id = unwrap_js_error(register(create_js_signer()));
+    let client_id = unwrap_js_error(client_create(account_id));
     assert_eq!(
         unwrap_js_error(client_read_from_persistence(client_id, "foo")),
         None
