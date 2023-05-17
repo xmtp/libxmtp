a81a60324114db086057d9828b425839ff038759
Richard Hua <rich@rkhua.com>
2023-05-17 13:29:26 +1000

    It is building now. Still need to plumb the apiclient through

--- bindings_ffi/build.rs
+++ bindings_ffi/build.rs
@@ -1,20 +1,20 @@
 // TODO: took from swift-bridge example, but not sure if this is needed
 // https://chinedufn.github.io/swift-bridge/building/xcode-and-cargo/index.html
-const XCODE_CONFIGURATION_ENV: &str = "CONFIGURATION";
+// const XCODE_CONFIGURATION_ENV: &str = "CONFIGURATION";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // == swift-bridge generation code ==
 
-    let out_dir = "Generated";
+    // let out_dir = "Generated";
 
-    let bridges = vec!["src/lib.rs"];
-    for path in &bridges {
-        println!("cargo:rerun-if-changed={}", path);
-    }
-    println!("cargo:rerun-if-env-changed={}", XCODE_CONFIGURATION_ENV);
+    // let bridges = vec!["src/lib.rs"];
+    // for path in &bridges {
+    //     println!("cargo:rerun-if-changed={}", path);
+    // }
+    // println!("cargo:rerun-if-env-changed={}", XCODE_CONFIGURATION_ENV);
 
-    swift_bridge_build::parse_bridges(bridges)
-        .write_all_concatenated(out_dir, env!("CARGO_PKG_NAME"));
+    // swift_bridge_build::parse_bridges(bridges)
+    //     .write_all_concatenated(out_dir, env!("CARGO_PKG_NAME"));
     // == end swift-bridge generation code ==
     Ok(())
 }
