469b14833d7631cf918731496534d2a39028a06e
Nicholas Molnar <nicholas@xmtp.com>
2023-05-24 22:07:47 -0700

    Add contact

---
+++ xmtp/src/contact.rs
@@ -0,0 +1,59 @@
+use prost::{DecodeError, EncodeError, Message};
+use thiserror::Error;
+
+use vodozemac::Curve25519PublicKey;
+use xmtp_cryptography::hash::keccak256;
+use xmtp_proto::xmtp::v3::message_contents::VmacContactBundle;
+
+use crate::{utils::base64_encode, vmac_protos::ProtoWrapper};
+
+#[derive(Debug, Error)]
+pub enum ContactError {
+    #[error("decode error")]
+    Decode(#[from] DecodeError),
+    #[error("encode error")]
+    Encode(#[from] EncodeError),
+    #[error("unknown error")]
+    Unknown,
+}
+
+pub struct Contact {
+    bundle: VmacContactBundle,
+}
+
+impl Contact {
+    pub fn from_bytes(bytes: Vec<u8>) -> Result<Self, ContactError> {
+        let bundle = VmacContactBundle::decode(bytes.as_slice())?;
+
+        Ok(Self { bundle })
+    }
+
+    pub fn to_bytes(&self) -> Result<Vec<u8>, ContactError> {
+        let mut buf = Vec::new();
+        self.bundle.encode(&mut buf)?;
+
+        Ok(buf)
+    }
+
+    // The id of a contact is the base64 encoding of the keccak256 hash of the identity key
+    pub fn id(&self) -> String {
+        base64_encode(keccak256(self.vmac_identity_key().to_string().as_str()).as_slice())
+    }
+
+    pub fn vmac_identity_key(&self) -> Curve25519PublicKey {
+        // TODO: Replace unwrap with proper error handling
+        let proto_key = ProtoWrapper {
+            proto: self.bundle.clone().identity_key.unwrap(),
+        };
+
+        proto_key.into()
+    }
+
+    pub fn vmac_fallback_key(&self) -> Curve25519PublicKey {
+        let proto_key = ProtoWrapper {
+            proto: self.bundle.clone().prekey.unwrap(),
+        };
+
+        proto_key.into()
+    }
+}
