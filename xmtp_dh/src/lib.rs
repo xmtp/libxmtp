d33d945febe903ebc74e0fd5f5fb6d8235ab7330
Richard Hua <rich@rkhua.com>
2023-05-26 14:02:42 +1000

    Remove dep on xmtpv3

--- xmtp_dh/src/lib.rs
+++ xmtp_dh/src/lib.rs
@@ -3,8 +3,5 @@ uniffi_macros::include_scaffolding!("xmtp_dh");
 pub use xmtp_dh;
 
 pub fn e2e_selftest() -> String {
-    // Returns Result<String>
-    xmtpv3::manager::e2e_selftest()
-        .map_err(|e| format!("{:?}", e))
-        .unwrap()
+    "Hello world"
 }
