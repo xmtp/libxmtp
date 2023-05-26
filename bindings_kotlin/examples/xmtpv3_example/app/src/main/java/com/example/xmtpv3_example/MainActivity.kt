a077c2f1c4134d402d2ea028351addc1c7cc9428
Richard Hua <rich@rkhua.com>
2023-05-26 13:34:09 +1000

    Copy over example app from vmac prototype

---
+++ bindings_kotlin/examples/xmtpv3_example/app/src/main/java/com/example/xmtpv3_example/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.xmtpv3_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.xmtpv3_example.R.id.selftest_output
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById<TextView>(selftest_output)
+        textView.text = uniffi.xmtpv3.e2eSelftest()
+    }
+}
