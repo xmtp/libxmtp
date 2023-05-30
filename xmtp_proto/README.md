9d4ca0e5937aef2344eb29b216d25ddc955977fa
Richard Hua <rich@rkhua.com>
2023-05-30 17:03:48 +1000

    Update proto generation and commit generated files

---
+++ xmtp_proto/README.md
@@ -0,0 +1,5 @@
+# xmtp_proto
+
+This crate generates Rust definitions and methods for protobufs from https://github.com/xmtp/proto.
+
+Make sure to run `./gen_defs.sh` and commit your changes whenever you need to consume new changes from the proto repo.
--- xmtp_proto/gen_defs.sh
+++ xmtp_proto/gen_defs.sh
@@ -3,7 +3,7 @@ if ! cargo install --list | grep "protoc-gen-prost-crate" > /dev/null; then
         echo "Failed to install protoc-gen-prost-crate"
         exit 1
     fi
-elif
+fi
 if ! buf generate https://github.com/xmtp/proto.git#branch=xmtpv3,subdir=proto; then
     echo "Failed to generate protobuf definitions"
     exit 1
