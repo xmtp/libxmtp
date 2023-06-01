fe4518a34d58fa7c8b99d90d62c6e4aca1d7be95
Richard Hua <rich@rkhua.com>
2023-06-01 11:55:41 +1000

    Move script into dev/ directory

--- dev/gen_protos.sh
+++ dev/gen_protos.sh
@@ -1,3 +1,4 @@
+pushd xmtp_proto > /dev/null
 if ! cargo install --list | grep "protoc-gen-prost-crate" > /dev/null; then
     if ! cargo install protoc-gen-prost-crate; then
         echo "Failed to install protoc-gen-prost-crate"
@@ -8,3 +9,4 @@ if ! buf generate https://github.com/xmtp/proto.git#branch=xmtpv3,subdir=proto; 
     echo "Failed to generate protobuf definitions"
     exit 1
 fi
+popd > /dev/null
