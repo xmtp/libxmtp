ab0d06aa4d3a3092d7529449bc09a3ce6f2f5993
Daniel McCartney <dmccartney@gmail.com>
2023-06-01 12:43:52 -0700

    feat: use proto encoding for rpc requests across bridge

--- bindings_swift/src/lib.rs
+++ bindings_swift/src/lib.rs
@@ -1,91 +1,29 @@
-use types::BatchQueryResponse;
-use types::QueryResponse;
+use prost::Message;
 use xmtp_crypto::{hashes, k256_helper};
 use xmtp_networking::grpc_api_helper;
-use xmtp_proto::xmtp::message_api::v1::{Envelope as EnvelopeProto, PagingInfo, QueryRequest};
-pub mod types;
+use xmtp_proto::xmtp::message_api::v1::{
+    BatchQueryRequest, BatchQueryResponse, PublishRequest, PublishResponse, QueryRequest,
+    QueryResponse, SubscribeRequest,
+};
 
 #[swift_bridge::bridge]
 mod ffi {
-    enum SortDirection {
-        Unspecified = 0,
-        Ascending = 1,
-        Descending = 2,
-    }
-
-    #[swift_bridge(swift_repr = "struct")]
-    struct IndexCursor {
-        digest: Vec<u8>,
-        sender_time_ns: u64,
-    }
-    #[swift_bridge(swift_repr = "struct")]
-    struct PagingInfo {
-        limit: u32,
-        cursor: Option<IndexCursor>,
-        direction: SortDirection,
-    }
-
-    extern "Rust" {
-        type Envelope;
-
-        fn create_envelope(topic: String, sender_time_ns: u64, payload: Vec<u8>) -> Envelope;
-
-        fn get_topic(&self) -> String;
-        fn get_sender_time_ns(&self) -> u64;
-        fn get_payload(&self) -> Vec<u8>;
-    }
-
     extern "Rust" {
         type RustSubscription;
 
-        fn get_messages(&self) -> Result<Vec<Envelope>, String>;
+        fn get_envelopes_as_query_response(&self) -> Result<Vec<u8>, String>;
 
         fn close(&mut self);
-    }
-
-    extern "Rust" {
-        type QueryRequest;
-    }
-
-    extern "Rust" {
-        type QueryResponse;
-
-        fn envelopes(&self) -> Vec<Envelope>;
-        fn paging_info(&self) -> Option<PagingInfo>;
-
-    }
-
-    extern "Rust" {
-        type BatchQueryResponse;
-
-        fn responses(&self) -> Vec<QueryResponse>;
     }
 
     extern "Rust" {
         type RustClient;
 
         async fn create_client(host: String, is_secure: bool) -> Result<RustClient, String>;
-
-        async fn batch_query(
-            &mut self,
-            requests: Vec<QueryRequest>,
-        ) -> Result<BatchQueryResponse, String>;
-
-        async fn query(
-            &mut self,
-            topic: String,
-            start_time_ns: Option<u64>,
-            end_time_ns: Option<u64>,
-            paging_info: Option<PagingInfo>,
-        ) -> Result<QueryResponse, String>;
-
-        async fn publish(
-            &mut self,
-            token: String,
-            envelopes: Vec<Envelope>,
-        ) -> Result<String, String>;
-
-        async fn subscribe(&mut self, topics: Vec<String>) -> Result<RustSubscription, String>;
+        async fn batch_query(&mut self, req: Vec<u8>) -> Result<Vec<u8>, String>;
+        async fn query(&mut self, req: Vec<u8>) -> Result<Vec<u8>, String>;
+        async fn publish(&mut self, token: String, req: Vec<u8>) -> Result<Vec<u8>, String>;
+        async fn subscribe(&mut self, req: Vec<u8>) -> Result<RustSubscription, String>;
     }
 
     extern "Rust" {
@@ -126,58 +64,41 @@ async fn create_client(host: String, is_secure: bool) -> Result<RustClient, Stri
 }
 
 impl RustClient {
-    async fn batch_query(
-        &mut self,
-        requests: Vec<QueryRequest>,
-    ) -> Result<BatchQueryResponse, String> {
-        let result = self
+    async fn batch_query(&mut self, req: Vec<u8>) -> Result<Vec<u8>, String> {
+        let request: BatchQueryRequest = Message::decode(&req[..]).map_err(|e| format!("{}", e))?;
+        let result: BatchQueryResponse = self
             .client
-            .batch_query(requests)
+            .batch_query(request)
             .await
             .map_err(|e| format!("{}", e))?;
-
-        Ok(BatchQueryResponse::from(result))
+        Ok(result.encode_to_vec())
     }
 
-    async fn query(
-        &mut self,
-        topic: String,
-        start_time_ns: Option<u64>,
-        end_time_ns: Option<u64>,
-        paging_info: Option<ffi::PagingInfo>,
-    ) -> Result<QueryResponse, String> {
-        let info = paging_info.map(PagingInfo::from);
-
-        let result = self
+    async fn query(&mut self, req: Vec<u8>) -> Result<Vec<u8>, String> {
+        let request: QueryRequest = Message::decode(&req[..]).map_err(|e| format!("{}", e))?;
+        let result: QueryResponse = self
             .client
-            .query(topic, start_time_ns, end_time_ns, info)
+            .query(request)
             .await
             .map_err(|e| format!("{}", e))?;
-
-        Ok(QueryResponse::from(result))
+        Ok(result.encode_to_vec())
     }
 
-    // Left a u32 in there to either 1) return the number of envelopes published or 2) return an error code
-    async fn publish(&mut self, token: String, envelopes: Vec<Envelope>) -> Result<String, String> {
-        let mut xmtp_envelopes = vec![];
-        for envelope in envelopes {
-            xmtp_envelopes.push(EnvelopeProto::from(envelope));
-        }
-
-        self.client
-            .publish(token, xmtp_envelopes)
+    async fn publish(&mut self, token: String, req: Vec<u8>) -> Result<Vec<u8>, String> {
+        let request: PublishRequest = Message::decode(&req[..]).map_err(|e| format!("{}", e))?;
+        let result: PublishResponse = self
+            .client
+            .publish(token, request)
             .await
             .map_err(|e| format!("{}", e))?;
-
-        // Swift-Bridge needs this to be a type that has an initializer in Swift
-        // after conversion, such as RustString
-        Ok("".to_string())
+        Ok(result.encode_to_vec())
     }
 
-    async fn subscribe(&mut self, topics: Vec<String>) -> Result<RustSubscription, String> {
+    async fn subscribe(&mut self, req: Vec<u8>) -> Result<RustSubscription, String> {
+        let request: SubscribeRequest = Message::decode(&req[..]).map_err(|e| format!("{}", e))?;
         let subscription = self
             .client
-            .subscribe(topics)
+            .subscribe(request)
             .await
             .map_err(|e| format!("{}", e))?;
 
@@ -190,75 +111,25 @@ pub struct RustSubscription {
 }
 
 impl RustSubscription {
-    pub fn get_messages(&self) -> Result<Vec<Envelope>, String> {
+    // Returns a serialized `QueryResponse` as a convenient envelopes wrapper.
+    pub fn get_envelopes_as_query_response(&self) -> Result<Vec<u8>, String> {
         let new_messages = self.subscription.get_messages();
-        // Return the last envelopes even if the stream is closed
-        if !new_messages.is_empty() {
-            return Ok(new_messages
-                .iter()
-                .map(|e| Envelope::from(e.clone()))
-                .collect());
+        if new_messages.is_empty() {
+            // If the stream is closed AND empty, return an error
+            if self.subscription.is_closed() {
+                return Err("subscription_closed".to_string());
+            }
         }
-        // If the stream is closed AND empty, return an error
-        if self.subscription.is_closed() {
-            return Err("subscription_closed".to_string());
+        return Ok(QueryResponse {
+            envelopes: new_messages,
+            ..QueryResponse::default()
         }
-
-        Ok(vec![])
+        .encode_to_vec());
     }
 
     pub fn close(&mut self) {
         self.subscription.close_stream();
         // Think I am going to have to do some manual memory management to ensure everything gets dropped
-    }
-}
-
-// Define as an opaque type so we can make it Vectorizable
-#[derive(Clone)]
-pub struct Envelope {
-    pub content_topic: String,
-    pub timestamp_ns: u64,
-    pub message: Vec<u8>,
-}
-
-pub fn create_envelope(topic: String, timestamp_ns: u64, message: Vec<u8>) -> Envelope {
-    Envelope {
-        content_topic: topic,
-        timestamp_ns,
-        message,
-    }
-}
-
-impl Envelope {
-    pub fn get_topic(&self) -> String {
-        self.content_topic.clone()
-    }
-
-    pub fn get_sender_time_ns(&self) -> u64 {
-        self.timestamp_ns
-    }
-
-    pub fn get_payload(&self) -> Vec<u8> {
-        self.message.clone()
-    }
-}
-
-impl Clone for ffi::IndexCursor {
-    fn clone(&self) -> Self {
-        ffi::IndexCursor {
-            digest: self.digest.clone(),
-            sender_time_ns: self.sender_time_ns,
-        }
-    }
-}
-
-impl Clone for ffi::PagingInfo {
-    fn clone(&self) -> Self {
-        ffi::PagingInfo {
-            limit: self.limit,
-            cursor: self.cursor.clone(),
-            direction: self.direction,
-        }
     }
 }
 
@@ -321,15 +192,20 @@ fn recover_public_key_k256_keccak256(
 
 #[cfg(test)]
 mod tests {
+    use prost::Message;
     use std::time::{SystemTime, UNIX_EPOCH};
     use uuid::Uuid;
+    use xmtp_proto::xmtp::message_api::v1::{
+        BatchQueryRequest, BatchQueryResponse, Envelope, PublishRequest, QueryRequest,
+        QueryResponse, SubscribeRequest,
+    };
 
     static ADDRESS: &str = "http://localhost:5556";
 
-    pub fn test_envelope(topic: String) -> super::Envelope {
+    pub fn test_envelope(topic: String) -> Envelope {
         let time_since_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
 
-        super::Envelope {
+        Envelope {
             timestamp_ns: time_since_epoch.as_nanos() as u64,
             content_topic: topic,
             message: vec![65],
@@ -337,12 +213,33 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_batch_query() {
+    async fn test_publish_batch_query() {
         let mut client = super::create_client(ADDRESS.to_string(), false)
             .await
             .unwrap();
-        let result = client.batch_query(vec![]).await.unwrap();
-        assert_eq!(result.responses().len(), 0);
+
+        let topic = Uuid::new_v4();
+        client
+            .publish(
+                "".to_string(),
+                PublishRequest {
+                    envelopes: vec![test_envelope(topic.to_string())],
+                }
+                .encode_to_vec(),
+            )
+            .await
+            .unwrap();
+
+        let q = QueryRequest {
+            content_topics: vec![topic.to_string()],
+            ..QueryRequest::default()
+        };
+        let req = BatchQueryRequest { requests: vec![q] }.encode_to_vec();
+
+        let result = client.batch_query(req).await.unwrap();
+
+        let res = BatchQueryResponse::decode(&result[..]).unwrap();
+        assert_eq!(res.responses.len(), 1);
     }
 
     // Try a query on a test topic, and make sure we get a response
@@ -352,22 +249,32 @@ mod tests {
             .await
             .unwrap();
         let topic = Uuid::new_v4();
-        let publish_result = client
-            .publish("".to_string(), vec![test_envelope(topic.to_string())])
+        client
+            .publish(
+                "".to_string(),
+                PublishRequest {
+                    envelopes: vec![test_envelope(topic.to_string())],
+                }
+                .encode_to_vec(),
+            )
             .await
             .unwrap();
-
-        assert_eq!(publish_result, "".to_string());
 
         let result = client
-            .query(topic.to_string(), None, None, None)
+            .query(
+                QueryRequest {
+                    content_topics: vec![topic.to_string()],
+                    ..QueryRequest::default()
+                }
+                .encode_to_vec(),
+            )
             .await
             .unwrap();
 
-        let envelopes = result.envelopes();
-        assert_eq!(envelopes.len(), 1);
+        let q: QueryResponse = Message::decode(&result[..]).unwrap();
+        assert_eq!(q.envelopes.len(), 1);
 
-        let first_envelope = envelopes.get(0).unwrap();
+        let first_envelope = q.envelopes.get(0).unwrap();
         assert_eq!(first_envelope.content_topic, topic.to_string());
         assert!(first_envelope.timestamp_ns > 0);
         assert!(!first_envelope.message.is_empty());
@@ -379,21 +286,41 @@ mod tests {
         let mut client = super::create_client(ADDRESS.to_string(), false)
             .await
             .unwrap();
-        let mut sub = client.subscribe(vec![topic.to_string()]).await.unwrap();
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-        let publish_result = client
-            .publish("".to_string(), vec![test_envelope(topic.to_string())])
+        let mut sub = client
+            .subscribe(
+                SubscribeRequest {
+                    content_topics: vec![topic.to_string()],
+                    ..SubscribeRequest::default()
+                }
+                .encode_to_vec(),
+            )
             .await
             .unwrap();
-        assert_eq!(publish_result, "".to_string());
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        client
+            .publish(
+                "".to_string(),
+                PublishRequest {
+                    envelopes: vec![test_envelope(topic.to_string())],
+                }
+                .encode_to_vec(),
+            )
+            .await
+            .unwrap();
         tokio::time::sleep(std::time::Duration::from_millis(200)).await;
 
-        let messages = sub.get_messages().unwrap();
-        assert_eq!(messages.len(), 1);
-        let messages = sub.get_messages().unwrap();
-        assert_eq!(messages.len(), 0);
+        // At first there's the one envelope we got.
+        let result = sub.get_envelopes_as_query_response().unwrap();
+        let res: QueryResponse = Message::decode(&result[..]).unwrap();
+        assert_eq!(res.envelopes.len(), 1);
 
+        // But the next time it's empty.
+        let result = sub.get_envelopes_as_query_response().unwrap();
+        let res: QueryResponse = Message::decode(&result[..]).unwrap();
+        assert_eq!(res.envelopes.len(), 0);
+
+        // And now since it is closed, it should error.
         sub.close();
-        assert!(sub.get_messages().is_err());
+        assert!(sub.get_envelopes_as_query_response().is_err());
     }
 }
