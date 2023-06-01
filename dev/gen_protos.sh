0c03c0d8e56a1bceda5e7d4c512c821390eb2d52
Richard Hua <rich@xmtp.com>
2023-05-31 18:56:47 -0700

    Generate protos via shell script (#130)
    The original `build.rs` makes some assumptions about what is available on the host operating system, as well as fetching from github. This breaks cross-compilation which happens via docker containers, means the build can't run when offline, and causes breaking changes to `xmtp/proto` to break the build in `libxmtp`. This PR commits the generated files instead, and replaces the `build.rs` with a shell script that we can run whenever we explicitly want to pull in changes.

---
+++ dev/gen_protos.sh
@@ -0,0 +1,12 @@
+pushd xmtp_proto > /dev/null
+if ! cargo install --list | grep "protoc-gen-prost-crate" > /dev/null; then
+    if ! cargo install protoc-gen-prost-crate; then
+        echo "Failed to install protoc-gen-prost-crate"
+        exit 1
+    fi
+fi
+if ! buf generate https://github.com/xmtp/proto.git#branch=xmtpv3,subdir=proto; then
+    echo "Failed to generate protobuf definitions"
+    exit 1
+fi
+popd > /dev/null
