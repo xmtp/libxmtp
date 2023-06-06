6e672f57b9f1b021025a599a5f6077c1fb696032
Richard Hua <rich@rkhua.com>
2023-06-06 22:44:29 +1000

    Build debug

--- bindings_ffi/cross_build.sh
+++ bindings_ffi/cross_build.sh
@@ -4,25 +4,26 @@ main() {
   set -ex
 
   # Change to "release" to generate much smaller libraries
-  PROFILE="release"
+  PROFILE="dev"
+  BINARY_PROFILE="debug"
 
-  cross build --target x86_64-linux-android --target-dir ./target --$PROFILE && \
-      cross build --target i686-linux-android --target-dir ./target --$PROFILE && \
-      cross build --target armv7-linux-androideabi --target-dir ./target --$PROFILE && \
-      cross build --target aarch64-linux-android --target-dir ./target --$PROFILE
+  cross build --target x86_64-linux-android --target-dir ./target --profile $PROFILE && \
+      cross build --target i686-linux-android --target-dir ./target --profile $PROFILE && \
+      cross build --target armv7-linux-androideabi --target-dir ./target --profile $PROFILE && \
+      cross build --target aarch64-linux-android --target-dir ./target --profile $PROFILE
 
   # Move everything to jniLibs folder and rename, TODO: should be the same name
   LIBRARY_NAME="libbindings_ffi"
   TARGET_NAME="libuniffi_xmtpv3"
   rm -rf jniLibs/
   mkdir -p jniLibs/arm64-v8a/ && \
-    cp target/aarch64-linux-android/$PROFILE/$LIBRARY_NAME.so jniLibs/arm64-v8a/$TARGET_NAME.so && \
+    cp target/aarch64-linux-android/$BINARY_PROFILE/$LIBRARY_NAME.so jniLibs/arm64-v8a/$TARGET_NAME.so && \
     mkdir -p jniLibs/armeabi-v7a/ && \
-      cp target/armv7-linux-androideabi/$PROFILE/$LIBRARY_NAME.so jniLibs/armeabi-v7a/$TARGET_NAME.so && \
+      cp target/armv7-linux-androideabi/$BINARY_PROFILE/$LIBRARY_NAME.so jniLibs/armeabi-v7a/$TARGET_NAME.so && \
     mkdir -p jniLibs/x86/ && \
-      cp target/i686-linux-android/$PROFILE/$LIBRARY_NAME.so jniLibs/x86/$TARGET_NAME.so && \
+      cp target/i686-linux-android/$BINARY_PROFILE/$LIBRARY_NAME.so jniLibs/x86/$TARGET_NAME.so && \
     mkdir -p jniLibs/x86_64/ && \
-      cp target/x86_64-linux-android/$PROFILE/$LIBRARY_NAME.so jniLibs/x86_64/$TARGET_NAME.so
+      cp target/x86_64-linux-android/$BINARY_PROFILE/$LIBRARY_NAME.so jniLibs/x86_64/$TARGET_NAME.so
 }
 
 time main
