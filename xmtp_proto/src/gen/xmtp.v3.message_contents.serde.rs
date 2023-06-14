c88f1ed52c63a1004527d75eda0aea4611304a9c
Nicholas Molnar <nicholas@xmtp.com>
2023-06-14 11:04:27 -0700

    Update protos

--- xmtp_proto/src/gen/xmtp.v3.message_contents.serde.rs
+++ xmtp_proto/src/gen/xmtp.v3.message_contents.serde.rs
@@ -279,6 +279,304 @@ impl<'de> serde::Deserialize<'de> for InstallationContactBundle {
         deserializer.deserialize_struct("xmtp.v3.message_contents.InstallationContactBundle", FIELDS, GeneratedVisitor)
     }
 }
+impl serde::Serialize for InvitationEnvelope {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if self.version.is_some() {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("xmtp.v3.message_contents.InvitationEnvelope", len)?;
+        if let Some(v) = self.version.as_ref() {
+            match v {
+                invitation_envelope::Version::V1(v) => {
+                    struct_ser.serialize_field("v1", v)?;
+                }
+            }
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for InvitationEnvelope {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "v1",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            V1,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "v1" => Ok(GeneratedField::V1),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = InvitationEnvelope;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct xmtp.v3.message_contents.InvitationEnvelope")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> std::result::Result<InvitationEnvelope, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut version__ = None;
+                while let Some(k) = map.next_key()? {
+                    match k {
+                        GeneratedField::V1 => {
+                            if version__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("v1"));
+                            }
+                            version__ = map.next_value::<::std::option::Option<_>>()?.map(invitation_envelope::Version::V1)
+;
+                        }
+                    }
+                }
+                Ok(InvitationEnvelope {
+                    version: version__,
+                })
+            }
+        }
+        deserializer.deserialize_struct("xmtp.v3.message_contents.InvitationEnvelope", FIELDS, GeneratedVisitor)
+    }
+}
+impl serde::Serialize for InvitationEnvelopeV1 {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if self.inviter.is_some() {
+            len += 1;
+        }
+        if !self.ciphertext.is_empty() {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("xmtp.v3.message_contents.InvitationEnvelopeV1", len)?;
+        if let Some(v) = self.inviter.as_ref() {
+            struct_ser.serialize_field("inviter", v)?;
+        }
+        if !self.ciphertext.is_empty() {
+            struct_ser.serialize_field("ciphertext", pbjson::private::base64::encode(&self.ciphertext).as_str())?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for InvitationEnvelopeV1 {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "inviter",
+            "ciphertext",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            Inviter,
+            Ciphertext,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "inviter" => Ok(GeneratedField::Inviter),
+                            "ciphertext" => Ok(GeneratedField::Ciphertext),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = InvitationEnvelopeV1;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct xmtp.v3.message_contents.InvitationEnvelopeV1")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> std::result::Result<InvitationEnvelopeV1, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut inviter__ = None;
+                let mut ciphertext__ = None;
+                while let Some(k) = map.next_key()? {
+                    match k {
+                        GeneratedField::Inviter => {
+                            if inviter__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("inviter"));
+                            }
+                            inviter__ = map.next_value()?;
+                        }
+                        GeneratedField::Ciphertext => {
+                            if ciphertext__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("ciphertext"));
+                            }
+                            ciphertext__ = 
+                                Some(map.next_value::<::pbjson::private::BytesDeserialize<_>>()?.0)
+                            ;
+                        }
+                    }
+                }
+                Ok(InvitationEnvelopeV1 {
+                    inviter: inviter__,
+                    ciphertext: ciphertext__.unwrap_or_default(),
+                })
+            }
+        }
+        deserializer.deserialize_struct("xmtp.v3.message_contents.InvitationEnvelopeV1", FIELDS, GeneratedVisitor)
+    }
+}
+impl serde::Serialize for InvitationV1 {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if !self.invitee_wallet_address.is_empty() {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("xmtp.v3.message_contents.InvitationV1", len)?;
+        if !self.invitee_wallet_address.is_empty() {
+            struct_ser.serialize_field("inviteeWalletAddress", &self.invitee_wallet_address)?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for InvitationV1 {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "invitee_wallet_address",
+            "inviteeWalletAddress",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            InviteeWalletAddress,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "inviteeWalletAddress" | "invitee_wallet_address" => Ok(GeneratedField::InviteeWalletAddress),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = InvitationV1;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct xmtp.v3.message_contents.InvitationV1")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> std::result::Result<InvitationV1, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut invitee_wallet_address__ = None;
+                while let Some(k) = map.next_key()? {
+                    match k {
+                        GeneratedField::InviteeWalletAddress => {
+                            if invitee_wallet_address__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("inviteeWalletAddress"));
+                            }
+                            invitee_wallet_address__ = Some(map.next_value()?);
+                        }
+                    }
+                }
+                Ok(InvitationV1 {
+                    invitee_wallet_address: invitee_wallet_address__.unwrap_or_default(),
+                })
+            }
+        }
+        deserializer.deserialize_struct("xmtp.v3.message_contents.InvitationV1", FIELDS, GeneratedVisitor)
+    }
+}
 impl serde::Serialize for RecoverableEcdsaSignature {
     #[allow(deprecated)]
     fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
