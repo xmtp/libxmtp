2df416072f9a8a8d022af8d6ee2c33f18eac5c53
Nicholas Molnar <nicholas@xmtp.com>
2023-06-15 13:48:05 -0700

    Add comment

--- xmtp/src/conversation.rs
+++ xmtp/src/conversation.rs
@@ -61,6 +61,7 @@ where
         let inner_invite_bytes = Invitation::build_inner_invite_bytes(self.peer_address.clone())?;
         for contact in self.members.iter() {
             let id = contact.id();
+            // TODO: Find existing session if it exists in the database
             let session = client.create_outbound_session(contact.clone())?;
             let invitation = Invitation::build(
                 client.account.contact(),
