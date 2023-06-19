d963b173f4ddb6a373790570f3f1bba150a031f9
Richard Hua <rich@xmtp.com>
2023-06-18 21:13:17 -0700

    Add integration test for xmtpv3 on Android (#141)
    This can tell us if there are any regressions in the end-to-end flow, and can eventually be added to CI.
    
    Right now the erroring test case is hanging indefinitely rather than throwing an error, will investigate that next.

--- bindings_ffi/examples/MainActivity.kt
+++ bindings_ffi/examples/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.TextView
 import com.example.xmtpv3_example.R.id.selftest_output
 import kotlinx.coroutines.runBlocking
 
+const val EMULATOR_LOCALHOST_ADDRESS = "http://10.0.2.2:5556"
+
 // An example Android app testing the end-to-end flow through Rust
 // Run setup_android_example.sh to set it up
 class MainActivity : AppCompatActivity() {
@@ -16,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
             val textView: TextView = findViewById<TextView>(selftest_output)
             try {
-                val client = uniffi.xmtpv3.createClient("http://10.0.2.2:5556", false);
+                val client = uniffi.xmtpv3.createClient(EMULATOR_LOCALHOST_ADDRESS, false);
                 textView.text = "Client constructed, wallet address: " + client.walletAddress();
             } catch (e: Exception) {
                 textView.text = "Failed to construct client: " + e.message;
