a7b5bc6e977201683fa4e5315cbc82ba390c20b9
Richard Hua <rich@rkhua.com>
2023-06-26 11:46:54 +1000

    Update Android integration test

--- bindings_ffi/examples/ExampleInstrumentedTest.kt
+++ bindings_ffi/examples/ExampleInstrumentedTest.kt
@@ -7,6 +7,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.web3j.crypto.Credentials
+import org.web3j.crypto.ECKeyPair
+import java.security.SecureRandom
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,18 +20,24 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun testHappyPath() {
+        val privateKey: ByteArray = SecureRandom().generateSeed(32)
+        val credentials: Credentials = Credentials.create(ECKeyPair.create(privateKey))
+        val inboxOwner = Web3jInboxOwner(credentials)
         runBlocking {
-            val client = uniffi.xmtpv3.createClient(EMULATOR_LOCALHOST_ADDRESS, false);
+            val client = uniffi.xmtpv3.createClient(inboxOwner, EMULATOR_LOCALHOST_ADDRESS, false);
             assertNotNull("Should be able to construct client", client.walletAddress())
         }
     }
 
     @Test
     fun testErrorThrows() {
+        val privateKey: ByteArray = SecureRandom().generateSeed(32)
+        val credentials: Credentials = Credentials.create(ECKeyPair.create(privateKey))
+        val inboxOwner = Web3jInboxOwner(credentials)
         runBlocking {
             var didThrow = false;
             try {
-                val client = uniffi.xmtpv3.createClient("http://incorrect:5556", false);
+                val client = uniffi.xmtpv3.createClient(inboxOwner, "http://incorrect:5556", false);
             } catch (e: Exception) {
                 didThrow = true;
             }
