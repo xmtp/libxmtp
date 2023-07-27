67182e773193b8576c5af53f5a75738774dca860
Martin Kobetic <mkobetic@users.noreply.github.com>
2023-07-27 07:52:14 -0400

    release action for android (#163)

---
+++ bindings_ffi/get_android_artifact.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+# Figure out the id of the Build run for the specified $HEAD_SHA
+RUN_ID=$(gh api /repos/$GITHUB_REPO/actions/runs \
+    -X GET \
+    -F head_sha=$HEAD_SHA \
+    --jq '.workflow_runs[] | select(.name == "Build") | .id')
+# Get the id of the libxmtp-android.zip artifacts for the run $RUN_ID
+ARTIFACT_ID=$(gh api /repos/$GITHUB_REPO/actions/runs/$RUN_ID/artifacts \
+    --jq '.artifacts[] | select(.name == "libxmtp-android.zip") | .id')
+# Fetch artifact $ARTIFACT_ID and unzip it because it will be double compressed
+# artifact.zip -> libxmtp-android.zip
+gh api /repos/$GITHUB_REPO/actions/artifacts/$ARTIFACT_ID/zip >artifact.zip
+unzip -o artifact.zip
