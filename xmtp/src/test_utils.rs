4a4ae815862bc8c60122b5137f588aed8c988d6c
Richard Hua <rich@rkhua.com>
2023-08-08 10:20:08 -0700

    Share networking client between clients for tests

--- xmtp/src/test_utils.rs
+++ xmtp/src/test_utils.rs
@@ -6,10 +6,27 @@ pub mod test_utils {
         ClientBuilder,
     };
 
-    pub async fn gen_test_client() -> Client<MockXmtpApiClient> {
-        let mut client = ClientBuilder::new_test().build().unwrap();
+    async fn gen_test_client_internal(api_client: MockXmtpApiClient) -> Client<MockXmtpApiClient> {
+        let mut client = ClientBuilder::new_test()
+            .api_client(api_client)
+            .build()
+            .unwrap();
         client.init().await.expect("BadReg");
         client
+    }
+
+    pub async fn gen_test_client() -> Client<MockXmtpApiClient> {
+        gen_test_client_internal(MockXmtpApiClient::new()).await
+    }
+
+    // Generate test clients pointing to the same network
+    pub async fn gen_two_test_clients() -> (Client<MockXmtpApiClient>, Client<MockXmtpApiClient>) {
+        let api_client_1 = MockXmtpApiClient::new();
+        let api_client_2 = api_client_1.clone();
+        (
+            gen_test_client_internal(api_client_1).await,
+            gen_test_client_internal(api_client_2).await,
+        )
     }
 
     pub async fn gen_test_conversation<'c, A: XmtpApiClient>(
