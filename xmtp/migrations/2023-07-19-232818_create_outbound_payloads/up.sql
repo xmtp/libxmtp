d79b0cd488f98f48c3f130947d567fc5aaf4155d
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2023-08-09 11:34:59 -0700

    Change Primary key of OutboundPayloads Table (#179)
    * change outbound_payloads key
    
    * add error details to wrapper
    
    * update name
    
    * set payload_id as primary

--- xmtp/migrations/2023-07-19-232818_create_outbound_payloads/up.sql
+++ xmtp/migrations/2023-07-19-232818_create_outbound_payloads/up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE outbound_payloads (
-    created_at_ns BIGINT PRIMARY KEY NOT NULL,
+    payload_id TEXT PRIMARY KEY NOT NULL,
+    created_at_ns BIGINT KEY NOT NULL,
     content_topic TEXT NOT NULL,
     payload BLOB NOT NULL,
     outbound_payload_state INTEGER NOT NULL
