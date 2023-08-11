4cfad9b387ba747ca199c9b54fd65e1938465c47
Richard Hua <rich@rkhua.com>
2023-08-10 17:10:45 -0700

    Move v2 crates to separate branch

--- xmtp_dh/gen_kotlin.sh
+++
@@ -1,8 +0,0 @@
-cargo build --release
-pushd .. > /dev/null
-rm -f xmtp_dh/src/uniffi/xmtp_dh/xmtp_dh.kt
-xmtp_dh/target/release/xmtp-dh-gen generate \
-    --lib-file xmtp_dh/target/release/libxmtp_dh.dylib \
-    xmtp_dh/src/xmtp_dh.udl \
-    --language kotlin
-popd > /dev/null
--- xmtp_keystore/src/keys/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod key_bundle;
-pub mod private_key;
-pub mod public_key;
