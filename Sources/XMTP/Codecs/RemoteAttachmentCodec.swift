a288cf754d16d4fdfd3106cda338447f5639771f
Naomi Plasterer <naomi@xmtp.com>
2023-10-03 13:37:09 -0700

    Fix iOS decoding from JS (#171)
    * fix: ios decode remote attachments from js
    
    * bump the pod spec

--- Sources/XMTP/Codecs/RemoteAttachmentCodec.swift
+++ Sources/XMTP/Codecs/RemoteAttachmentCodec.swift
@@ -174,11 +174,11 @@ public struct RemoteAttachmentCodec: ContentCodec {
 			throw RemoteAttachmentError.invalidScheme("no scheme parameter")
 		}
 
-		guard let scheme = RemoteAttachment.Scheme(rawValue: schemeString) else {
-			throw RemoteAttachmentError.invalidScheme("invalid scheme value. must be https://")
+        if (!schemeString.starts(with: "https")) {
+			throw RemoteAttachmentError.invalidScheme("invalid scheme value. must start with https")
 		}
 
-		var attachment = try RemoteAttachment(url: url, contentDigest: contentDigest, secret: secret, salt: salt, nonce: nonce, scheme: scheme)
+		var attachment = try RemoteAttachment(url: url, contentDigest: contentDigest, secret: secret, salt: salt, nonce: nonce, scheme: RemoteAttachment.Scheme.https)
 
 		if let contentLength = content.parameters["contentLength"] {
 			attachment.contentLength = Int(contentLength)
