d6d95d9b86f0ca464b961a4195317d6263d07c49
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2023-10-19 16:26:02 -0700

    Start adding tests

--- mls_validation_service/src/handlers.rs
+++ mls_validation_service/src/handlers.rs
@@ -1,6 +1,5 @@
 use tonic::{Request, Response, Status};
 
-use openmls::credentials::BasicCredential;
 use openmls::prelude::TlsDeserializeTrait;
 use xmtp_proto::xmtp::mls_validation::v1::{
     validate_identities_response::ValidationResponse, validation_api_server::ValidationApi,
@@ -64,5 +63,41 @@ impl ValidationApi for ValidationServer {
         request: Request<ValidateGroupMessagesRequest>,
     ) -> Result<Response<ValidateGroupMessagesResponse>, Status> {
         Err(Status::unimplemented("not implemented"))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use openmls_basic_credential::SignatureKeyPair;
+    use ethers::signers::LocalWallet;
+    use xmtp_proto::xmtp::v3::message_contents::Eip191Association as Eip191AssociationProto;
+    use crate::{owner::InboxOwner, association::{AssociationText, Eip191Association}};
+    use prost::Message;
+
+    fn generate_identity() {
+        let rng = &mut rand::thread_rng();
+        let wallet = LocalWallet::new(rng);
+        let signature_key_pair = SignatureKeyPair::new(ciphersuite.signature_algorithm()).unwrap();
+        let pub_key = signature_key_pair.public();
+        let wallet_address = wallet.get_address();
+        let association_text = AssociationText::new_static(wallet_address, pub_key.to_vec());
+        let signature = wallet
+            .sign(&association_text.text())
+            .expect("failed to sign");
+
+        let association =
+            Eip191Association::new(pub_key, association_text, signature).expect("bad signature");
+        let association_proto: Eip191AssociationProto = association.into();
+        let mut buf = Vec::new();
+        association_proto
+            .encode(&mut buf)
+            .expect("failed to serialize");
+        buf
+    }
+
+    #[tokio::test]
+    async fn test_validate_identities_happy() {
+        let pub_key = generate_identity()
     }
 }
