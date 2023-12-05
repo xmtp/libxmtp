90e5562dfd7139215cd4cb78efc9683a2c9cc0c5
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2023-12-05 09:14:51 -0800

    Fix incorrect function args (#361)

--- user_preferences_bindings_wasm/src/lib.rs
+++ user_preferences_bindings_wasm/src/lib.rs
@@ -7,22 +7,30 @@ use xmtp_user_preferences::{
 
 #[wasm_bindgen]
 pub fn user_preferences_encrypt(
+    public_key: Vec<u8>,
     private_key: Vec<u8>,
     message: Vec<u8>,
-    topic: String,
 ) -> Result<Vec<u8>, String> {
-    let ciphertext = encrypt_message(private_key.as_slice(), message.as_slice(), topic)?;
+    let ciphertext = encrypt_message(
+        public_key.as_slice(),
+        private_key.as_slice(),
+        message.as_slice(),
+    )?;
 
     Ok(ciphertext)
 }
 
 #[wasm_bindgen]
 pub fn user_preferences_decrypt(
+    public_key: Vec<u8>,
     private_key: Vec<u8>,
     message: Vec<u8>,
-    topic: String,
 ) -> Result<Vec<u8>, String> {
-    let decrypted = decrypt_message(private_key.as_slice(), message.as_slice(), topic)?;
+    let decrypted = decrypt_message(
+        public_key.as_slice(),
+        private_key.as_slice(),
+        message.as_slice(),
+    )?;
 
     Ok(decrypted)
 }
