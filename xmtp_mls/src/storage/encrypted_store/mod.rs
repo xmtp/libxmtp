f9dd84331fb4295af020f1f1711f4c7baee63c47
Richard Hua <rich@rkhua.com>
2023-12-14 12:18:23 +1100

    Emit a consistent error to fix unit test

--- xmtp_mls/src/storage/encrypted_store/mod.rs
+++ xmtp_mls/src/storage/encrypted_store/mod.rs
@@ -117,7 +117,8 @@ impl EncryptedMessageStore {
 
     fn init_db(&mut self) -> Result<(), StorageError> {
         let conn = &mut self.raw_conn()?;
-        conn.batch_execute("PRAGMA journal_mode = WAL;")?;
+        conn.batch_execute("PRAGMA journal_mode = WAL;")
+            .map_err(|e| StorageError::DbInit(e.to_string()))?;
 
         log::info!("Running DB migrations");
         conn.run_pending_migrations(MIGRATIONS)
