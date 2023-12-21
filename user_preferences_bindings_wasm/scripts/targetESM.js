8cf44a0783829c14fec389118ec24393fd662fa0
Ry Racherbaumer <ry@xmtp.com>
2023-12-20 18:34:25 -0600

    Use sync file read/write

--- user_preferences_bindings_wasm/scripts/targetESM.js
+++ user_preferences_bindings_wasm/scripts/targetESM.js
@@ -1,18 +1,18 @@
-import { readFile, writeFile } from "node:fs/promises";
+import { readFileSync, writeFileSync } from "node:fs";
 
-const cargoTomlContent = await readFile("./Cargo.toml", "utf8");
+const cargoTomlContent = readFileSync("./Cargo.toml", "utf8");
 const cargoPackageName = /\[package\]\nname = "(.*?)"/.exec(
   cargoTomlContent
 )[1];
 const name = cargoPackageName.replace(/-/g, "_");
 
-const content = await readFile(`./dist/node/${name}.js`, "utf8");
+const content = readFileSync(`./dist/node/${name}.js`, "utf8");
 
 const patched = content
   // use global TextDecoder TextEncoder
   .replace(
     "let imports = {};",
-    `import { readFile } from "node:fs/promises";
+    `import { readFileSync } from "node:fs";
 import { dirname, join }  from "node:path";
 import { fileURLToPath } from 'url';
 let imports = {};
@@ -29,8 +29,8 @@ let imports = {};
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const path = join(__dirname, "${name}_bg.wasm");
-const bytes = await readFile(path);
+const bytes = readFileSync(path);
 `
   );
 
-await writeFile(`./dist/node/${name}.mjs`, patched);
+writeFileSync(`./dist/node/${name}.mjs`, patched);
