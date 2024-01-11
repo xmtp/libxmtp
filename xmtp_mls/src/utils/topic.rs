66b10c799df8dea66179883cde8e48f0463b6dbd
Steven Normore <steven@xmtp.com>
2024-01-11 16:45:44 -0500

    fix: use /xmtp/mls/1 topic prefix

--- xmtp_mls/src/utils/topic.rs
+++ xmtp_mls/src/utils/topic.rs
@@ -1,9 +1,9 @@
 use crate::utils::id::serialize_group_id;
 
 pub fn get_group_topic(group_id: &[u8]) -> String {
-    format!("/xmtp/3/g-{}/proto", serialize_group_id(group_id))
+    format!("/xmtp/mls/1/g-{}/proto", serialize_group_id(group_id))
 }
 
 pub fn get_welcome_topic(installation_id: &Vec<u8>) -> String {
-    format!("/xmtp/3/w-{}/proto", hex::encode(installation_id))
+    format!("/xmtp/mls/1/w-{}/proto", hex::encode(installation_id))
 }
