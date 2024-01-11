0404d2ccf34fb08a9c46b1905014c861a3a61c9a
Steven Normore <snormore@gmail.com>
2024-01-11 17:43:02 -0500

    refactor: move message_api.v3 to mls.api.v1 (#412)
    * refactor: move message_api.v3 to mls.api.v1
    
    * Update dev/gen_protos.sh to use main
    
    * chore: cargo fmt
    
    * fix: use /xmtp/mls/1 topic prefix

--- dev/gen_protos.sh
+++ dev/gen_protos.sh
@@ -7,7 +7,7 @@ if ! cargo install --list | grep "protoc-gen-prost-crate" > /dev/null; then
 fi
 
 # Please always specify the exact commit to use for generation!
-if ! buf generate https://github.com/xmtp/proto.git#branch=main,ref=7be5c72,subdir=proto; then
+if ! buf generate https://github.com/xmtp/proto.git#branch=main,ref=665f2f1,subdir=proto; then
     echo "Failed to generate protobuf definitions"
     exit 1
 fi
