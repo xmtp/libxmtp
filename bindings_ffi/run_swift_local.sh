526cfc1feda4c1988e10531ac9f723d2c2e8928a
Cameron Voell <cameronvoell@gmail.com>
2024-01-29 08:15:31 -0800

    Release swift bindings (#460)
    * Correct order in swiftlocal makefile
    
    * Added github action for adding zipped xcframework to github release on tag action
    
    * Run on branch commits instead of tags
    
    * Updated tag name to not begin with refs
    
    * Run swift local only needs to copy over source now
    
    * fix tag name, add license to release zip artifact
    
    * Add step for calculating checksum for release description
    
    * Fix SHA command and check LICENSE is copied as expected
    
    * Updated to only fire from manual workflow dispatch for now

--- bindings_ffi/run_swift_local.sh
+++ bindings_ffi/run_swift_local.sh
@@ -1,4 +1,3 @@
 # Assumes libxmtp is in a peer directory of libxmtp-swift
-make swiftlocal ; rm -rf ../../libxmtp-swift/LibXMTPRust.xcframework
-cp -r ./LibXMTPRust.xcframework ../../libxmtp-swift/LibXMTPRust.xcframework
+make swift
 cp build/swift/xmtpv3.swift ../../libxmtp-swift/Sources/LibXMTP/xmtpv3.swift
