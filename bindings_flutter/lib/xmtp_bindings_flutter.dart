130e96b8164d774504b27361b9853876d594cb34
Daniel McCartney <dmccartney@gmail.com>
2024-02-02 20:29:59 -0500

    fix: clean up some nits from publishing warnings (#479)

--- bindings_flutter/lib/xmtp_bindings_flutter.dart
+++ bindings_flutter/lib/xmtp_bindings_flutter.dart
@@ -3,4 +3,4 @@ library xmtp_bindings_flutter;
 
 export 'src/generated/frb_generated.dart';
 export 'src/generated/libxmtp_api.dart';
-export 'src/loader.dart' show libxmtp, libxmtpInit, libxmtpDispose;
+export 'src/loader.dart' show libxmtpInit, libxmtpDispose;
