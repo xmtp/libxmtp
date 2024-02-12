767824ea8ee15207e9b5fa896f417b7a25447d64
Naomi Plasterer <naomi@xmtp.com>
2024-02-12 10:15:03 -0800

    actually commit the code

--- bindings_ffi/examples/MainActivity.kt
+++ bindings_ffi/examples/MainActivity.kt
@@ -5,19 +5,19 @@ import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.xmtpv3_example.R.id.selftest_output
+import java.nio.charset.StandardCharsets
 import kotlinx.coroutines.runBlocking
 import org.bouncycastle.util.encoders.Hex.toHexString
 import org.web3j.crypto.Credentials
-import org.web3j.crypto.ECKeyPair
 import org.web3j.crypto.Sign
+import org.xmtp.android.library.Client
+import org.xmtp.android.library.ClientOptions
+import org.xmtp.android.library.XMTPEnvironment
+import org.xmtp.android.library.messages.PrivateKeyBuilder
 import uniffi.xmtpv3.FfiConversationCallback
 import uniffi.xmtpv3.FfiGroup
 import uniffi.xmtpv3.FfiInboxOwner
 import uniffi.xmtpv3.FfiLogger
-import uniffi.xmtpv3.LegacyIdentitySource
-import java.io.File
-import java.nio.charset.StandardCharsets
-import java.security.SecureRandom
 
 const val EMULATOR_LOCALHOST_ADDRESS = "http://10.0.2.2:5556"
 const val DEV_NETWORK_ADDRESS = "https://dev.xmtp.network:5556"
@@ -40,9 +40,15 @@ class AndroidFfiLogger : FfiLogger {
     }
 }
 
-class ConversationCallback: FfiConversationCallback {
+class ConversationCallback : FfiConversationCallback {
     override fun onConversation(conversation: FfiGroup) {
-        Log.i("App", "INFO - Conversation callback with ID: " + toHexString(conversation.id()) + ", members: " + conversation.listMembers())
+        Log.i(
+                "App",
+                "INFO - Conversation callback with ID: " +
+                        toHexString(conversation.id()) +
+                        ", members: " +
+                        conversation.listMembers()
+        )
     }
 }
 
@@ -54,44 +60,58 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView: TextView = findViewById<TextView>(selftest_output)
-        val privateKey: ByteArray = SecureRandom().generateSeed(32)
-        val credentials: Credentials = Credentials.create(ECKeyPair.create(privateKey))
-        val inboxOwner = Web3jInboxOwner(credentials)
-        val dbDir: File = File(this.filesDir.absolutePath, "xmtp_db")
-        dbDir.mkdir()
-        val dbPath: String = dbDir.absolutePath + "/android_example.db3"
-        val dbEncryptionKey = SecureRandom().generateSeed(32)
-        Log.i(
-            "App",
-            "INFO -\naccountAddress: " + inboxOwner.getAddress() + "\nprivateKey: " + privateKey.asList() + "\nDB path: " + dbPath + "\nDB encryption key: " + dbEncryptionKey
-        )
+        // val privateKey: ByteArray = SecureRandom().generateSeed(32)
+        // val credentials: Credentials = Credentials.create(ECKeyPair.create(privateKey))
+        // val inboxOwner = Web3jInboxOwner(credentials)
+        // val dbDir: File = File(this.filesDir.absolutePath, "xmtp_db")
+        // dbDir.mkdir()
+        // val dbPath: String = dbDir.absolutePath + "/android_example.db3"
+        // val dbEncryptionKey = SecureRandom().generateSeed(32)
+        // Log.i(
+        //     "App",
+        //     "INFO -\naccountAddress: " + inboxOwner.getAddress() + "\nprivateKey: " +
+        // privateKey.asList() + "\nDB path: " + dbPath + "\nDB encryption key: " + dbEncryptionKey
+        // )
 
         runBlocking {
             try {
-                val client = uniffi.xmtpv3.createClient(
-                    AndroidFfiLogger(),
-                    EMULATOR_LOCALHOST_ADDRESS,
-                    false,
-                    dbPath,
-                    dbEncryptionKey,
-                    inboxOwner.getAddress(),
-                    LegacyIdentitySource.NONE,
-                    null,
-                )
-                var walletSignature: ByteArray? = null;
-                val textToSign = client.textToSign();
-                if (textToSign != null) {
-                    walletSignature = inboxOwner.sign(textToSign)
-                }
-                client.registerIdentity(walletSignature);
-                textView.text = "Client constructed, wallet address: " + client.accountAddress()
+                val options =
+                        ClientOptions(
+                                api =
+                                        ClientOptions.Api(
+                                                env = XMTPEnvironment.LOCAL,
+                                                isSecure = false,
+                                        ),
+                                enableAlphaMls = true,
+                                appContext = this@MainActivity
+                        )
+                val key = PrivateKeyBuilder()
+                val client = Client().create(account = key, options = options)
+
+                // val client = uniffi.xmtpv3.createClient(
+                //     AndroidFfiLogger(),
+                //     EMULATOR_LOCALHOST_ADDRESS,
+                //     false,
+                //     dbPath,
+                //     dbEncryptionKey,
+                //     inboxOwner.getAddress(),
+                //     LegacyIdentitySource.NONE,
+                //     null,
+                // )
+                // var walletSignature: ByteArray? = null;
+                // val textToSign = client.textToSign();
+                // if (textToSign != null) {
+                //     walletSignature = inboxOwner.sign(textToSign)
+                // }
+                // client.registerIdentity(walletSignature);
+                textView.text = "Client constructed, wallet address: " + client.address
                 Log.i("App", "Setting up conversation streaming")
-                client.conversations().stream(ConversationCallback());
+                client.conversations.stream()
             } catch (e: Exception) {
                 textView.text = "Failed to construct client: " + e.message
             }
         }
 
-        dbDir.deleteRecursively()
+        //        dbDir.deleteRecursively()
     }
 }
