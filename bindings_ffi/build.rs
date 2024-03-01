5fc5467e2e4c96cf99ac8fee84ca998d2fe525d8
tuddman <tuddman@users.noreply.github.com>
2024-03-01 20:58:33 +0100

    chore: clippy cleanup in bindings_ffi

--- bindings_ffi/build.rs
+++ bindings_ffi/build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     uniffi::generate_scaffolding("./src/xmtpv3.udl").expect("Building the UDL file failed");
     Command::new("make")
-        .args(&["libxmtp-version"])
+        .args(["libxmtp-version"])
         .status()
         .expect("failed to make libxmtp-version");
 }
