9afb446a5f38d17f0708444928c21ee0f584b13b
cameronvoell <cameronvoell@users.noreply.github.com>
2024-03-04 09:59:21 -0800

    Latest cross builds and android test updated for sending from self while streaming

--- bindings_ffi/examples/MainActivity.kt
+++ bindings_ffi/examples/MainActivity.kt
@@ -15,14 +15,21 @@ import org.web3j.crypto.ECKeyPair
 import org.web3j.crypto.Sign
 import org.xmtp.android.library.Client
 import org.xmtp.android.library.ClientOptions
+import org.xmtp.android.library.Group
 import org.xmtp.android.library.XMTPEnvironment
 import org.xmtp.android.library.messages.PrivateKeyBuilder
 import org.xmtp.android.library.messages.toV2
 import uniffi.xmtpv3.FfiConversationCallback
 import uniffi.xmtpv3.FfiGroup
 import uniffi.xmtpv3.FfiInboxOwner
+import uniffi.xmtpv3.FfiListConversationsOptions
 import uniffi.xmtpv3.FfiLogger
+import uniffi.xmtpv3.FfiMessage
+import uniffi.xmtpv3.FfiMessageCallback
+import uniffi.xmtpv3.GroupPermissions
 import uniffi.xmtpv3.LegacyIdentitySource
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.DurationUnit
 
 const val EMULATOR_LOCALHOST_ADDRESS = "http://10.0.2.2:5556"
 const val DEV_NETWORK_ADDRESS = "https://dev.xmtp.network:5556"
@@ -57,6 +64,18 @@ class ConversationCallback : FfiConversationCallback {
     }
 }
 
+class MessageCallback : FfiMessageCallback {
+    override fun onMessage(message: FfiMessage) {
+        Log.i("App",
+            "INFO - Message callback with ID: " +
+                    toHexString(message.id) +
+                    ", from: " +
+                   message.addrFrom
+        )
+    }
+
+}
+
 // An example Android app testing the end-to-end flow through Rust
 // Run setup_android_example.sh to set it up
 class MainActivity : AppCompatActivity() {
@@ -70,39 +89,89 @@ class MainActivity : AppCompatActivity() {
             dbDir.deleteRecursively()
         } catch (e: Exception) {}
         dbDir.mkdir()
-        val dbPath: String = dbDir.absolutePath + "/android_example.db3"
-        val dbEncryptionKey = SecureRandom().generateSeed(32)
+        val alixDbPath: String = dbDir.absolutePath + "/android_example_alix.db3"
+        val boDbPath: String = dbDir.absolutePath + "/android_example_bo.db3"
+        val alixDbEncryptionKey = SecureRandom().generateSeed(32)
+        val boDbEncryptionKey = SecureRandom().generateSeed(32)
         Log.i(
                 "App",
                 "INFO -\nDB path: " +
-                        dbPath +
+                        alixDbPath +
                         "\nDB encryption key: " +
-                        dbEncryptionKey
+                        alixDbEncryptionKey
         )
 
         runBlocking {
             try {
-                val key = PrivateKeyBuilder()
-                val client =
+
+                // Create Alix Client
+                val alixKey = PrivateKeyBuilder()
+                val alix =
                         uniffi.xmtpv3.createClient(
                                 AndroidFfiLogger(),
                                 EMULATOR_LOCALHOST_ADDRESS,
                                 false,
-                                dbPath,
-                                dbEncryptionKey,
-                                key.address,
+                                alixDbPath,
+                                alixDbEncryptionKey,
+                                alixKey.address,
                                 LegacyIdentitySource.KEY_GENERATOR,
-                                getV2SerializedSignedPrivateKey(key),
+                                getV2SerializedSignedPrivateKey(alixKey),
                         )
-                var walletSignature: ByteArray? = null
-                val textToSign = client.textToSign()
-                if (textToSign != null) {
-                    walletSignature = key.sign(textToSign).toByteArray()
+                var alixWalletSignature: ByteArray? = null
+                val alixTextToSign = alix.textToSign()
+                if (alixTextToSign != null) {
+                    alixWalletSignature = alixKey.sign(alixTextToSign).toByteArray()
                 }
-                client.registerIdentity(walletSignature);
-                textView.text = "Libxmtp version\n" + uniffi.xmtpv3.getVersionInfo() + "\n\nClient constructed, wallet address: " + client.accountAddress()
+                alix.registerIdentity(alixWalletSignature);
+                textView.text = "Libxmtp version\n" + uniffi.xmtpv3.getVersionInfo() + "\n\nClient constructed, wallet address: " + alix.accountAddress()
+
+                // Create Bo Client
+                val boKey = PrivateKeyBuilder()
+                val bo =
+                    uniffi.xmtpv3.createClient(
+                        AndroidFfiLogger(),
+                        EMULATOR_LOCALHOST_ADDRESS,
+                        false,
+                        boDbPath,
+                        boDbEncryptionKey,
+                        boKey.address,
+                        LegacyIdentitySource.KEY_GENERATOR,
+                        getV2SerializedSignedPrivateKey(boKey),
+                    )
+                var boWalletSignature: ByteArray? = null
+                val boTextToSign = alix.textToSign()
+                if (boTextToSign != null) {
+                    boWalletSignature = boKey.sign(boTextToSign).toByteArray()
+                }
+                bo.registerIdentity(boWalletSignature);
+                textView.text = "Libxmtp version\n" + uniffi.xmtpv3.getVersionInfo() + "\n\nClient constructed, wallet address: " + bo.accountAddress()
+
+                var alixGroup = alix.conversations().createGroup(listOf(bo.accountAddress()), GroupPermissions.EVERYONE_IS_ADMIN)
+                bo.conversations().sync()
+
+                var boGroup = bo.conversations().list(
+                    opts = FfiListConversationsOptions(
+                        null,
+                        null,
+                        null
+                    )
+                ).first()
+                alixGroup.stream(MessageCallback())
+                val message = "hello"
+                val preparedMessage =
+                boGroup.send(message.encodeToByteArray())
+                boGroup.send(message.encodeToByteArray())
+                boGroup.send(message.encodeToByteArray())
+
+                alixGroup.send(message.encodeToByteArray())
+                alixGroup.send(message.encodeToByteArray())
+                alixGroup.send(message.encodeToByteArray())
+
+                boGroup.send(message.encodeToByteArray())
+                boGroup.send(message.encodeToByteArray())
+                boGroup.send(message.encodeToByteArray())
+
                 Log.i("App", "Setting up conversation streaming")
-                client.conversations().stream(ConversationCallback())
             } catch (e: Exception) {
                 textView.text = "Failed to construct client: " + e.message
             }
