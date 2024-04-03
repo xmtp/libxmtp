7e82d9806aa04d21bec3bf3fda8c6b1a25812e32
Alex Risch <alex.j.risch@gmail.com>
2024-04-03 15:21:43 -0600

    feat: Frames Client (#302)
    * chore: Update Protos
    
    Generate protos
    
    * chore: Update Protos
    
    Updates Protos
    Updated Read me for buf gen
    
    * feat: Frames Client
    
    Added frames client
    Need to do some verification first
    
    * feat: Frames Client
    
    Fixed handling
    Added test
    Fixed Lint
    
    * feat: Frames Client
    
    Fixed handling
    Added test
    Fixed Lint
    
    * feat: Frames Client
    
    Fixed handling
    Added test
    Fixed Lint
    
    * feat: Frames Client
    
    Fixed handling
    Added test
    Fixed Lint
    
    ---------
    
    Co-authored-by: Alex Risch <alexrisch@Alexs-MacBook-Pro-2.local>

---
+++ Sources/XMTPiOS/Frames/FramesConstants.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alex Risch on 3/28/24.
+//
+
+import Foundation
+
+let OPEN_FRAMES_PROXY_URL = "https://frames.xmtp.chat/"
+
+let PROTOCOL_VERSION = "2024-02-09"
