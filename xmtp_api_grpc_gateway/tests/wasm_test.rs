2dbd9ec78f9d4b7805977bb90a0d9d8bab0d111f
Ry Racherbaumer <ry@xmtp.com>
2024-04-10 13:20:48 -0700

    WIP

--- xmtp_api_grpc_gateway/tests/wasm_test.rs
+++ xmtp_api_grpc_gateway/tests/wasm_test.rs
@@ -1,7 +1,5 @@
-extern crate wasm_bindgen_test;
-extern crate xmtp_api_grpc_gateway;
-use wasm_bindgen::prelude::*;
 use wasm_bindgen_test::*;
+use xmtp_api_grpc::auth_token::Authenticator;
 use xmtp_api_grpc_gateway::XmtpGrpcGatewayClient;
 use xmtp_proto::api_client::XmtpApiClient;
 use xmtp_proto::xmtp::message_api::v1::{
@@ -11,19 +9,23 @@ use xmtp_proto::xmtp::message_api::v1::{
 // Only run these tests in a browser.
 wasm_bindgen_test_configure!(run_in_browser);
 
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
+const PRIVATE_KEY_BUNDLE_HEX: &str = "0a88030ac20108eec0888ae33112220a201cd19d1d6e129cb8f8ba4bd85aae10ffcc97a3de939d85f9bc378d47e6ba83711a940108eec0888ae33112460a440a40130cfb1cd667f48585f90372fe4b529da318e83221a3bfd1446ef6cf00d173543fed831d1517d310b05bd5ab138fde22af50a3ffce1aa72da8c7084e9bab0e4910011a430a4104c4eb77c3b2eaacaca12e2b55c6c42dc33f4518a5690bb49cd6ae0e0a652e59fbc9defd98242d30a0737a13c3461cac1edc0f8e3007d65b1637382088ac1cd3d712c00108a4c1888ae33112220a2062e553bceac5247e7bebfdcc8c31959965603e442f79c6346028060ab2129e931a920108a4c1888ae33112440a420a40d12c6ab6eb1874edd3044fdc753543516130bd4d1db11024bd81cd9c2c4bb6b6138e85ed313f387ea7707e09090659b580ee22f42f022c4521e4a11ab7abddfc1a430a4104175097c31bbe1700729f1f1ede87b8bd21a5bc62e4bb4c963e0de885080048bd31138b657fd9146aa8255f1c57c4fa1f8cb7b30bed8803eed48d6a3e67e71ccf";
+const WALLET_ADDRESS: &str = "0xA38A1f04B29dea1de621E17447fB4efB11BFfBdf";
+
+fn get_auth_token() -> String {
+    // This is a private key bundle exported from the JS SDK and hex encoded
+    let authenticator = Authenticator::from_hex(
+        PRIVATE_KEY_BUNDLE_HEX.to_string(),
+        WALLET_ADDRESS.to_string(),
+    );
+    authenticator.create_token()
 }
 
 #[wasm_bindgen_test]
 pub async fn test_query_publish_query() {
     let xmtp_url: String = "http://localhost:5555".to_string();
     let topic = uuid::Uuid::new_v4();
-    let auth_token = ""; // TODO
+    let auth_token = get_auth_token();
 
     let api = XmtpGrpcGatewayClient::new(xmtp_url);
     let q = QueryRequest {
@@ -37,7 +39,7 @@ pub async fn test_query_publish_query() {
 
     // But after we publish something...
     api.publish(
-        auth_token.to_string(),
+        auth_token,
         PublishRequest {
             envelopes: vec![Envelope {
                 content_topic: topic.to_string(),
@@ -63,7 +65,7 @@ pub async fn test_batch_query_publish_batch_query() {
     let api = XmtpGrpcGatewayClient::new(xmtp_url);
     let topic1 = uuid::Uuid::new_v4();
     let topic2 = uuid::Uuid::new_v4();
-    let auth_token = ""; // TODO
+    let auth_token = get_auth_token();
 
     // First we issue this batch query and get no results.
     let batch_q = BatchQueryRequest {
