381589b3c1a5577c868b3dcc42cafd7840a67dfd
tuddman <tuddman@users.noreply.github.com>
2024-04-30 16:26:05 +0200

    moves setup to message_history mod

--- xmtp_mls/src/groups/message_history.rs
+++ xmtp_mls/src/groups/message_history.rs
@@ -13,15 +13,26 @@ use xmtp_proto::{
 
 use super::GroupError;
 
-use crate::Client;
+use crate::client::ClientError;
+use crate::groups::StoredGroupMessage;
+use crate::storage::group::StoredGroup;
 use crate::storage::StorageError;
+use crate::Client;
 
 impl<'c, ApiClient> Client<ApiClient>
 where
     ApiClient: XmtpMlsClient + XmtpIdentityClient,
 {
-    pub fn allow_history_sync(&self) -> Result<(), StorageError> {
-        self.create_sync_group().map(|_| ())
+    pub async fn allow_history_sync(&self) -> Result<(), ClientError> {
+        let sync_group = self.create_sync_group()?;
+        let conn = self.store.conn()?;
+        let provider = sync_group.client.mls_provider(&conn);
+        sync_group
+            .add_missing_installations(provider)
+            .await
+            .map_err(|e| ClientError::Generic(e.to_string()))?;
+
+        Ok(())
     }
 
     #[allow(dead_code)]
@@ -48,6 +59,26 @@ where
             })),
         };
         Ok(())
+    }
+
+    #[allow(dead_code)]
+    pub(crate) async fn prepare_messages_to_send(
+        &self,
+    ) -> Result<Vec<StoredGroupMessage>, StorageError> {
+        println!("bundle_messages_to_send called");
+        let conn = self.store.conn()?;
+        let groups = conn.find_groups(None, None, None, None)?;
+        let mut all_messages: Vec<StoredGroupMessage> = vec![];
+
+        for StoredGroup { id, .. } in groups.clone() {
+            let messages = conn.get_group_messages(id, None, None, None, None, None)?;
+            println!("{:#?}", messages);
+            all_messages.extend(messages);
+        }
+
+        println!("groups: {:#?}", groups);
+        println!("# of group messages: {:?}", all_messages.len());
+        Ok(all_messages)
     }
 }
 
@@ -99,11 +130,20 @@ mod tests {
     use crate::utils::time::now_ns;
 
     #[tokio::test]
+    async fn test_allow_history_sync() {
+        let wallet = generate_local_wallet();
+        let client = ClientBuilder::new_test_client(&wallet).await;
+        assert!(client.allow_history_sync().await.is_ok());
+    }
+
+
+    
+    #[tokio::test]
     async fn test_send_mesage_history_request() {
         let wallet = generate_local_wallet();
         let client = ClientBuilder::new_test_client(&wallet).await;
         // calls create_sync_group() internally.
-        client.allow_history_sync().expect("create sync group");
+        client.allow_history_sync().await.expect("create sync group");
 
         let result = client.send_message_history_request().await;
         assert_ok!(result);
@@ -114,7 +154,7 @@ mod tests {
         let wallet = generate_local_wallet();
         let client = ClientBuilder::new_test_client(&wallet).await;
         // calls create_sync_group() internally.
-        client.allow_history_sync().expect("create sync group");
+        client.allow_history_sync().await.expect("create sync group");
 
         let request_id = new_request_id();
         let url = "https://test.com/abc-123";
@@ -137,5 +177,14 @@ mod tests {
         assert_ok!(add_members_result);
 
         let _ = amal_b.send_message_history_request().await;
+    }
+
+    #[tokio::test]
+    async fn test_prepare_messages_to_send() {
+        let wallet = generate_local_wallet();
+        let amal_a = ClientBuilder::new_test_client(&wallet).await;
+        // let group_a = amal_a.create_group(None);
+        let messages_result = amal_a.prepare_messages_to_send().await;
+        println!("{:?}", messages_result);
     }
 }
