b072c211b4d89712d2c3d80d7c435867632141be
yoduyodu <wang7ong@gmail.com>
2024-05-07 15:26:26 -0700

    remove redundant checks

--- xmtp_mls/src/identity/xmtp_id/identity.rs
+++ xmtp_mls/src/identity/xmtp_id/identity.rs
@@ -8,13 +8,12 @@ use openmls::{
 use openmls_basic_credential::SignatureKeyPair;
 use openmls_traits::types::CryptoError;
 use prost::Message;
-use rand::Rng;
 use sha2::{Digest, Sha512};
 use thiserror::Error;
 use xmtp_id::{
     associations::{
         builder::{SignatureRequest, SignatureRequestBuilder, SignatureRequestError},
-        InstallationKeySignature, LegacyDelegatedSignature, MemberIdentifier,
+        generate_inbox_id, InstallationKeySignature, LegacyDelegatedSignature, MemberIdentifier,
         RecoverableEcdsaSignature,
     },
     constants::INSTALLATION_KEY_SIGNATURE_CONTEXT,
@@ -73,18 +72,29 @@ impl Identity {
     }
 
     pub(crate) async fn create_to_be_signed<ApiClient: XmtpMlsClient + XmtpIdentityClient>(
-        inbox_id: InboxId,
-        account_address: String,
+        address: String,
         api_client: &ApiClientWrapper<ApiClient>,
     ) -> Result<Self, IdentityError> {
+        // check if address is already associated with an inbox_id, generate a new inbox_id if not.
+        let inbox_ids = api_client.get_inbox_ids(vec![address.clone()]).await?;
+        let mut is_new_inbox = false;
+        let mut nonce = 0;
+        let inbox_id;
+        if let Some(id) = inbox_ids.get(&address) {
+            inbox_id = id.clone();
+        } else {
+            is_new_inbox = true;
+            nonce = rand::random::<u64>();
+            inbox_id = generate_inbox_id(&address, &nonce);
+        }
+
         let signature_keys = SignatureKeyPair::new(CIPHERSUITE.signature_algorithm())?;
         let installation_public_key = signature_keys.public();
-        let member_identifier: MemberIdentifier = account_address.into();
+        let member_identifier: MemberIdentifier = address.into();
 
         let mut builder = SignatureRequestBuilder::new(inbox_id.clone());
-
-        if !Self::is_existing_inbox(inbox_id.clone(), api_client).await? {
-            builder = builder.create_inbox(member_identifier.clone(), rand::thread_rng().gen());
+        if is_new_inbox {
+            builder = builder.create_inbox(member_identifier.clone(), nonce);
         }
 
         let mut signature_request = builder
@@ -94,7 +104,7 @@ impl Identity {
         // We can pre-sign the request with an installation key signature, since we have access to the key
         signature_request
             .add_signature(Box::new(
-                Self::sign_with_installation_key(
+                sign_with_installation_key(
                     signature_request.signature_text(),
                     sized_installation_key(signature_keys.private())?,
                 )
@@ -105,35 +115,39 @@ impl Identity {
         let identity = Self {
             inbox_id: inbox_id.clone(),
             installation_keys: signature_keys,
-            credential: create_credential(inbox_id)?,
+            credential: create_credential(inbox_id.clone())?,
             signature_request: Some(signature_request),
         };
 
         Ok(identity)
     }
 
+    // Create new inbox using legacy key, will error if the address is already associated with an inbox
     pub(crate) async fn create_from_legacy<ApiClient: XmtpMlsClient + XmtpIdentityClient>(
-        inbox_id: InboxId,
-        account_address: String, // TODO: we can derive account_address from the private_key, it can be removed.
+        address: String,
         legacy_signed_private_key: Vec<u8>,
         api_client: &ApiClientWrapper<ApiClient>,
     ) -> Result<Self, IdentityError> {
-        if Self::is_existing_inbox(inbox_id.clone(), api_client).await? {
+        let inbox_ids = api_client.get_inbox_ids(vec![address.clone()]).await?;
+        if inbox_ids.contains_key(&address) {
             return Err(IdentityError::LegacyKeyReuse);
         }
+        let nonce = 0;
+        let inbox_id = generate_inbox_id(&address, &nonce);
+
         let signature_keys = SignatureKeyPair::new(CIPHERSUITE.signature_algorithm())?;
         let installation_public_key = signature_keys.public();
-        let member_identifier: MemberIdentifier = account_address.into();
+        let member_identifier: MemberIdentifier = address.into();
         let builder = SignatureRequestBuilder::new(inbox_id.clone());
         let mut signature_request = builder
-            .create_inbox(member_identifier.clone(), 0)
+            .create_inbox(member_identifier.clone(), nonce)
             .add_association(installation_public_key.to_vec().into(), member_identifier)
             .build();
 
         // We can pre-sign the request with an installation key signature, since we have access to the key
         signature_request
             .add_signature(Box::new(
-                Self::sign_with_installation_key(
+                sign_with_installation_key(
                     signature_request.signature_text(),
                     sized_installation_key(signature_keys.private())?,
                 )
@@ -143,7 +157,7 @@ impl Identity {
 
         signature_request
             .add_signature(Box::new(
-                Self::sign_with_legacy_key(
+                sign_with_legacy_key(
                     signature_request.signature_text(),
                     legacy_signed_private_key,
                 )
@@ -165,81 +179,65 @@ impl Identity {
         Ok(identity)
     }
 
-    async fn is_existing_inbox<ApiClient: XmtpMlsClient + XmtpIdentityClient>(
-        inbox_id: InboxId,
-        api_client: &ApiClientWrapper<ApiClient>,
-    ) -> Result<bool, IdentityError> {
-        let identity_updates = api_client
-            .get_identity_updates_v2(vec![GetIdentityUpdatesV2Filter {
-                inbox_id: inbox_id.to_string(),
-                sequence_id: None,
-            }])
-            .await?;
-
-        let inbox_updates = identity_updates.get(&inbox_id);
-
-        Ok(inbox_updates.is_some())
-    }
-
-    async fn sign_with_installation_key(
-        signature_text: String,
-        installation_private_key: &[u8; 32],
-    ) -> Result<InstallationKeySignature, IdentityError> {
-        let signing_key: SigningKey = SigningKey::from_bytes(installation_private_key);
-        let verifying_key = signing_key.verifying_key();
-        let mut prehashed: Sha512 = Sha512::new();
-        prehashed.update(signature_text.clone());
-        let sig = signing_key
-            .sign_prehashed(prehashed, Some(INSTALLATION_KEY_SIGNATURE_CONTEXT))
-            .unwrap();
-
-        let installation_key_sig = InstallationKeySignature::new(
-            signature_text.clone(),
-            sig.to_vec(),
-            verifying_key.as_bytes().to_vec(),
-        );
-
-        Ok(installation_key_sig)
-    }
-
     pub fn credential(&self) -> OpenMlsCredential {
         self.credential.clone()
     }
+}
 
-    async fn sign_with_legacy_key(
-        signature_text: String,
-        legacy_signed_private_key: Vec<u8>,
-    ) -> Result<LegacyDelegatedSignature, IdentityError> {
-        let legacy_signed_private_key_proto =
-            LegacySignedPrivateKeyProto::decode(legacy_signed_private_key.as_slice())?;
-        let signed_private_key::Union::Secp256k1(secp256k1) = legacy_signed_private_key_proto
-            .union
+async fn sign_with_installation_key(
+    signature_text: String,
+    installation_private_key: &[u8; 32],
+) -> Result<InstallationKeySignature, IdentityError> {
+    let signing_key: SigningKey = SigningKey::from_bytes(installation_private_key);
+    let verifying_key = signing_key.verifying_key();
+    let mut prehashed: Sha512 = Sha512::new();
+    prehashed.update(signature_text.clone());
+    let sig = signing_key
+        .sign_prehashed(prehashed, Some(INSTALLATION_KEY_SIGNATURE_CONTEXT))
+        .unwrap();
+
+    let installation_key_sig = InstallationKeySignature::new(
+        signature_text.clone(),
+        sig.to_vec(),
+        verifying_key.as_bytes().to_vec(),
+    );
+
+    Ok(installation_key_sig)
+}
+
+async fn sign_with_legacy_key(
+    signature_text: String,
+    legacy_signed_private_key: Vec<u8>,
+) -> Result<LegacyDelegatedSignature, IdentityError> {
+    let legacy_signed_private_key_proto =
+        LegacySignedPrivateKeyProto::decode(legacy_signed_private_key.as_slice())?;
+    let signed_private_key::Union::Secp256k1(secp256k1) = legacy_signed_private_key_proto
+        .union
+        .ok_or(IdentityError::MalformedLegacyKey(
+            "Missing secp256k1.union field".to_string(),
+        ))?;
+    let legacy_private_key = secp256k1.bytes;
+    let (mut delegating_signature, recovery_id) = k256_helper::sign_sha256(
+        &legacy_private_key, // secret_key
+        // TODO: Verify this will create a verifiable signature
+        signature_text.as_bytes(), // message
+    )
+    .map_err(IdentityError::LegacySignature)?;
+    delegating_signature.push(recovery_id); // TODO: normalize recovery ID if necessary
+
+    let legacy_signed_public_key_proto =
+        legacy_signed_private_key_proto
+            .public_key
             .ok_or(IdentityError::MalformedLegacyKey(
-                "Missing secp256k1.union field".to_string(),
+                "Missing public_key field".to_string(),
             ))?;
-        let legacy_private_key = secp256k1.bytes;
-        let (mut delegating_signature, recovery_id) = k256_helper::sign_sha256(
-            &legacy_private_key, // secret_key
-            // TODO: Verify this will create a verifiable signature
-            signature_text.as_bytes(), // message
-        )
-        .map_err(IdentityError::LegacySignature)?;
-        delegating_signature.push(recovery_id); // TODO: normalize recovery ID if necessary
 
-        let legacy_signed_public_key_proto =
-            legacy_signed_private_key_proto
-                .public_key
-                .ok_or(IdentityError::MalformedLegacyKey(
-                    "Missing public_key field".to_string(),
-                ))?;
+    let recoverable_sig = RecoverableEcdsaSignature::new(signature_text, delegating_signature);
 
-        let recoverable_sig = RecoverableEcdsaSignature::new(signature_text, delegating_signature);
-
-        Ok(LegacyDelegatedSignature::new(
-            recoverable_sig,
-            legacy_signed_public_key_proto,
-        ))
-    }
+    Ok(LegacyDelegatedSignature::new(
+        recoverable_sig,
+        legacy_signed_public_key_proto,
+    ))
 }
 
 #[allow(dead_code)]
--- xmtp_mls/src/identity/xmtp_id/mod.rs
+++ xmtp_mls/src/identity/xmtp_id/mod.rs
@@ -17,32 +17,9 @@ pub enum Member {
 }
 
 pub enum IdentityStrategy {
-    /// Tries to get an identity from the disk store, if not found creates an identity, will create an inbox if address isn't associated with one already.
-    ///
-    /// Platform SDK could have following logic before `IdentityStrategy.initialize_identity(api_client, store)`:
-    ///
-    /// inbox_id = libxmtp.get_inbox_id(wallet_address)
-    /// if !inbox_id {
-    ///     inbox_id = libxmtp.generate_inbox_id(wallet_address, 0)
-    ///     if use_legacy_key() {
-    ///         pass // We will create inbox & add association in one call in `IdentityStrategy.initialize_identity(api_client, store)`.
-    ///     } else {
-    ///         inbox_id = libxmtp.generate_inbox_id(wallet_address, random_nonce)
-    ///         libxmtp.create_new_inbox_with_user_signature(inbox_id, wallet_address)
-    ///         TODO: Similar to use_legacy_key(), we can include this in one call so that user doesn't have to sign twice.
-    ///     }
-    /// } else {
-    ///     // rare case
-    ///     if user_wants_additional_inbox() {
-    ///         inbox_id = libxmtp.generate_inbox_id(wallet_address, random_nonce)
-    ///         libxmtp.create_new_inbox_with_user_signature(inbox_id, wallet_address)
-    ///     }
-    /// }
-    ///
-    /// identity = IdentityStrategy.initialize_identity(api_client, store)
-    /// client = create_client(identity, ...)
-    ///
-    /// Note that if you want to associate the wallet with a different inbox_id than already associated, do that as a independent step before creating a client.
+    /// Tries to get an identity from the disk store. If not found, then it checks whether the address has an associated inbox and create one if not.
+    /// Finally use that inbox to create an identity.
+    /// Note that if you want to associate the wallet with a different inbox_id than already associated, do that separately.
     CreateIfNotFound(Member),
     /// Identity that is already in the disk store
     CachedOnly,
@@ -72,36 +49,11 @@ impl IdentityStrategy {
             }
             IdentityStrategy::CreateIfNotFound(member) => {
                 let identity = match member {
-                    Member::Address(address) => {
-                        let inbox_ids = api_client.get_inbox_ids(vec![address.clone()]).await?;
-                        let inbox_id = inbox_ids
-                            .get(&address)
-                            .ok_or(ClientBuilderError::UncoveredCase)?;
-                        if let Some(stored_identity) = stored_identity {
-                            if &stored_identity.clone().inbox_id != inbox_id {
-                                return Err(ClientBuilderError::StoredIdentityMismatch);
-                            } else {
-                                return Ok(stored_identity);
-                            }
-                        } else {
-                            Identity::create_to_be_signed(inbox_id.to_string(), address, api_client)
-                                .await?
-                        }
-                    }
+                    Member::Address(address) => stored_identity
+                        .unwrap_or(Identity::create_to_be_signed(address, api_client).await?),
 
-                    Member::LegacyKey(address, key) => {
-                        let inbox_ids = api_client.get_inbox_ids(vec![address.clone()]).await?;
-                        let inbox_id = inbox_ids
-                            .get(&address)
-                            .ok_or(ClientBuilderError::UncoveredCase)?;
-
-                        if let Some(stored_identity) = stored_identity {
-                            return Ok(stored_identity);
-                        } else {
-                            Identity::create_from_legacy(inbox_id.clone(), address, key, api_client)
-                                .await?
-                        }
-                    }
+                    Member::LegacyKey(address, key) => stored_identity
+                        .unwrap_or(Identity::create_from_legacy(address, key, api_client).await?),
                 };
 
                 Ok(identity)
