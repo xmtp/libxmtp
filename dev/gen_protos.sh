ed06d87b8ffb40704fe5580faa598a767cf0b99b
Andrew Plaza <github@andrewplaza.dev>
2024-05-08 16:37:38 -0400

    use proto main

--- dev/gen_protos.sh
+++ dev/gen_protos.sh
@@ -1,19 +1,13 @@
 pushd xmtp_proto > /dev/null
-if ! cargo install --list | grep "protoc-gen-prost-crate" > /dev/null; then
-    if ! cargo install protoc-gen-prost-crate; then
-        echo "Failed to install protoc-gen-prost-crate"
-        exit 1
-    fi
-fi
+#if ! cargo install --list | grep "protoc-gen-prost-crate" > /dev/null; then
+#   if ! cargo install protoc-gen-prost-crate; then
+#       echo "Failed to install protoc-gen-prost-crate"
+#       exit 1
+#   fi
+#fi
 
-if ! buf generate ~/Projects/xmtp/workspace-proto/insipx/validate-cred/proto; then
+if ! buf generate https://github.com/xmtp/proto.git#branch=main,subdir=proto; then
   echo "Failed to generate protobuf definitions"
   exit 1
 fi
-
-
-# if ! buf generate https://github.com/xmtp/proto.git#branch=insipx/validate-cred,subdir=proto; then
-#   echo "Failed to generate protobuf definitions"
-#   exit 1
-# fi
 popd > /dev/null
