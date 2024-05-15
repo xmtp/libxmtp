4c0c4c0b7a5aa929da18a98dc81eb8e135caa0bf
cameronvoell <cameronvoell@users.noreply.github.com>
2024-05-15 06:52:10 -0700

    lint fix after merge

--- xmtp_mls/src/groups/mod.rs
+++ xmtp_mls/src/groups/mod.rs
@@ -333,10 +333,8 @@ impl MlsGroup {
     ) -> Result<MlsGroup, GroupError> {
         let conn = context.store.conn()?;
         let provider = XmtpOpenMlsProvider::new(conn);
-        let protected_metadata = build_protected_metadata_extension(
-            &context.identity,
-            Purpose::Sync,
-        )?;
+        let protected_metadata =
+            build_protected_metadata_extension(&context.identity, Purpose::Sync)?;
         let mutable_metadata = build_mutable_metadata_extension_default(&context.identity)?;
         let group_membership = build_starting_group_membership_extension(
             context.inbox_id(),
