904a7ef3e1fd8fab339eaaf6dd86665e2006da7c
Andrew Plaza <github@andrewplaza.dev>
2024-05-20 17:31:36 -0400

    restore gen_protos

--- dev/gen_protos.sh
+++ dev/gen_protos.sh
@@ -1,12 +1,12 @@
 pushd xmtp_proto > /dev/null
-#if ! cargo install --list | grep "protoc-gen-prost-crate" > /dev/null; then
-#    if ! cargo install protoc-gen-prost-crate; then
-#       echo "Failed to install protoc-gen-prost-crate"
-#       exit 1
-#   fi
-#fi
+if ! cargo install --list | grep "protoc-gen-prost-crate" > /dev/null; then
+    if ! cargo install protoc-gen-prost-crate; then
+        echo "Failed to install protoc-gen-prost-crate"
+        exit 1
+    fi
+fi
 
-if ! buf generate https://github.com/xmtp/proto.git#branch=insipx/validation-cred,subdir=proto; then
+if ! buf generate https://github.com/xmtp/proto.git#branch=main,subdir=proto; then
     echo "Failed to generate protobuf definitions"
     exit 1
 fi
