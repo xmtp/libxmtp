c22b2ec51b67095988fcc8a60eb051f30e7afc9c
Ry Racherbaumer <ry@xmtp.com>
2024-06-11 12:07:17 -0500

    Run prettier

--- bindings_node/test/Client.test.ts
+++ bindings_node/test/Client.test.ts
@@ -1,33 +1,27 @@
-import {
-  createClient,
-  createRegisteredClient,
-  createUser,
-  TEST_API_URL,
-} from "@test/helpers";
-import { generateInboxId, getInboxIdForAddress } from "../dist/index";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from 'vitest'
+import { createClient, createRegisteredClient, createUser } from '@test/helpers'
 
-describe("Client", () => {
-  it("should not be registered at first", async () => {
-    const user = createUser();
-    const client = await createClient(user);
-    expect(client.isRegistered()).toBe(false);
-  });
+describe('Client', () => {
+  it('should not be registered at first', async () => {
+    const user = createUser()
+    const client = await createClient(user)
+    expect(client.isRegistered()).toBe(false)
+  })
 
-  it("should be registered aafter registration", async () => {
-    const user = createUser();
+  it('should be registered aafter registration', async () => {
+    const user = createUser()
     // must create 2 clients to get the expected value
     // this is currently a limitation in the rust implementation as the
     // underlying signature request does not mutate after registration
-    await createRegisteredClient(user);
-    const client = await createClient(user);
-    expect(client.isRegistered()).toBe(true);
-  });
+    await createRegisteredClient(user)
+    const client = await createClient(user)
+    expect(client.isRegistered()).toBe(true)
+  })
 
-  it("should be able to message registered identity", async () => {
-    const user = createUser();
-    const client = await createRegisteredClient(user);
-    const canMessage = await client.canMessage([user.account.address]);
-    expect(canMessage).toEqual({ [user.account.address.toLowerCase()]: true });
-  });
-});
+  it('should be able to message registered identity', async () => {
+    const user = createUser()
+    const client = await createRegisteredClient(user)
+    const canMessage = await client.canMessage([user.account.address])
+    expect(canMessage).toEqual({ [user.account.address.toLowerCase()]: true })
+  })
+})
--- bindings_node/test/helpers.ts
+++ bindings_node/test/helpers.ts
@@ -1,20 +1,20 @@
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
-import { createWalletClient, http, toBytes } from "viem";
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-import { sepolia } from "viem/chains";
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createWalletClient, http, toBytes } from 'viem'
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts'
+import { sepolia } from 'viem/chains'
 import {
   createClient as create,
   generateInboxId,
   getInboxIdForAddress,
-} from "../dist/index";
+} from '../dist/index'
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-export const TEST_API_URL = "http://localhost:5556";
+const __dirname = dirname(fileURLToPath(import.meta.url))
+export const TEST_API_URL = 'http://localhost:5556'
 
 export const createUser = () => {
-  const key = generatePrivateKey();
-  const account = privateKeyToAccount(key);
+  const key = generatePrivateKey()
+  const account = privateKeyToAccount(key)
   return {
     key,
     account,
@@ -23,30 +23,30 @@ export const createUser = () => {
       chain: sepolia,
       transport: http(),
     }),
-  };
-};
+  }
+}
 
-export type User = ReturnType<typeof createUser>;
+export type User = ReturnType<typeof createUser>
 
 export const createClient = async (user: User) => {
-  const dbPath = join(__dirname, `${user.account.address}.db3`);
+  const dbPath = join(__dirname, `${user.account.address}.db3`)
   const inboxId =
     (await getInboxIdForAddress(TEST_API_URL, false, user.account.address)) ||
-    generateInboxId(user.account.address);
-  return create(TEST_API_URL, false, dbPath, inboxId, user.account.address);
-};
+    generateInboxId(user.account.address)
+  return create(TEST_API_URL, false, dbPath, inboxId, user.account.address)
+}
 
 export const createRegisteredClient = async (user: User) => {
-  const client = await createClient(user);
+  const client = await createClient(user)
   if (!client.isRegistered()) {
-    const signatureText = client.signatureText();
+    const signatureText = client.signatureText()
     if (signatureText) {
       const signature = await user.wallet.signMessage({
         message: signatureText,
-      });
-      client.addEcdsaSignature(toBytes(signature));
+      })
+      client.addEcdsaSignature(toBytes(signature))
     }
-    await client.registerIdentity();
+    await client.registerIdentity()
   }
-  return client;
-};
+  return client
+}
