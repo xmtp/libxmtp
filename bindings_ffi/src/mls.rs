8d82f67301cd21896144eec2d149ecaad27bc7d7
cameronvoell <cameronvoell@users.noreply.github.com>
2024-06-24 16:41:49 -0700

    preconfigured policies match after new metadata fields added, and custom policies don't throw error

--- bindings_ffi/src/mls.rs
+++ bindings_ffi/src/mls.rs
@@ -313,6 +313,7 @@ pub struct FfiConversations {
 pub enum GroupPermissions {
     AllMembers,
     AdminOnly,
+    CustomPolicy,
 }
 
 impl From<PreconfiguredPolicies> for GroupPermissions {
@@ -1009,7 +1010,11 @@ pub struct FfiGroupPermissions {
 #[uniffi::export]
 impl FfiGroupPermissions {
     pub fn policy_type(&self) -> Result<GroupPermissions, GenericError> {
-        Ok(self.inner.preconfigured_policy()?.into())
+        if let Ok(preconfigured_policy) = self.inner.preconfigured_policy() {
+            Ok(preconfigured_policy.into())
+        } else {
+            Ok(GroupPermissions::CustomPolicy)
+        }
     }
 }
 
