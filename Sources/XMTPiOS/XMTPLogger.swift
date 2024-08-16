8c5a3980c613eb0b78c121aaeed913643bb7132a
Cameron Voell <1103838+cameronvoell@users.noreply.github.com>
2024-08-16 08:34:49 -0700

    fix: show libxmtp logs message on iOS (#387)
    * fix: show libxmtp logs message on iOS
    
    * update podspec
    
    ---------
    
    Co-authored-by: cameronvoell <cameronvoell@users.noreply.github.com>

--- Sources/XMTPiOS/XMTPLogger.swift
+++ Sources/XMTPiOS/XMTPLogger.swift
@@ -15,13 +15,13 @@ class XMTPLogger: FfiLogger {
 	func log(level: UInt32, levelLabel: String, message: String) {
 		switch level {
 		case 1:
-			logger.error("libxmtp[\(levelLabel, privacy: .public)] - \(message)")
+			logger.error("libxmtp[\(levelLabel, privacy: .public)] - \(message, privacy: .public)")
 		case 2, 3:
-			logger.info("libxmtp[\(levelLabel, privacy: .public)] - \(message)")
+			logger.info("libxmtp[\(levelLabel, privacy: .public)] - \(message, privacy: .public)")
 		case 4:
-			logger.debug("libxmtp[\(levelLabel, privacy: .public)] - \(message)")
+			logger.debug("libxmtp[\(levelLabel, privacy: .public)] - \(message, privacy: .public)")
 		case 5:
-			logger.trace("libxmtp[\(levelLabel, privacy: .public)] - \(message)")
+			logger.trace("libxmtp[\(levelLabel, privacy: .public)] - \(message, privacy: .public)")
 		default:
 			print("libxmtp[\(levelLabel)] - \(message)")
 		}
