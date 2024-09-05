d82da9f7170bad9745af9c77a493dff983173bfe
Andrew Plaza <github@andrewplaza.dev>
2024-09-05 13:53:39 -0400

    fix compile for bindings_wasm

--- bindings_wasm/src/mls_client.rs
+++ bindings_wasm/src/mls_client.rs
@@ -6,8 +6,11 @@ use wasm_bindgen::JsValue;
 use xmtp_api_http::XmtpHttpApiClient;
 use xmtp_cryptography::signature::ed25519_public_key_to_address;
 use xmtp_id::associations::generate_inbox_id as xmtp_id_generate_inbox_id;
-use xmtp_id::associations::{
-  AccountId, MemberIdentifier, RecoverableEcdsaSignature, Signature, SmartContractWalletSignature,
+use xmtp_id::{
+  associations::{
+    AccountId, MemberIdentifier, RecoverableEcdsaSignature, SmartContractWalletSignature,
+  },
+  GenericSignature,
 };
 use xmtp_mls::api::ApiClientWrapper;
 use xmtp_mls::builder::ClientBuilder;
