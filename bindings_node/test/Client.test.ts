a08b539326d3714a408fd11aa68335be2f23fdea
Ry Racherbaumer <ry@xmtp.com>
2024-09-13 16:27:27 -0500

    Add tests for adding/revoking wallets/installations

--- bindings_node/test/Client.test.ts
+++ bindings_node/test/Client.test.ts
@@ -1,5 +1,8 @@
+import { v4 } from 'uuid'
+import { toBytes } from 'viem'
 import { describe, expect, it } from 'vitest'
 import { createClient, createRegisteredClient, createUser } from '@test/helpers'
+import { NapiSignatureRequestType } from '../dist'
 
 describe('Client', () => {
   it('should not be registered at first', async () => {
@@ -42,5 +45,123 @@ describe('Client', () => {
       user.account.address.toLowerCase(),
     ])
     expect(inboxState.recoveryAddress).toBe(user.account.address.toLowerCase())
+  })
+
+  it('should add a wallet association to the client', async () => {
+    const user = createUser()
+    const user2 = createUser()
+    const client = await createRegisteredClient(user)
+    const signatureText = await client.addWalletSignatureText(
+      user.account.address,
+      user2.account.address
+    )
+    expect(signatureText).toBeDefined()
+
+    // sign message
+    const signature = await user.wallet.signMessage({
+      message: signatureText,
+    })
+    const signature2 = await user2.wallet.signMessage({
+      message: signatureText,
+    })
+
+    await client.addSignature(
+      NapiSignatureRequestType.AddWallet,
+      toBytes(signature)
+    )
+    await client.addSignature(
+      NapiSignatureRequestType.AddWallet,
+      toBytes(signature2)
+    )
+    await client.applySignatureRequests()
+    const inboxState = await client.inboxState(false)
+    expect(inboxState.accountAddresses.length).toEqual(2)
+    expect(inboxState.accountAddresses).toContain(
+      user.account.address.toLowerCase()
+    )
+    expect(inboxState.accountAddresses).toContain(
+      user2.account.address.toLowerCase()
+    )
+  })
+
+  it('should revoke a wallet association from the client', async () => {
+    const user = createUser()
+    const user2 = createUser()
+    const client = await createRegisteredClient(user)
+    const signatureText = await client.addWalletSignatureText(
+      user.account.address,
+      user2.account.address
+    )
+    expect(signatureText).toBeDefined()
+
+    // sign message
+    const signature = await user.wallet.signMessage({
+      message: signatureText,
+    })
+    const signature2 = await user2.wallet.signMessage({
+      message: signatureText,
+    })
+
+    await client.addSignature(
+      NapiSignatureRequestType.AddWallet,
+      toBytes(signature)
+    )
+    await client.addSignature(
+      NapiSignatureRequestType.AddWallet,
+      toBytes(signature2)
+    )
+    await client.applySignatureRequests()
+
+    const signatureText2 = await client.revokeWalletSignatureText(
+      user2.account.address
+    )
+    expect(signatureText2).toBeDefined()
+
+    // sign message
+    const signature3 = await user.wallet.signMessage({
+      message: signatureText2,
+    })
+
+    await client.addSignature(
+      NapiSignatureRequestType.RevokeWallet,
+      toBytes(signature3)
+    )
+    await client.applySignatureRequests()
+    const inboxState = await client.inboxState(false)
+    expect(inboxState.accountAddresses).toEqual([
+      user.account.address.toLowerCase(),
+    ])
+  })
+
+  it('should revoke all installations', async () => {
+    const user = createUser()
+
+    const client = await createRegisteredClient(user)
+    user.uuid = v4()
+    const client2 = await createRegisteredClient(user)
+    user.uuid = v4()
+    const client3 = await createRegisteredClient(user)
+
+    const inboxState = await client3.inboxState(true)
+    expect(inboxState.installationIds.length).toEqual(3)
+    expect(inboxState.installationIds).toContain(client.installationId())
+    expect(inboxState.installationIds).toContain(client2.installationId())
+    expect(inboxState.installationIds).toContain(client3.installationId())
+
+    const signatureText = await client3.revokeInstallationsSignatureText()
+    expect(signatureText).toBeDefined()
+
+    // sign message
+    const signature = await user.wallet.signMessage({
+      message: signatureText,
+    })
+
+    await client3.addSignature(
+      NapiSignatureRequestType.RevokeInstallations,
+      toBytes(signature)
+    )
+    await client3.applySignatureRequests()
+    const inboxState2 = await client3.inboxState(true)
+    expect(inboxState2.installationIds).toEqual([client3.installationId()])
   })
 })
