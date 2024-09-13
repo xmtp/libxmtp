fcaed1a3babcaaee8385923631af4a7c15033942
Ry Racherbaumer <ry@xmtp.com>
2024-09-13 15:24:15 -0500

    Refactor management of signature requests

--- bindings_node/src/mls_client.rs
+++ bindings_node/src/mls_client.rs
@@ -1,14 +1,15 @@
 use crate::conversations::NapiConversations;
-use napi::bindgen_prelude::{BigInt, Error, Result, Uint8Array};
+use napi::bindgen_prelude::{Error, Result, Uint8Array};
 use napi_derive::napi;
 use std::collections::HashMap;
 use std::ops::Deref;
 use std::sync::Arc;
+use tokio::sync::Mutex;
 pub use xmtp_api_grpc::grpc_api_helper::Client as TonicApiClient;
 use xmtp_cryptography::signature::ed25519_public_key_to_address;
+use xmtp_id::associations::builder::SignatureRequest;
 use xmtp_id::associations::unverified::UnverifiedSignature;
 use xmtp_id::associations::{generate_inbox_id as xmtp_id_generate_inbox_id, AssociationState};
-use xmtp_id::associations::{AccountId, MemberIdentifier};
 use xmtp_mls::api::ApiClientWrapper;
 use xmtp_mls::builder::ClientBuilder;
 use xmtp_mls::identity::IdentityStrategy;
@@ -17,6 +18,15 @@ use xmtp_mls::storage::{EncryptedMessageStore, EncryptionKey, StorageOption};
 use xmtp_mls::Client as MlsClient;
 
 pub type RustXmtpClient = MlsClient<TonicApiClient>;
+
+#[napi]
+#[derive(Eq, Hash, PartialEq)]
+pub enum NapiSignatureRequestType {
+  AddWallet,
+  CreateInbox,
+  RevokeWallet,
+  RevokeInstallations,
+}
 
 #[napi(object)]
 pub struct NapiInboxState {
@@ -44,7 +54,7 @@ impl From<AssociationState> for NapiInboxState {
 #[napi]
 pub struct NapiClient {
   inner_client: Arc<RustXmtpClient>,
-  signatures: HashMap<MemberIdentifier, UnverifiedSignature>,
+  signature_requests: Arc<Mutex<HashMap<NapiSignatureRequestType, SignatureRequest>>>,
   pub account_address: String,
 }
 
@@ -104,7 +114,7 @@ pub async fn create_client(
   Ok(NapiClient {
     inner_client: Arc::new(xmtp_client),
     account_address,
-    signatures: HashMap::new(),
+    signature_requests: Arc::new(Mutex::new(HashMap::new())),
   })
 }
 
@@ -167,58 +177,6 @@ impl NapiClient {
   }
 
   #[napi]
-  pub fn add_ecdsa_signature(&mut self, signature_bytes: Uint8Array) -> Result<()> {
-    if self.is_registered() {
-      return Err(Error::from_reason(
-        "An identity is already registered with this client",
-      ));
-    }
-
-    let signature = UnverifiedSignature::new_recoverable_ecdsa(signature_bytes.deref().to_vec());
-
-    self.signatures.insert(
-      MemberIdentifier::Address(self.account_address.clone().to_lowercase()),
-      signature,
-    );
-
-    Ok(())
-  }
-
-  #[napi]
-  pub fn add_scw_signature(
-    &mut self,
-    signature_bytes: Uint8Array,
-    chain_id: BigInt,
-    account_address: String,
-    // TODO:nm remove this
-    _chain_rpc_url: String,
-    block_number: BigInt,
-  ) -> Result<()> {
-    if self.is_registered() {
-      return Err(Error::from_reason(
-        "An identity is already registered with this client",
-      ));
-    }
-
-    let (_, chain_id_u64, _) = chain_id.get_u64();
-
-    let account_id = AccountId::new_evm(chain_id_u64, account_address.clone());
-
-    let signature = UnverifiedSignature::new_smart_contract_wallet(
-      signature_bytes.deref().to_vec(),
-      account_id,
-      block_number.get_u64().1,
-    );
-
-    self.signatures.insert(
-      MemberIdentifier::Address(account_address.clone().to_lowercase()),
-      signature,
-    );
-
-    Ok(())
-  }
-
-  #[napi]
   pub async fn register_identity(&self) -> Result<()> {
     if self.is_registered() {
       return Err(Error::from_reason(
@@ -226,48 +184,36 @@ impl NapiClient {
       ));
     }
 
-    if self.signatures.is_empty() {
-      return Err(Error::from_reason(
-        "No client signatures found, add at least 1 before registering",
-      ));
-    }
+    let mut signature_requests = self.signature_requests.lock().await;
 
-    let mut signature_request = match self.inner_client.identity().signature_request() {
-      Some(signature_req) => signature_req,
-      // this should never happen since we're checking for it above in is_registered
-      None => return Err(Error::from_reason("No signature request found")),
-    };
-
-    // apply added signatures to the signature request
-    for signature in self.signatures.values() {
-      signature_request
-        .add_signature(
-          signature.clone(),
-          self
-            .inner_client
-            .smart_contract_signature_verifier()
-            .as_ref(),
-        )
-        .await
-        .map_err(|e| Error::from_reason(format!("{}", e)))?;
-    }
+    let signature_request = signature_requests
+      .get(&NapiSignatureRequestType::CreateInbox)
+      .ok_or(Error::from_reason("No signature request found"))?;
 
     self
       .inner_client
-      .register_identity(signature_request)
+      .register_identity(signature_request.clone())
       .await
       .map_err(|e| Error::from_reason(format!("{}", e)))?;
+
+    signature_requests.remove(&NapiSignatureRequestType::CreateInbox);
 
     Ok(())
   }
 
   #[napi]
-  pub fn signature_text(&self) -> Option<String> {
-    self
-      .inner_client
-      .identity()
-      .signature_request()
-      .map(|signature_req| signature_req.signature_text())
+  pub async fn create_inbox_signature_text(&self) -> Result<Option<String>> {
+    let signature_request = match self.inner_client.identity().signature_request() {
+      Some(signature_req) => signature_req,
+      // this should never happen since we're checking for it above in is_registered
+      None => return Err(Error::from_reason("No signature request found")),
+    };
+    let signature_text = signature_request.signature_text();
+    let mut signature_requests = self.signature_requests.lock().await;
+
+    signature_requests.insert(NapiSignatureRequestType::CreateInbox, signature_request);
+
+    Ok(Some(signature_text))
   }
 
   #[napi]
@@ -311,5 +257,120 @@ impl NapiClient {
       .await
       .map_err(|e| Error::from_reason(format!("{}", e)))?;
     Ok(state.into())
+  }
+
+  #[napi]
+  pub async fn add_wallet_signature_text(
+    &self,
+    existing_wallet_address: String,
+    new_wallet_address: String,
+  ) -> Result<String> {
+    let signature_request = self
+      .inner_client
+      .associate_wallet(existing_wallet_address.into(), new_wallet_address.into())
+      .map_err(|e| Error::from_reason(format!("{}", e)))?;
+    let signature_text = signature_request.signature_text();
+    let mut signature_requests = self.signature_requests.lock().await;
+
+    signature_requests.insert(NapiSignatureRequestType::AddWallet, signature_request);
+
+    Ok(signature_text)
+  }
+
+  #[napi]
+  pub async fn revoke_wallet_signature_text(&self, wallet_address: String) -> Result<String> {
+    let signature_request = self
+      .inner_client
+      .revoke_wallets(vec![wallet_address.into()])
+      .await
+      .map_err(|e| Error::from_reason(format!("{}", e)))?;
+    let signature_text = signature_request.signature_text();
+    let mut signature_requests = self.signature_requests.lock().await;
+
+    signature_requests.insert(NapiSignatureRequestType::RevokeWallet, signature_request);
+
+    Ok(signature_text)
+  }
+
+  #[napi]
+  pub async fn revoke_installations_signature_text(&self) -> Result<String> {
+    let installation_id = self.inner_client.installation_public_key();
+    let inbox_state = self
+      .inner_client
+      .inbox_state(true)
+      .await
+      .map_err(|e| Error::from_reason(format!("{}", e)))?;
+    let other_installation_ids = inbox_state
+      .installation_ids()
+      .into_iter()
+      .filter(|id| id != &installation_id)
+      .collect();
+    let signature_request = self
+      .inner_client
+      .revoke_installations(other_installation_ids)
+      .await
+      .map_err(|e| Error::from_reason(format!("{}", e)))?;
+    let signature_text = signature_request.signature_text();
+    let mut signature_requests = self.signature_requests.lock().await;
+
+    signature_requests.insert(
+      NapiSignatureRequestType::RevokeInstallations,
+      signature_request,
+    );
+
+    Ok(signature_text)
+  }
+
+  #[napi]
+  pub async fn add_signature(
+    &self,
+    signature_type: NapiSignatureRequestType,
+    signature_bytes: Uint8Array,
+  ) -> Result<()> {
+    let mut signature_requests = self.signature_requests.lock().await;
+
+    let signature_request = signature_requests
+      .get_mut(&signature_type)
+      .ok_or(Error::from_reason("Signature request not found"))?;
+    let signature = UnverifiedSignature::new_recoverable_ecdsa(signature_bytes.deref().to_vec());
+
+    signature_request
+      .add_signature(
+        signature,
+        self
+          .inner_client
+          .smart_contract_signature_verifier()
+          .as_ref(),
+      )
+      .await
+      .map_err(|e| Error::from_reason(format!("{}", e)))?;
+
+    Ok(())
+  }
+
+  #[napi]
+  pub async fn apply_signature_requests(&self) -> Result<()> {
+    let mut signature_requests = self.signature_requests.lock().await;
+
+    let request_types: Vec<NapiSignatureRequestType> = signature_requests.keys().cloned().collect();
+    for signature_request_type in request_types {
+      // ignore the create inbox request since it's applied with register_identity
+      if signature_request_type == NapiSignatureRequestType::CreateInbox {
+        continue;
+      }
+
+      if let Some(signature_request) = signature_requests.get(&signature_request_type) {
+        self
+          .inner_client
+          .apply_signature_request(signature_request.clone())
+          .await
+          .map_err(|e| Error::from_reason(format!("{}", e)))?;
+
+        // remove the signature request after applying it
+        signature_requests.remove(&signature_request_type);
+      }
+    }
+
+    Ok(())
   }
 }
