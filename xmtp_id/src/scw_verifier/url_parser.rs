0fdde3869ecfa6a60dea9ffc94dd04361e559f24
Dakota Brink <git@kota.is>
2024-09-24 12:44:02 -0500

    explain unwraps

--- xmtp_id/src/scw_verifier/url_parser.rs
+++ xmtp_id/src/scw_verifier/url_parser.rs
@@ -2,10 +2,16 @@ use std::{collections::HashMap, fs, str::FromStr};
 use url::Url;
 
 pub fn parse_chain_urls() -> HashMap<u64, Url> {
-    let json = fs::read_to_string("chain_urls.json").unwrap();
-    let json: HashMap<u64, String> = serde_json::from_str(&json).unwrap();
+    let json = fs::read_to_string("chain_urls.json").expect("chain_urls.json is missing");
+    let json: HashMap<u64, String> =
+        serde_json::from_str(&json).expect("chain_urls.json is malformatted");
 
     json.into_iter()
-        .map(|(id, url)| (id, Url::from_str(&url).unwrap()))
+        .map(|(id, url)| {
+            (
+                id,
+                Url::from_str(&url).expect("unable to parse url in chain_urls.json"),
+            )
+        })
         .collect()
 }
