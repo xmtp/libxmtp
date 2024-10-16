38a0cfaf60ef9f24ce191a95fc5e622b7d82d145
Dakota Brink <git@kota.is>
2024-10-16 10:41:54 -0400

    foundry -> anvil

--- xmtp_id/src/scw_verifier/mod.rs
+++ xmtp_id/src/scw_verifier/mod.rs
@@ -130,8 +130,8 @@ impl MultiSmartContractSignatureVerifier {
             };
         });
 
-        if let Ok(url) = env::var("FOUNDRY_URL") {
-            info!("Adding foundry to the verifiers: {url}");
+        if let Ok(url) = env::var("ANVIL_URL") {
+            info!("Adding anvil to the verifiers: {url}");
             self.verifiers.insert(
                 "eip155:31337".to_string(),
                 Box::new(RpcSmartContractWalletVerifier::new(url)),
