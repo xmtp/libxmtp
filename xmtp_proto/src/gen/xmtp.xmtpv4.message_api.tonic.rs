53768229daccb5de21fa7faee7d3b58056dd3abf
Martin Kysel <code@martinkysel.com>
2024-10-21 11:15:42 -0400

    Fix Proto vs WASM

--- xmtp_proto/src/gen/xmtp.xmtpv4.message_api.tonic.rs
+++ xmtp_proto/src/gen/xmtp.xmtpv4.message_api.tonic.rs
@@ -1,5 +1,6 @@
 // @generated
 /// Generated client implementations.
+#[cfg(not(target_arch = "wasm32"))]
 pub mod replication_api_client {
     #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
     use tonic::codegen::*;
@@ -217,6 +218,7 @@ pub mod replication_api_client {
     }
 }
 /// Generated server implementations.
+#[cfg(not(target_arch = "wasm32"))]
 pub mod replication_api_server {
     #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
     use tonic::codegen::*;
