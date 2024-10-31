8b1dcb035de6c5c884f555c5558937947c070ab1
Ry Racherbaumer <rygine@users.noreply.github.com>
2024-10-31 12:39:10 -0500

    Add 1:1 groups to WASM bindings (#1203)
    * Add client and signature requests accessors
    
    * Move consent state methods into consent_state mod
    
    * Move signature methods into separate signatures mod
    
    * Move inbox state methods into inbox_state mod
    
    * Add 1:1 messages
    
    * Clippy fix

--- bindings_wasm/src/mls_client.rs
+++ bindings_wasm/src/mls_client.rs
@@ -7,34 +7,34 @@ use wasm_bindgen::JsValue;
 use xmtp_api_http::XmtpHttpApiClient;
 use xmtp_cryptography::signature::ed25519_public_key_to_address;
 use xmtp_id::associations::builder::SignatureRequest;
-use xmtp_id::associations::unverified::UnverifiedSignature;
 use xmtp_mls::builder::ClientBuilder;
 use xmtp_mls::groups::scoped_client::LocalScopedGroupClient;
 use xmtp_mls::identity::IdentityStrategy;
-use xmtp_mls::storage::consent_record::StoredConsentRecord;
 use xmtp_mls::storage::{EncryptedMessageStore, EncryptionKey, StorageOption};
 use xmtp_mls::Client as MlsClient;
 
-use crate::consent_state::{WasmConsent, WasmConsentEntityType, WasmConsentState};
 use crate::conversations::WasmConversations;
-use crate::inbox_state::WasmInboxState;
+use crate::signatures::WasmSignatureRequestType;
 
 pub type RustXmtpClient = MlsClient<XmtpHttpApiClient>;
-
-#[wasm_bindgen]
-#[derive(Clone, Eq, Hash, PartialEq)]
-pub enum WasmSignatureRequestType {
-  AddWallet,
-  CreateInbox,
-  RevokeWallet,
-  RevokeInstallations,
-}
 
 #[wasm_bindgen]
 pub struct WasmClient {
   account_address: String,
   inner_client: Arc<RustXmtpClient>,
   signature_requests: Arc<Mutex<HashMap<WasmSignatureRequestType, SignatureRequest>>>,
+}
+
+impl WasmClient {
+  pub fn inner_client(&self) -> &Arc<RustXmtpClient> {
+    &self.inner_client
+  }
+
+  pub fn signature_requests(
+    &self,
+  ) -> &Arc<Mutex<HashMap<WasmSignatureRequestType, SignatureRequest>>> {
+    &self.signature_requests
+  }
 }
 
 #[wasm_bindgen(js_name = createClient)]
@@ -155,137 +155,6 @@ impl WasmClient {
     Ok(())
   }
 
-  #[wasm_bindgen(js_name = createInboxSignatureText)]
-  pub async fn create_inbox_signature_text(&self) -> Result<Option<String>, JsError> {
-    let signature_request = match self.inner_client.identity().signature_request() {
-      Some(signature_req) => signature_req,
-      // this should never happen since we're checking for it above in is_registered
-      None => return Err(JsError::new("No signature request found")),
-    };
-    let signature_text = signature_request.signature_text();
-    let mut signature_requests = self.signature_requests.lock().await;
-
-    signature_requests.insert(WasmSignatureRequestType::CreateInbox, signature_request);
-
-    Ok(Some(signature_text))
-  }
-
-  #[wasm_bindgen(js_name = addWalletSignatureText)]
-  pub async fn add_wallet_signature_text(
-    &self,
-    existing_wallet_address: String,
-    new_wallet_address: String,
-  ) -> Result<String, JsError> {
-    let signature_request = self
-      .inner_client
-      .associate_wallet(
-        existing_wallet_address.to_lowercase(),
-        new_wallet_address.to_lowercase(),
-      )
-      .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-    let signature_text = signature_request.signature_text();
-    let mut signature_requests = self.signature_requests.lock().await;
-
-    signature_requests.insert(WasmSignatureRequestType::AddWallet, signature_request);
-
-    Ok(signature_text)
-  }
-
-  #[wasm_bindgen(js_name = revokeWalletSignatureText)]
-  pub async fn revoke_wallet_signature_text(
-    &self,
-    wallet_address: String,
-  ) -> Result<String, JsError> {
-    let signature_request = self
-      .inner_client
-      .revoke_wallets(vec![wallet_address.to_lowercase()])
-      .await
-      .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-    let signature_text = signature_request.signature_text();
-    let mut signature_requests = self.signature_requests.lock().await;
-
-    signature_requests.insert(WasmSignatureRequestType::RevokeWallet, signature_request);
-
-    Ok(signature_text)
-  }
-
-  #[wasm_bindgen(js_name = revokeInstallationsSignatureText)]
-  pub async fn revoke_installations_signature_text(&self) -> Result<String, JsError> {
-    let installation_id = self.inner_client.installation_public_key();
-    let inbox_state = self
-      .inner_client
-      .inbox_state(true)
-      .await
-      .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-    let other_installation_ids = inbox_state
-      .installation_ids()
-      .into_iter()
-      .filter(|id| id != &installation_id)
-      .collect();
-    let signature_request = self
-      .inner_client
-      .revoke_installations(other_installation_ids)
-      .await
-      .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-    let signature_text = signature_request.signature_text();
-    let mut signature_requests = self.signature_requests.lock().await;
-
-    signature_requests.insert(
-      WasmSignatureRequestType::RevokeInstallations,
-      signature_request,
-    );
-
-    Ok(signature_text)
-  }
-
-  #[wasm_bindgen(js_name = addSignature)]
-  pub async fn add_signature(
-    &self,
-    signature_type: WasmSignatureRequestType,
-    signature_bytes: Uint8Array,
-  ) -> Result<(), JsError> {
-    let mut signature_requests = self.signature_requests.lock().await;
-
-    if let Some(signature_request) = signature_requests.get_mut(&signature_type) {
-      let signature = UnverifiedSignature::new_recoverable_ecdsa(signature_bytes.to_vec());
-
-      signature_request
-        .add_signature(signature, self.inner_client.scw_verifier())
-        .await
-        .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-    } else {
-      return Err(JsError::new("Signature request not found"));
-    }
-
-    Ok(())
-  }
-
-  #[wasm_bindgen(js_name = applySignatureRequests)]
-  pub async fn apply_signature_requests(&self) -> Result<(), JsError> {
-    let mut signature_requests = self.signature_requests.lock().await;
-
-    let request_types: Vec<WasmSignatureRequestType> = signature_requests.keys().cloned().collect();
-    for signature_request_type in request_types {
-      // ignore the create inbox request since it's applied with register_identity
-      if signature_request_type == WasmSignatureRequestType::CreateInbox {
-        continue;
-      }
-
-      if let Some(signature_request) = signature_requests.get(&signature_request_type) {
-        self
-          .inner_client
-          .apply_signature_request(signature_request.clone())
-          .await
-          .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-
-        // remove the signature request after applying it
-        signature_requests.remove(&signature_request_type);
-      }
-    }
-
-    Ok(())
-  }
-
   #[wasm_bindgen(js_name = requestHistorySync)]
   pub async fn request_history_sync(&self) -> Result<(), JsError> {
     let provider = self
@@ -310,65 +179,6 @@ impl WasmClient {
       .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
 
     Ok(inbox_id)
-  }
-
-  /**
-   * Get the client's inbox state.
-   *
-   * If `refresh_from_network` is true, the client will go to the network first to refresh the state.
-   * Otherwise, the state will be read from the local database.
-   */
-  #[wasm_bindgen(js_name = inboxState)]
-  pub async fn inbox_state(&self, refresh_from_network: bool) -> Result<WasmInboxState, JsError> {
-    let state = self
-      .inner_client
-      .inbox_state(refresh_from_network)
-      .await
-      .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-    Ok(state.into())
-  }
-
-  #[wasm_bindgen(js_name = getLatestInboxState)]
-  pub async fn get_latest_inbox_state(&self, inbox_id: String) -> Result<WasmInboxState, JsError> {
-    let conn = self
-      .inner_client
-      .store()
-      .conn()
-      .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-    let state = self
-      .inner_client
-      .get_latest_association_state(&conn, &inbox_id)
-      .await
-      .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-    Ok(state.into())
-  }
-
-  #[wasm_bindgen(js_name = setConsentStates)]
-  pub async fn set_consent_states(&self, records: Vec<WasmConsent>) -> Result<(), JsError> {
-    let inner = self.inner_client.as_ref();
-    let stored_records: Vec<StoredConsentRecord> =
-      records.into_iter().map(StoredConsentRecord::from).collect();
-
-    inner
-      .set_consent_states(&stored_records)
-      .await
-      .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-    Ok(())
-  }
-
-  #[wasm_bindgen(js_name = getConsentState)]
-  pub async fn get_consent_state(
-    &self,
-    entity_type: WasmConsentEntityType,
-    entity: String,
-  ) -> Result<WasmConsentState, JsError> {
-    let inner = self.inner_client.as_ref();
-    let result = inner
-      .get_consent_state(entity_type.into(), entity)
-      .await
-      .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-
-    Ok(result.into())
   }
 
   #[wasm_bindgen]
