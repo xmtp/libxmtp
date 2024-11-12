b2c9782ca0326a4505e1f3df9e1cdc488ec487fc
Martin Kysel <code@martinkysel.com>
2024-11-12 13:34:58 -0500

    fix post-merge

--- replicate-crash.sh
+++ replicate-crash.sh
@@ -1,23 +1,23 @@
 rm -f client1.db
 rm -f client2.db
 
-./examples/cli/xli.sh --db client1.db --testnet --local register
-output=$("./examples/cli/xli.sh" --db client1.db --testnet --local info)
+./examples/cli/xli.sh --db client1.db --testnet --env local register
+output=$("./examples/cli/xli.sh" --db client1.db --testnet --env local info)
 client_1_address=$(echo "$output" | grep 'addressess' | sed -E 's/.*\["([^"]*)"\].*/\1/')
 echo $client_1_address
 
-./examples/cli/xli.sh --db client2.db --testnet --local register
-output=$("./examples/cli/xli.sh" --db client2.db --testnet --local info)
+./examples/cli/xli.sh --db client2.db --testnet --env local register
+output=$("./examples/cli/xli.sh" --db client2.db --testnet --env local info)
 client_2_address=$(echo "$output" | grep 'addressess' | sed -E 's/.*\["([^"]*)"\].*/\1/')
 echo $client_2_address
 
-./examples/cli/xli.sh --db client1.db --testnet --local create-group
+./examples/cli/xli.sh --db client1.db --testnet --env local create-group
 
-output=$("./examples/cli/xli.sh" --db client1.db --testnet --local list-groups)
+output=$("./examples/cli/xli.sh" --db client1.db --testnet --env local list-groups)
 group_id=$(echo "$output" | grep 'SerializableGroup' | sed -E 's/.*group_id: "([^"]*)".*/\1/' | head -n 1)
 
 echo "$group_id"
 
-./examples/cli/xli.sh --db client1.db --testnet --local send "$group_id" "Automated message send"
+./examples/cli/xli.sh --db client1.db --testnet --env local send "$group_id" "Automated message send"
 
-./examples/cli/xli.sh --db client1.db --testnet --local add-group-members "$group_id" -a "$client_2_address"
+./examples/cli/xli.sh --db client1.db --testnet --env local add-group-members "$group_id" -a "$client_2_address"
