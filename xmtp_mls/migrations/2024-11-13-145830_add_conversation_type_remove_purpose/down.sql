da7b64a35ee97c71e22c765490140650055d39cd
Dakota Brink <git@kota.is>
2024-11-13 16:21:38 -0500

    add down

--- xmtp_mls/migrations/2024-11-13-145830_add_conversation_type_remove_purpose/down.sql
+++ xmtp_mls/migrations/2024-11-13-145830_add_conversation_type_remove_purpose/down.sql
@@ -1,1 +1,10 @@
--- This file should undo anything in `up.sql`
+ALTER TABLE groups
+ADD COLUMN purpose INTEGER NOT NULL;
+
+UPDATE groups
+SET purpose = CASE
+    WHEN conversation_type = 3 THEN 2
+    ELSE 1
+END;
+
+ALTER TABLE groups DROP COLUMN conversation_type;
