0b0bcdebc982f5aedff99b851d64b54eb0f5193f
Dakota Brink <779390+codabrink@users.noreply.github.com>
2024-11-18 16:44:53 -0500

    Live Consent Sync (#1234)
    * update proto
    
    * process streamed updates
    
    * first stream update location
    
    * use local events to dispatch update
    
    * optional buffer on the local events
    
    * cleanup
    
    * rename
    
    * cleanup
    
    * add warn
    
    * lint
    
    * logs
    
    * wip
    
    * cleanup
    
    * remove comment
    
    * syncable groups fix
    
    * revert this file
    
    * invert the approach
    
    * add down
    
    * wip
    
    * wip tests
    
    * ignore non sync msgs
    
    * cleanup, test fix
    
    * lint
    
    * wasm
    
    * wasm
    
    * cleanup
    
    * map an error
    
    * lint
    
    * lint
    
    * small fix
    
    * test
    
    * revert
    
    * do not drop channel
    
    * lint
    
    * alternate remedy
    
    * wasm fix
    
    * revert gen_protos
    
    * fix potential race condition in test

--- xmtp_mls/src/subscriptions.rs
+++ xmtp_mls/src/subscriptions.rs
@@ -15,12 +15,20 @@ use crate::{
     retry::{Retry, RetryableError},
     retry_async, retryable,
     storage::{
+        consent_record::StoredConsentRecord,
         group::{ConversationType, GroupQueryArgs, StoredGroup},
         group_message::StoredGroupMessage,
         StorageError,
     },
     Client, XmtpApi,
 };
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum LocalEventError {
+    #[error("Unable to send event: {0}")]
+    Send(String),
+}
 
 #[derive(Debug)]
 /// Wrapper around a [`tokio::task::JoinHandle`] but with a oneshot receiver
@@ -37,6 +45,7 @@ pub enum LocalEvents<C> {
     // a new group was created
     NewGroup(MlsGroup<C>),
     SyncMessage(SyncMessage),
+    ConsentUpdate(Vec<StoredConsentRecord>),
 }
 
 #[derive(Clone)]
@@ -55,24 +64,26 @@ impl<C> LocalEvents<C> {
         }
     }
 
-    pub(crate) fn sync_filter(self) -> Option<SyncMessage> {
+    pub(crate) fn sync_filter(self) -> Option<Self> {
         use LocalEvents::*;
-        match self {
-            SyncMessage(msg) => Some(msg),
+
+        match &self {
+            SyncMessage(_) => Some(self),
+            ConsentUpdate(_) => Some(self),
             _ => None,
         }
     }
 }
 
-pub(crate) trait StreamMessages {
-    fn stream_sync_messages(self) -> impl Stream<Item = Result<SyncMessage, SubscribeError>>;
+pub(crate) trait StreamMessages<C> {
+    fn stream_sync_messages(self) -> impl Stream<Item = Result<LocalEvents<C>, SubscribeError>>;
 }
 
-impl<C> StreamMessages for broadcast::Receiver<LocalEvents<C>>
+impl<C> StreamMessages<C> for broadcast::Receiver<LocalEvents<C>>
 where
     C: Clone + Send + Sync + 'static,
 {
-    fn stream_sync_messages(self) -> impl Stream<Item = Result<SyncMessage, SubscribeError>> {
+    fn stream_sync_messages(self) -> impl Stream<Item = Result<LocalEvents<C>, SubscribeError>> {
         BroadcastStream::new(self).filter_map(|event| async {
             crate::optify!(event, "Missed message due to event queue lag")
                 .and_then(LocalEvents::sync_filter)
