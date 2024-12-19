0f32da1afa033309219f1cee5c2ffce8ec9ef673
cameronvoell <cameronvoell@users.noreply.github.com>
2024-12-19 12:25:42 -0800

    add codec files and type ids

---
+++ xmtp_content_types/src/reaction.rs
@@ -0,0 +1,6 @@
+pub struct ReactionCodec {}
+
+impl ReactionCodec {
+    const AUTHORITY_ID: &'static str = "xmtp.org";
+    pub const TYPE_ID: &'static str = "reaction";
+}
---
+++ xmtp_content_types/src/transaction_reference.rs
@@ -0,0 +1,6 @@
+pub struct TransactionReferenceCodec {}
+
+impl TransactionReferenceCodec {
+    const AUTHORITY_ID: &'static str = "xmtp.org";
+    pub const TYPE_ID: &'static str = "transaction_reference";
+}
