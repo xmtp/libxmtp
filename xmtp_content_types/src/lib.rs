db841995bc454452170238d54c13ca4a4da2155e
Cameron Voell <1103838+cameronvoell@users.noreply.github.com>
2024-12-19 17:18:01 -0800

    Save group message content types (#1435)
    Part of https://github.com/xmtp/libxmtp/issues/1403
    
    Adds fields to `group_messages` table corresponding to `ContentTypeId` proto and saves them when sending and receiving messages.
    
    `ContentTypeId` for reference: 
    
    https://github.com/xmtp/proto/blob/404a0f41a6dc00f5de5fcfc24856c8b4e417fe59/proto/mls/message_contents/content.proto#L10-L16
    
    ```proto
    // ContentTypeId is used to identify the type of content stored in a Message.
    message ContentTypeId {
      string authority_id = 1;  // authority governing this content type
      string type_id = 2;       // type identifier
      uint32 version_major = 3; // major version of the type
      uint32 version_minor = 4; // minor version of the type
    }
    ```

--- xmtp_content_types/src/lib.rs
+++ xmtp_content_types/src/lib.rs
@@ -1,15 +1,15 @@
+pub mod attachment;
 pub mod group_updated;
 pub mod membership_change;
+pub mod reaction;
+pub mod read_receipt;
+pub mod remote_attachment;
+pub mod reply;
 pub mod text;
+pub mod transaction_reference;
 
 use thiserror::Error;
 use xmtp_proto::xmtp::mls::message_contents::{ContentTypeId, EncodedContent};
-
-pub enum ContentType {
-    GroupMembershipChange,
-    GroupUpdated,
-    Text,
-}
 
 #[derive(Debug, Error)]
 pub enum CodecError {
