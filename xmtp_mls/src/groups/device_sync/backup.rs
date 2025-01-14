3d8fad9db13b6a378e25c7a369f3e5b5846048e3
Dakota Brink <git@kota.is>
2025-01-14 11:38:44 -0500

    test wip

--- xmtp_mls/src/groups/device_sync/backup.rs
+++ xmtp_mls/src/groups/device_sync/backup.rs
@@ -25,3 +25,48 @@ impl From<BackupOptions> for BackupMetadata {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[cfg(target_arch = "wasm32")]
+    wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_dedicated_worker);
+    use std::sync::Arc;
+
+    use backup_exporter::BackupExporter;
+    use wasm_bindgen_test::wasm_bindgen_test;
+    use xmtp_cryptography::utils::generate_local_wallet;
+
+    use crate::{builder::ClientBuilder, groups::GroupMetadataOptions};
+
+    use super::*;
+
+    #[wasm_bindgen_test(unsupported = tokio::test(flavor = "multi_thread", worker_threads = 1))]
+    #[cfg_attr(target_family = "wasm", ignore)]
+    async fn test_consent_sync() {
+        let alix_wallet = generate_local_wallet();
+        let alix = ClientBuilder::new_test_client(&alix_wallet).await;
+        let alix_provider = Arc::new(alix.mls_provider().unwrap());
+
+        let bo_wallet = generate_local_wallet();
+        let bo = ClientBuilder::new_test_client(&bo_wallet).await;
+
+        // alix.();
+        let alix_group = alix
+            .create_group(None, GroupMetadataOptions::default())
+            .unwrap();
+        alix_group
+            .add_members_by_inbox_id(&[bo.inbox_id()])
+            .await
+            .unwrap();
+        alix_group.send_message(b"hello there").await.unwrap();
+
+        let opts = BackupOptions {
+            start_ns: None,
+            end_ns: None,
+            elements: vec![BackupElementSelection::Messages],
+        };
+
+        let exporter = BackupExporter::new(opts, &alix_provider);
+        let tempdir = tempfile::TempDir::new().unwrap();
+    }
+}
