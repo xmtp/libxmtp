9a4317486cd3666c74a6b717f363c17f3ca45eaf
Dakota Brink <779390+codabrink@users.noreply.github.com>
2025-01-15 13:29:26 -0500

    Retry consent sync (#1500)
    * retry consent sync
    
    * return early
    
    ---------
    
    Co-authored-by: Cameron Voell <1103838+cameronvoell@users.noreply.github.com>

--- xmtp_mls/src/groups/mod.rs
+++ xmtp_mls/src/groups/mod.rs
@@ -724,14 +724,14 @@ impl<ScopedClient: ScopedGroupClient> MlsGroup<ScopedClient> {
             .await?;
 
         let message_id =
-            self.prepare_message(message, provider, |now| Self::into_envelope(message, now));
+            self.prepare_message(message, provider, |now| Self::into_envelope(message, now))?;
 
         self.sync_until_last_intent_resolved(provider).await?;
 
         // implicitly set group consent state to allowed
         self.update_consent_state(ConsentState::Allowed)?;
 
-        message_id
+        Ok(message_id)
     }
 
     /// Publish all unpublished messages. This happens by calling `sync_until_last_intent_resolved`
