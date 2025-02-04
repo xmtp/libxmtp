aa17876db857ae64a6cc3a32c053112859fdeb50
Martin Kysel <code@martinkysel.com>
2025-02-04 14:05:24 -0500

    Use new decentralization protos (#1592)

--- xmtp_proto/src/convert.rs
+++ xmtp_proto/src/convert.rs
@@ -102,12 +102,14 @@ impl TryFrom<WelcomeMessageInput> for ClientEnvelope {
 }
 
 impl AuthenticatedData {
+    #[allow(deprecated)]
     pub fn with_topic(topic: Vec<u8>) -> AuthenticatedData {
         AuthenticatedData {
             //TODO(mkysel) originator is hardcoded for now, but will have to become configurable
-            target_originator: 100,
+            target_originator: Some(100),
             target_topic: topic,
             depends_on: None,
+            is_commit: false,
         }
     }
 }
