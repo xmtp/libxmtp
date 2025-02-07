41cba8c730f9f680a970dccb0fd4df6b92467ca7
Mojtaba Chenani <chenani@outlook.com>
2025-02-07 22:41:56 +0100

    fix(disappearing-messages): run disappearing messages cleaner worker every second (#1611)
    * run disappearing messages worker every second

--- xmtp_mls/src/groups/disappearing_messages.rs
+++ xmtp_mls/src/groups/disappearing_messages.rs
@@ -4,11 +4,15 @@ use crate::Client;
 use std::time::Duration;
 use thiserror::Error;
 use tokio::sync::OnceCell;
+use xmtp_common::time::Interval;
 use xmtp_id::scw_verifier::SmartContractSignatureVerifier;
 use xmtp_proto::api_client::trait_impls::XmtpApi;
 
-/// Restart the DisappearingMessagesCleanerWorker every 1 sec to delete the expired messages
+/// Duration to wait before restarting the worker in case of an error.
 pub const WORKER_RESTART_DELAY: Duration = Duration::from_secs(1);
+
+/// Interval at which the DisappearingMessagesCleanerWorker runs to delete expired messages.
+pub const INTERVAL_DURATION: Duration = Duration::from_secs(1);
 
 #[derive(Debug, Error)]
 pub enum DisappearingMessagesCleanerError {
@@ -68,21 +72,18 @@ where
 {
     /// Iterate on the list of groups and delete expired messages
     async fn delete_expired_messages(&mut self) -> Result<(), DisappearingMessagesCleanerError> {
-        let provider = self.client.mls_provider()?;
-        match provider.conn_ref().delete_expired_messages() {
-            Ok(deleted_count) => {
-                tracing::info!("Successfully deleted {} expired messages", deleted_count);
-            }
-            Err(e) => {
-                tracing::error!("Failed to delete expired messages, error: {:?}", e);
-            }
-        }
+        self.client
+            .mls_provider()?
+            .conn_ref()
+            .delete_expired_messages()?;
         Ok(())
     }
     async fn run(&mut self) -> Result<(), DisappearingMessagesCleanerError> {
-        if let Err(err) = self.delete_expired_messages().await {
-            tracing::error!("Error during deletion of expired messages: {:?}", err);
+        let mut interval = Interval::new(INTERVAL_DURATION);
+
+        loop {
+            interval.tick().await;
+            self.delete_expired_messages().await?;
         }
-        Ok(())
     }
 }
