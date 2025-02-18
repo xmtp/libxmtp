b70f39f0f799396c25800d9c718d6a4270d2f0dc
Dakota Brink <git@kota.is>
2025-02-18 10:04:04 -0500

    lint

--- bindings_wasm/build.rs
+++ bindings_wasm/build.rs
@@ -1,8 +1,7 @@
 fn main() {
-  if cfg!(target_os = "macos") {
-    if std::env::var("TARGET").unwrap_or_default() == "wasm32-unknown-unknown" {
-      println!("cargo:rustc-link-arg=--linker=wasm-ld");
-      println!("cargo:rustc-env=CC_wasm32-unknown-unknown=/opt/homebrew/opt/llvm/bin/clang");
-    }
+  let target = std::env::var("TARGET").unwrap_or_default();
+  if cfg!(target_os = "macos") && target == "wasm32-unknown-unknown" {
+    println!("cargo:rustc-link-arg=--linker=wasm-ld");
+    println!("cargo:rustc-env=CC_wasm32-unknown-unknown=/opt/homebrew/opt/llvm/bin/clang");
   }
 }
