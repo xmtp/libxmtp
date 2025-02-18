50d9213dee854202f8cd6d3790dd59c9e2cfc1f8
Naomi Plasterer <naomi@xmtp.com>
2025-02-17 17:13:13 -0800

    Make public init (#471)
    * make public init
    
    * bump the pod
    
    * bump pod again

--- Sources/XMTPiOS/Libxmtp/DisappearingMessageSettings.swift
+++ Sources/XMTPiOS/Libxmtp/DisappearingMessageSettings.swift
@@ -10,6 +10,11 @@ import LibXMTP
 public struct DisappearingMessageSettings {
 	public let disappearStartingAtNs: Int64
 	public let retentionDurationInNs: Int64
+	
+	public init(disappearStartingAtNs: Int64, retentionDurationInNs: Int64) {
+		self.disappearStartingAtNs = disappearStartingAtNs
+		self.retentionDurationInNs = retentionDurationInNs
+	}
 
 	static func createFromFfi(_ ffiSettings: FfiMessageDisappearingSettings) -> DisappearingMessageSettings {
 		return DisappearingMessageSettings(
--- XMTP.podspec
+++ XMTP.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name         = "XMTP"
-  spec.version      = "3.0.29"
+  spec.version      = "3.0.30"
 
   spec.summary      = "XMTP SDK Cocoapod"
 
