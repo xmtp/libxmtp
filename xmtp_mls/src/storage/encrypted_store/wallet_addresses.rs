e9fc089df2fb1baa79c7313659bd5a3e35eb0b77
Dakota Brink <git@kota.is>
2025-02-19 15:47:32 -0500

    cleanup

--- xmtp_mls/src/storage/encrypted_store/wallet_addresses.rs
+++ xmtp_mls/src/storage/encrypted_store/wallet_addresses.rs
@@ -64,21 +64,7 @@ pub(crate) mod tests {
     use xmtp_id::associations::RootIdentifier;
 
     use crate::storage::wallet_addresses::WalletEntry;
-    use crate::{storage::encrypted_store::tests::with_connection, FetchListWithKey, Store};
-
-    // Test storing a single wallet
-    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
-    #[cfg_attr(not(target_arch = "wasm32"), tokio::test)]
-    async fn test_store_wallet() {
-        with_connection(|conn| {
-            let new_entry = WalletEntry {
-                inbox_id: "inbox_id_1".to_string(),
-                wallet_address: "wallet_address_1".to_string(),
-            };
-            assert!(new_entry.store(conn).is_ok(), "Failed to store wallet");
-        })
-        .await;
-    }
+    use crate::{storage::encrypted_store::tests::with_connection, Store};
 
     // Test storing duplicated wallets (same inbox_id and wallet_address)
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
@@ -103,71 +89,10 @@ pub(crate) mod tests {
         .await;
     }
 
-    // Test fetching wallets by a list of inbox_ids
-    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
-    #[cfg_attr(not(target_arch = "wasm32"), tokio::test)]
-    async fn test_fetch_wallets() {
-        with_connection(|conn| {
-            // Insert multiple entries with different inbox_ids
-            let new_entry1 = WalletEntry {
-                inbox_id: "fetch_test1".to_string(),
-                wallet_address: "wallet1".to_string(),
-            };
-            let new_entry2 = WalletEntry {
-                inbox_id: "fetch_test2".to_string(),
-                wallet_address: "wallet2".to_string(),
-            };
-            let new_entry3 = WalletEntry {
-                inbox_id: "fetch_test3".to_string(),
-                wallet_address: "wallet3".to_string(),
-            };
-            new_entry1.store(conn).unwrap();
-            new_entry2.store(conn).unwrap();
-            new_entry3.store(conn).unwrap();
-
-            // Fetch wallets with inbox_ids "fetch_test1" and "fetch_test2"
-            let inbox_ids = vec!["fetch_test1".to_string(), "fetch_test2".to_string()];
-            let fetched_wallets: Vec<WalletEntry> =
-                conn.load_cached_inbox_ids(&inbox_ids).unwrap_or_default();
-
-            // Verify that 3 entries are fetched (2 from "fetch_test1" and 1 from "fetch_test2")
-            assert_eq!(
-                fetched_wallets.len(),
-                2,
-                "Expected 2 wallets, found {}",
-                fetched_wallets.len()
-            );
-
-            // Verify contents of fetched entries
-            let fetched_addresses: Vec<String> = fetched_wallets
-                .iter()
-                .map(|w| w.wallet_address.clone())
-                .collect();
-            assert!(
-                fetched_addresses.contains(&"wallet1".to_string()),
-                "wallet1 not found in fetched results"
-            );
-            assert!(
-                fetched_addresses.contains(&"wallet2".to_string()),
-                "wallet2 not found in fetched results"
-            );
-
-            // Fetch wallets with a non-existent list of inbox_ids
-            let non_existent_wallets: Vec<WalletEntry> = conn
-                .fetch_list_with_key(&["nonexistent".to_string()])
-                .unwrap_or_default();
-            assert!(
-                non_existent_wallets.is_empty(),
-                "Expected no wallets, found some"
-            );
-        })
-        .await;
-    }
-
     // Test storing and fetching multiple wallet addresses with multiple keys
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
     #[cfg_attr(not(target_arch = "wasm32"), tokio::test)]
-    async fn test_store_wallet_addresses() {
+    async fn test_fetch_and_store_wallet_addresses() {
         with_connection(|conn| {
             let ident1 = RootIdentifier::rand_ethereum();
             let ident1_inbox = ident1.inbox_id(0).unwrap();
@@ -183,6 +108,15 @@ pub(crate) mod tests {
             // Verify it's the correct inbox_id
             let cached_inbox_id = stored_wallets.get(&format!("{:?}", idents[0])).unwrap();
             assert_eq!(*cached_inbox_id, ident1_inbox);
+
+            // Fetch wallets with a non-existent list of inbox_ids
+            let non_existent_wallets = conn
+                .fetch_cached_inbox_ids(&[RootIdentifier::rand_ethereum()])
+                .unwrap_or_default();
+            assert!(
+                non_existent_wallets.is_empty(),
+                "Expected no wallets, found some"
+            );
         })
         .await;
     }
