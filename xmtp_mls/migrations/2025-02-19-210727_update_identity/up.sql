1389448fc8c6e84ee3dc56b7f7cfb30988a11a46
Dakota Brink <git@kota.is>
2025-02-25 13:17:43 -0500

    update migration

--- xmtp_mls/migrations/2025-02-19-210727_update_identity/up.sql
+++ xmtp_mls/migrations/2025-02-19-210727_update_identity/up.sql
@@ -1,21 +1,13 @@
 -- Remove the existing pkey on wallet_addresses
-ALTER TABLE wallet_addresses
-DROP CONSTRAINT wallet_addresses_pkey;
+DROP TABLE wallet_addresses;
 
 -- Change the name to identity_cache
-ALTER TABLE wallet_addresses
-RENAME TO identity_cache;
-
--- rename wallet_address to identity
--- add identity_kind like in consent_records below
-ALTER TABLE identity_cache
-RENAME COLUMN wallet_address TO identity;
-
-ALTER TABLE identity_cache
-ADD COLUMN identity_kind INT NOT NULL DEFAULT 1;
-
--- Add new composite primary key
-ALTER TABLE identity_cache ADD PRIMARY KEY (identity, identity_kind);
+CREATE TABLE identity_cache (
+    inbox_id TEXT NOT NULL,
+    identity TEXT NOT NULL,
+    identity_kind INT NOT NULL,
+    PRIMARY KEY (identity, identity_kind)
+);
 
 -- Add a new identity kind (Ethereum, Passkey, Solana, Sui...)
 ALTER TABLE consent_records
