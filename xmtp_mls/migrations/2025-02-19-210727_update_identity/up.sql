d82a5edeb487ff44c24810bb23b0d6a6dbc7e937
Dakota Brink <git@kota.is>
2025-02-25 13:04:40 -0500

    update migration

--- xmtp_mls/migrations/2025-02-19-210727_update_identity/up.sql
+++ xmtp_mls/migrations/2025-02-19-210727_update_identity/up.sql
@@ -1,5 +1,6 @@
 -- Remove the existing pkey on wallet_addresses
-ALTER TABLE wallet_addresses DROP CONSTRAINT wallet_addresses_pkey;
+ALTER TABLE wallet_addresses
+DROP CONSTRAINT wallet_addresses_pkey;
 
 -- Change the name to identity_cache
 ALTER TABLE wallet_addresses
@@ -8,12 +9,13 @@ RENAME TO identity_cache;
 -- rename wallet_address to identity
 -- add identity_kind like in consent_records below
 ALTER TABLE identity_cache
-RENAME COLUMN wallet_address TO identity,
+RENAME COLUMN wallet_address TO identity;
+
+ALTER TABLE identity_cache
 ADD COLUMN identity_kind INT NOT NULL DEFAULT 1;
 
 -- Add new composite primary key
-ALTER TABLE identity_cache
-ADD PRIMARY KEY (identity, identity_kind);
+ALTER TABLE identity_cache ADD PRIMARY KEY (identity, identity_kind);
 
 -- Add a new identity kind (Ethereum, Passkey, Solana, Sui...)
 ALTER TABLE consent_records
@@ -21,10 +23,19 @@ ADD COLUMN identity_kind INT;
 
 -- Set all the current Identities to Ethereum, since that's all we supported before now
 UPDATE consent_records
-SET identity_kind = 1
-WHERE consent_type = 3;
+SET
+    identity_kind = 1
+WHERE
+    consent_type = 3;
 
--- Add a constraint so that the kind must be present if the type is "Identity"
-ALTER TABLE consent_records
-ADD CONSTRAINT chk_identity_kind_not_null
-CHECK ((consent_type = 3 AND identity_kind IS NOT NULL) OR (consent_type != 3 AND identity_kind IS NULL));
+-- Add the constraint with syntax that works across different database systems
+ALTER TABLE consent_records ADD CHECK (
+    (
+        consent_type = 3
+        AND identity_kind IS NOT NULL
+    )
+    OR (
+        consent_type != 3
+        AND identity_kind IS NULL
+    )
+);
