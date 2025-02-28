1f2f4d05acbc2f974aa179f1357ab81352d7dff3
Naomi Plasterer <naomi@xmtp.com>
2025-02-28 13:39:55 -0800

    clean up a bunch of v2 stuff

--- bindings_ffi/src/mls.rs
+++ bindings_ffi/src/mls.rs
@@ -2067,11 +2067,6 @@ impl FfiConversation {
         let conversation_type = self.inner.conversation_type(&provider).await?;
         Ok(conversation_type.into())
     }
-
-    pub async fn epoch(&self) -> Result<u64, GenericError> {
-        let provider = self.inner.mls_provider()?;
-        Ok(self.inner.epoch(&provider).await?)
-    }
 }
 
 #[uniffi::export]
@@ -2378,7 +2373,7 @@ pub fn decode_multi_remote_attachment(
 pub struct FfiMessage {
     pub id: Vec<u8>,
     pub sent_at_ns: i64,
-    pub convo_id: Vec<u8>,
+    pub conversation_id: Vec<u8>,
     pub sender_inbox_id: String,
     pub content: Vec<u8>,
     pub kind: FfiConversationMessageKind,
@@ -2390,7 +2385,7 @@ impl From<StoredGroupMessage> for FfiMessage {
         Self {
             id: msg.id,
             sent_at_ns: msg.sent_at_ns,
-            convo_id: msg.group_id,
+            conversation_id: msg.group_id,
             sender_inbox_id: msg.sender_inbox_id,
             content: msg.decrypted_message_bytes,
             kind: msg.kind.into(),
--- xmtp_user_preferences/README.md
+++
@@ -1,3 +0,0 @@
-# XMTP Personal Private Portable Preferences
-
-A library for encrypting messages where the sender and recipient are the same (self-messaging).
