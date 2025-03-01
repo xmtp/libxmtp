ec5bacd551bc04f1b5dcfc9b7962abb64ddd497c
Naomi Plasterer <naomi@xmtp.com>
2025-02-28 20:44:54 -0800

    Add Hmac Keys for individual Conversation (#1680)
    * add hmac keys for conversation
    
    * Add conversation hmac keys test
    
    * Use Uint8Array for HmacKey key
    
    * Minor cleanup
    
    * Return Vec instead of HashMap
    
    * write an ffi test
    
    ---------
    
    Co-authored-by: Ry Racherbaumer <ry@xmtp.com>

--- bindings_wasm/src/conversation.rs
+++ bindings_wasm/src/conversation.rs
@@ -4,7 +4,7 @@ use wasm_bindgen::{prelude::wasm_bindgen, JsError};
 use xmtp_mls::storage::group::ConversationType;
 
 use crate::client::RustXmtpClient;
-use crate::conversations::MessageDisappearingSettings;
+use crate::conversations::{HmacKey, MessageDisappearingSettings};
 use crate::encoded_content::EncodedContent;
 use crate::messages::{ListMessagesOptions, Message};
 use crate::permissions::{MetadataField, PermissionPolicy, PermissionUpdateType};
@@ -607,6 +607,20 @@ impl Conversation {
         .as_ref()
         .is_some_and(|s| s.from_ns > 0 && s.in_ns > 0)
     })
+  }
+
+  #[wasm_bindgen(js_name = getHmacKeys)]
+  pub fn get_hmac_keys(&self) -> Result<JsValue, JsError> {
+    let group = self.to_mls_group();
+
+    let keys = group
+      .hmac_keys(-1..=1)
+      .map_err(|e| JsError::new(&format!("{e}")))?
+      .into_iter()
+      .map(Into::into)
+      .collect::<Vec<HmacKey>>();
+
+    Ok(crate::to_value(&keys)?)
   }
 }
 
