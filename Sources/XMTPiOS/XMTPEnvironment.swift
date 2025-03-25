47121e8b2052674066ec066f03ea26ed356ef544
Naomi Plasterer <naomi@xmtp.com>
2025-03-24 20:30:16 -0700

    Allow LOCAL with a custom string (#491)
    * add ability to set a custom url for local
    
    * bump iOS version
    
    * small tweak so the port is appended

--- Sources/XMTPiOS/XMTPEnvironment.swift
+++ Sources/XMTPiOS/XMTPEnvironment.swift
@@ -9,18 +9,28 @@ import Foundation
 
 /// Contains hosts an `ApiClient` can connect to
 public enum XMTPEnvironment: String, Sendable {
-	case dev = "grpc.dev.xmtp.network:443",
-	     production = "grpc.production.xmtp.network:443",
-	     local = "localhost:5556"
+	case dev = "grpc.dev.xmtp.network"
+	case production = "grpc.production.xmtp.network"
+	case local = "localhost"
+
+	// Optional override for the local environment
+	public static var customLocalAddress: String?
+
+	var address: String {
+		switch self {
+		case .local:
+			return XMTPEnvironment.customLocalAddress ?? rawValue
+		default:
+			return rawValue
+		}
+	}
 
 	var url: String {
 		switch self {
-		case .dev:
-			return "https://\(rawValue)"
-		case .production:
-			return "https://\(rawValue)"
+		case .dev, .production:
+			return "https://\(address):443"
 		case .local:
-			return "http://\(rawValue)"
+			return "http://\(address):5556"
 		}
 	}
 
