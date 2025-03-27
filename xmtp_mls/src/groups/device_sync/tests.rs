1a6fd4951ee06f1854d61dabdba276163193a6d8
Dakota Brink <git@kota.is>
2025-03-27 13:45:23 -0400

    cleanup

--- xmtp_mls/src/groups/device_sync/tests.rs
+++ xmtp_mls/src/groups/device_sync/tests.rs
@@ -52,6 +52,9 @@ async fn only_one_payload_sent() -> Result<()> {
     alix1.worker.wait(SyncMetric::PayloadsSent, 1).await;
     alix1.worker.clear_metric(SyncMetric::PayloadsSent);
 
+    alix1.sync_welcomes(&alix1.provider).await?;
+    alix2.sync_welcomes(&alix2.provider).await?;
+
     let alix3 = Tester::new_from_wallet(alix1.wallet.clone()).await;
     alix3.worker.wait_for_init().await;
 
@@ -74,6 +77,7 @@ async fn only_one_payload_sent() -> Result<()> {
     let timeout1 = tokio::time::timeout(Duration::from_secs(1), wait1).await;
     let timeout2 = tokio::time::timeout(Duration::from_secs(1), wait2).await;
 
+    // We want one of them to timeout (only one payload sent)
     assert_ne!(timeout1.is_ok(), timeout2.is_ok());
 
     Ok(())
