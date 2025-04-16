4eaa3f5e7d27e5d4431a596bd7f781c20e5c1059
cameronvoell <cameronvoell@users.noreply.github.com>
2025-04-16 10:36:22 -0700

    add git sha to log file names

--- bindings_ffi/src/logger.rs
+++ bindings_ffi/src/logger.rs
@@ -270,8 +270,9 @@ fn enable_debug_file_inner(
     let _ = exit_debug_writer();
 
     let version = env!("CARGO_PKG_VERSION");
+    let commit_sha = option_env!("GIT_COMMIT_SHA").unwrap_or("unknown");
     let file_appender = RollingFileAppender::builder()
-        .filename_prefix(format!("libxmtp-v{}.log", version))
+        .filename_prefix(format!("libxmtp-v{}.{}.log", version, commit_sha))
         .rotation(rotation.into())
         .max_log_files(max_files as usize)
         .build(&directory)?;
