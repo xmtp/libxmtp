d0436695b5c6d2082046a7b7b0c721467c6f33ab
cameronvoell <cameronvoell@users.noreply.github.com>
2025-04-16 10:50:05 -0700

    added new log lines when making decisions on whether to update the cursor.

--- bindings_ffi/build.rs
+++ bindings_ffi/build.rs
@@ -7,15 +7,15 @@ fn main() {
         .expect("failed to make libxmtp-version");
 
     let output = Command::new("git")
-        .args(&["rev-parse", "--short=7", "HEAD"])
+        .args(["rev-parse", "--short=7", "HEAD"])
         .output();
-    
+
     let git_hash = match output {
         Ok(output) if output.status.success() => {
             String::from_utf8_lossy(&output.stdout).trim().to_string()
-        },
-        _ => "unknown".to_string()
+        }
+        _ => "unknown".to_string(),
     };
-    
+
     println!("cargo:rustc-env=GIT_COMMIT_SHA={}", git_hash);
 }
