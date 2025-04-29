f10120ae4fcff0e724b0be1d0341bb9d38d916a3
Dakota Brink <git@kota.is>
2025-04-29 13:59:57 -0700

    update protos

--- xmtp_mls/src/groups/device_sync.rs
+++ xmtp_mls/src/groups/device_sync.rs
@@ -170,8 +170,8 @@ where
             &sync_group.group_id[..4]
         );
 
-        let content_bytes =
-            serde_json::to_vec(&content).map_err(|err| ClientError::Generic(err.to_string()))?;
+        let content_bytes = serde_json::to_vec_pretty(&content)
+            .map_err(|err| ClientError::Generic(err.to_string()))?;
         let message_id =
             sync_group.prepare_message(&content_bytes, provider, |now| PlaintextEnvelope {
                 content: Some(Content::V1(V1 {
@@ -267,6 +267,8 @@ pub trait IterWithContent<A, B> {
 impl IterWithContent<StoredGroupMessage, DeviceSyncContent> for Vec<StoredGroupMessage> {
     fn iter_with_content(self) -> impl Iterator<Item = (StoredGroupMessage, DeviceSyncContent)> {
         self.into_iter().filter_map(|msg| {
+            let str = String::from_utf8_lossy(&msg.decrypted_message_bytes);
+            tracing::error!("{str}");
             let content = serde_json::from_slice(&msg.decrypted_message_bytes).ok()?;
             Some((msg, content))
         })
