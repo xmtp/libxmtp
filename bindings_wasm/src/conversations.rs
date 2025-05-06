c91e90ff7c1cea1c98a662bc1757678b7f6719b8
Ry Racherbaumer <ry@xmtp.com>
2025-05-06 14:32:04 -0500

    WASM bindings update

--- bindings_wasm/src/conversations.rs
+++ bindings_wasm/src/conversations.rs
@@ -315,10 +315,9 @@ impl Conversations {
 
 #[wasm_bindgen]
 impl Conversations {
-  #[wasm_bindgen(js_name = createGroup)]
-  pub async fn create_group(
+  #[wasm_bindgen(js_name = createGroupOptimistic)]
+  pub async fn create_group_optimistic(
     &self,
-    account_identifiers: Vec<Identifier>,
     options: Option<CreateGroupOptions>,
   ) -> Result<Conversation, JsError> {
     let options = options.unwrap_or(CreateGroupOptions {
@@ -361,27 +360,27 @@ impl Conversations {
       _ => None,
     };
 
-    let convo = if account_identifiers.is_empty() {
-      let group = self
-        .inner_client
-        .create_group(group_permissions, metadata_options)
-        .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-      group
-        .sync()
-        .await
-        .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-      group
-    } else {
-      self
-        .inner_client
-        .create_group_with_members(
-          &account_identifiers.to_internal()?,
-          group_permissions,
-          metadata_options,
-        )
-        .await
-        .map_err(|e| JsError::new(format!("{}", e).as_str()))?
+    let group = self
+      .inner_client
+      .create_group(group_permissions, metadata_options)
+      .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
+
+    Ok(group.into())
+  }
+
+  #[wasm_bindgen(js_name = createGroup)]
+  pub async fn create_group(
+    &self,
+    account_identifiers: Vec<Identifier>,
+    options: Option<CreateGroupOptions>,
+  ) -> Result<Conversation, JsError> {
+    let convo = self.create_group_optimistic(options).await?;
+
+    if !account_identifiers.is_empty() {
+      convo.add_members(account_identifiers).await?;
     };
+
+    convo.sync().await?;
 
     Ok(convo.into())
   }
@@ -392,63 +391,13 @@ impl Conversations {
     inbox_ids: Vec<String>,
     options: Option<CreateGroupOptions>,
   ) -> Result<Conversation, JsError> {
-    let options = options.unwrap_or(CreateGroupOptions {
-      permissions: None,
-      group_name: None,
-      group_image_url_square: None,
-      group_description: None,
-      custom_permission_policy_set: None,
-      message_disappearing_settings: None,
-    });
+    let convo = self.create_group_optimistic(options).await?;
 
-    if let Some(GroupPermissionsOptions::CustomPolicy) = options.permissions {
-      if options.custom_permission_policy_set.is_none() {
-        return Err(JsError::new("CustomPolicy must include policy set"));
-      }
-    } else if options.custom_permission_policy_set.is_some() {
-      return Err(JsError::new("Only CustomPolicy may specify a policy set"));
-    }
-
-    let metadata_options = options.clone().into_group_metadata_options();
-
-    let group_permissions = match options.permissions {
-      Some(GroupPermissionsOptions::Default) => {
-        Some(PreconfiguredPolicies::Default.to_policy_set())
-      }
-      Some(GroupPermissionsOptions::AdminOnly) => {
-        Some(PreconfiguredPolicies::AdminsOnly.to_policy_set())
-      }
-      Some(GroupPermissionsOptions::CustomPolicy) => {
-        if let Some(policy_set) = options.custom_permission_policy_set {
-          Some(
-            policy_set
-              .try_into()
-              .map_err(|e| JsError::new(format!("{}", e).as_str()))?,
-          )
-        } else {
-          None
-        }
-      }
-      _ => None,
+    if !inbox_ids.is_empty() {
+      convo.add_members_by_inbox_id(inbox_ids).await?;
     };
 
-    let convo = if inbox_ids.is_empty() {
-      let group = self
-        .inner_client
-        .create_group(group_permissions, metadata_options)
-        .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-      group
-        .sync()
-        .await
-        .map_err(|e| JsError::new(format!("{}", e).as_str()))?;
-      group
-    } else {
-      self
-        .inner_client
-        .create_group_with_inbox_ids(&inbox_ids, group_permissions, metadata_options)
-        .await
-        .map_err(|e| JsError::new(format!("{}", e).as_str()))?
-    };
+    convo.sync().await?;
 
     Ok(convo.into())
   }
