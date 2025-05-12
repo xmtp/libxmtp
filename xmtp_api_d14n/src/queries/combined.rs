9435ec341b28c82348e6131f567731bf18b34d6c
Dakota Brink <git@kota.is>
2025-05-12 12:22:33 -0400

    update

--- xmtp_api_d14n/src/queries/combined.rs
+++ xmtp_api_d14n/src/queries/combined.rs
@@ -1,12 +1,28 @@
-use xmtp_common::RetryableError;
-use xmtp_proto::api_client::ApiStats;
-use xmtp_proto::traits::{ApiClientError, Client, Query};
-use xmtp_proto::{mls_v1, prelude::XmtpMlsClient};
-
+use crate::d14n::GetNewestEnvelopes;
+use crate::d14n::PublishClientEnvelopes;
+use crate::d14n::QueryEnvelope;
+use crate::protocol::CollectionExtractor;
+use crate::protocol::GroupMessageExtractor;
+use crate::protocol::KeyPackagesExtractor;
+use crate::protocol::SequencedExtractor;
+use crate::protocol::TopicKind;
+use crate::protocol::WelcomeMessageExtractor;
+use crate::protocol::traits::Envelope;
+use crate::protocol::traits::EnvelopeCollection;
+use crate::protocol::traits::Extractor;
 use crate::v3::{
     FetchKeyPackages, QueryGroupMessages, QueryWelcomeMessages, SendGroupMessages,
     SendWelcomeMessages, UploadKeyPackage,
 };
+use xmtp_common::RetryableError;
+use xmtp_proto::api_client::ApiStats;
+use xmtp_proto::api_client::XmtpMlsClient;
+use xmtp_proto::mls_v1;
+use xmtp_proto::traits::Client;
+use xmtp_proto::traits::{ApiClientError, Query};
+use xmtp_proto::xmtp::xmtpv4::envelopes::ClientEnvelope;
+use xmtp_proto::xmtp::xmtpv4::message_api::GetNewestEnvelopeResponse;
+use xmtp_proto::xmtp::xmtpv4::message_api::QueryEnvelopesResponse;
 
 #[derive(Clone)]
 pub struct CombinedD14nClient<C, D> {
@@ -41,11 +57,20 @@ where
         &self,
         request: mls_v1::FetchKeyPackagesRequest,
     ) -> Result<mls_v1::FetchKeyPackagesResponse, Self::Error> {
-        FetchKeyPackages::builder()
-            .installation_keys(request.installation_keys)
+        let topics = request
+            .installation_keys
+            .iter()
+            .map(|key| TopicKind::KeyPackagesV1.build(key))
+            .collect();
+
+        let result: GetNewestEnvelopeResponse = GetNewestEnvelopes::builder()
+            .topics(topics)
             .build()?
             .query(&self.xmtpd_client)
-            .await
+            .await?;
+        let extractor = CollectionExtractor::new(result.results, KeyPackagesExtractor::new());
+        let key_packages = extractor.get()?;
+        Ok(mls_v1::FetchKeyPackagesResponse { key_packages })
     }
 
     async fn send_group_messages(
@@ -73,22 +98,45 @@ where
         &self,
         request: mls_v1::QueryGroupMessagesRequest,
     ) -> Result<mls_v1::QueryGroupMessagesResponse, Self::Error> {
-        QueryGroupMessages::builder()
-            .group_id(request.group_id)
+        let response: QueryEnvelopesResponse = QueryEnvelope::builder()
+            .topic(TopicKind::GroupMessagesV1.build(request.group_id.as_slice()))
+            .paging_info(request.paging_info)
             .build()?
             .query(&self.xmtpd_client)
-            .await
+            .await?;
+
+        let messages = SequencedExtractor::builder()
+            .envelopes(response.envelopes)
+            .build::<GroupMessageExtractor>()
+            .get()?;
+
+        Ok(mls_v1::QueryGroupMessagesResponse {
+            messages,
+            paging_info: None,
+        })
     }
     async fn query_welcome_messages(
         &self,
         request: mls_v1::QueryWelcomeMessagesRequest,
     ) -> Result<mls_v1::QueryWelcomeMessagesResponse, Self::Error> {
-        QueryWelcomeMessages::builder()
-            .installation_key(request.installation_key)
+        let topic = TopicKind::WelcomeMessagesV1.build(request.installation_key.as_slice());
+
+        let response = QueryEnvelope::builder()
+            .topic(topic)
             .paging_info(request.paging_info)
             .build()?
             .query(&self.xmtpd_client)
-            .await
+            .await?;
+
+        let messages = SequencedExtractor::builder()
+            .envelopes(response.envelopes)
+            .build::<WelcomeMessageExtractor>()
+            .get()?;
+
+        Ok(mls_v1::QueryWelcomeMessagesResponse {
+            messages,
+            paging_info: None,
+        })
     }
 
     fn stats(&self) -> ApiStats {
