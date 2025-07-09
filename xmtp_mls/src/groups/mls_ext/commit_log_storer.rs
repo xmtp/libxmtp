97afe1681bdb145e8f7efadc114317b677bd8107
Richard Hua <rich@rkhua.com>
2025-07-09 09:57:43 -0700

    welcome error handling

--- xmtp_mls/src/groups/mls_ext/commit_log_storer.rs
+++ xmtp_mls/src/groups/mls_ext/commit_log_storer.rs
@@ -1,10 +1,13 @@
 use crate::groups::GroupError;
 use crate::groups::{mls_sync::GroupMessageProcessingError, validated_commit::ValidatedCommit};
 use crate::identity::Identity;
+use crate::StorageError;
 use openmls::group::{MlsGroup, MlsGroupCreateConfig, StagedCommit};
 use openmls::prelude::CredentialWithKey;
+use openmls::prelude::GroupEpoch;
 use openmls::prelude::GroupId;
 use openmls::prelude::StagedWelcome;
+use openmls::schedule::EpochAuthenticator;
 use xmtp_db::{
     local_commit_log::{CommitType, NewLocalCommitLog},
     remote_commit_log::CommitResult,
@@ -41,6 +44,20 @@ pub trait CommitLogStorer: std::marker::Sized {
         validated_commit: &ValidatedCommit,
         sequence_id: i64,
     ) -> Result<(), GroupMessageProcessingError>;
+
+    /// Marks a commit as failed in the commit log.
+    /// Only call this when the status of the commit is final.
+    /// Specifically, do not call this for retryable errors, or
+    /// VersionTooLow/GroupPaused errors.
+    fn mark_failed_commit_logged<Db: ConnectionExt>(
+        provider: &XmtpOpenMlsProvider<Db>,
+        group_id: &[u8],
+        commit_sequence_id: u64,
+        commit_epoch: GroupEpoch,
+        error: &GroupMessageProcessingError,
+        last_epoch_number: GroupEpoch,
+        last_epoch_authenticator: &EpochAuthenticator,
+    ) -> Result<(), StorageError>;
 }
 
 impl CommitLogStorer for MlsGroup {
@@ -122,6 +139,7 @@ impl CommitLogStorer for MlsGroup {
         sender_inbox_id: &str,
         sender_installation_id: &[u8],
     ) -> Result<Self, GroupError> {
+        // Failed welcomes do not need to be added to the commit log
         let mls_group = welcome.into_group(provider)?;
 
         if crate::configuration::ENABLE_COMMIT_LOG {
@@ -170,6 +188,54 @@ impl CommitLogStorer for MlsGroup {
             .store(provider.db())?;
         }
 
+        Ok(())
+    }
+
+    fn mark_failed_commit_logged<Db: ConnectionExt>(
+        provider: &XmtpOpenMlsProvider<Db>,
+        group_id: &[u8],
+        commit_sequence_id: u64,
+        commit_epoch: GroupEpoch,
+        error: &GroupMessageProcessingError,
+        last_epoch_number: GroupEpoch,
+        last_epoch_authenticator: &EpochAuthenticator,
+    ) -> Result<(), StorageError> {
+        if !crate::configuration::ENABLE_COMMIT_LOG {
+            return Ok(());
+        }
+        let conn = provider.db();
+        let mut maybe_recently_welcomed = true;
+        // Latest log may not exist if a client upgraded from a version without local commit logs
+        if let Some(latest_log) = conn.get_latest_log_for_group(group_id)? {
+            // Because we don't increment the cursor for non-retryable errors, we may have already logged this commit
+            if latest_log.commit_sequence_id == commit_sequence_id as i64
+                && latest_log.commit_result != CommitResult::Success
+            {
+                return Ok(());
+            }
+            if latest_log.commit_type != Some(CommitType::Welcome.to_string()) {
+                maybe_recently_welcomed = false;
+            }
+        }
+        // If we've recently joined the group, we may get a bunch of wrong epoch errors
+        // until we 'catch up' to the commit that spawned the welcome. We can ignore these for now.
+        if commit_epoch.as_u64() <= last_epoch_number.as_u64() && maybe_recently_welcomed {
+            return Ok(());
+        }
+
+        NewLocalCommitLog {
+            group_id: group_id.to_vec(),
+            commit_sequence_id: commit_sequence_id as i64,
+            last_epoch_authenticator: last_epoch_authenticator.as_slice().to_vec(),
+            commit_result: error.commit_result(),
+            applied_epoch_number: last_epoch_number.as_u64() as i64,
+            applied_epoch_authenticator: last_epoch_authenticator.as_slice().to_vec(),
+            error_message: Some(format!("{:?}", error)),
+            sender_inbox_id: None,
+            sender_installation_id: None,
+            commit_type: None,
+        }
+        .store(conn)?;
         Ok(())
     }
 }
