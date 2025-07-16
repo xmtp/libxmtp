fc44b685bd0be4687611d651da72fe5a9d51edf8
Martin Kysel <code@martinkysel.com>
2025-07-16 17:34:10 -0600

    Protos and no-is-commit

--- xmtp_proto/src/gen/xmtp.xmtpv4.envelopes.serde.rs
+++ xmtp_proto/src/gen/xmtp.xmtpv4.envelopes.serde.rs
@@ -13,9 +13,6 @@ impl serde::Serialize for AuthenticatedData {
         if self.depends_on.is_some() {
             len += 1;
         }
-        if self.is_commit {
-            len += 1;
-        }
         let mut struct_ser = serializer.serialize_struct("xmtp.xmtpv4.envelopes.AuthenticatedData", len)?;
         if !self.target_topic.is_empty() {
             #[allow(clippy::needless_borrow)]
@@ -24,9 +21,6 @@ impl serde::Serialize for AuthenticatedData {
         }
         if let Some(v) = self.depends_on.as_ref() {
             struct_ser.serialize_field("depends_on", v)?;
-        }
-        if self.is_commit {
-            struct_ser.serialize_field("is_commit", &self.is_commit)?;
         }
         struct_ser.end()
     }
@@ -42,15 +36,12 @@ impl<'de> serde::Deserialize<'de> for AuthenticatedData {
             "targetTopic",
             "depends_on",
             "dependsOn",
-            "is_commit",
-            "isCommit",
         ];
 
         #[allow(clippy::enum_variant_names)]
         enum GeneratedField {
             TargetTopic,
             DependsOn,
-            IsCommit,
             __SkipField__,
         }
         impl<'de> serde::Deserialize<'de> for GeneratedField {
@@ -75,7 +66,6 @@ impl<'de> serde::Deserialize<'de> for AuthenticatedData {
                         match value {
                             "targetTopic" | "target_topic" => Ok(GeneratedField::TargetTopic),
                             "dependsOn" | "depends_on" => Ok(GeneratedField::DependsOn),
-                            "isCommit" | "is_commit" => Ok(GeneratedField::IsCommit),
                             _ => Ok(GeneratedField::__SkipField__),
                         }
                     }
@@ -97,7 +87,6 @@ impl<'de> serde::Deserialize<'de> for AuthenticatedData {
             {
                 let mut target_topic__ = None;
                 let mut depends_on__ = None;
-                let mut is_commit__ = None;
                 while let Some(k) = map_.next_key()? {
                     match k {
                         GeneratedField::TargetTopic => {
@@ -114,12 +103,6 @@ impl<'de> serde::Deserialize<'de> for AuthenticatedData {
                             }
                             depends_on__ = map_.next_value()?;
                         }
-                        GeneratedField::IsCommit => {
-                            if is_commit__.is_some() {
-                                return Err(serde::de::Error::duplicate_field("isCommit"));
-                            }
-                            is_commit__ = Some(map_.next_value()?);
-                        }
                         GeneratedField::__SkipField__ => {
                             let _ = map_.next_value::<serde::de::IgnoredAny>()?;
                         }
@@ -128,7 +111,6 @@ impl<'de> serde::Deserialize<'de> for AuthenticatedData {
                 Ok(AuthenticatedData {
                     target_topic: target_topic__.unwrap_or_default(),
                     depends_on: depends_on__,
-                    is_commit: is_commit__.unwrap_or_default(),
                 })
             }
         }
