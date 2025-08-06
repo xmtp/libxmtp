ebd2fdf573f5ec2f51c2e2c0d9931bf9179fa773
Andrew Plaza <github@andrewplaza.dev>
2025-08-06 13:07:22 -0400

    clearer contextual tracing for tests

--- xmtp_mls/src/utils/test/fixtures.rs
+++ xmtp_mls/src/utils/test/fixtures.rs
@@ -1,28 +1,36 @@
 use xmtp_cryptography::utils::generate_local_wallet;
 
-use crate::builder::ClientBuilder;
+use crate::{
+    builder::ClientBuilder,
+    utils::{ClientTester, LocalTesterBuilder, Tester},
+};
 
 use super::FullXmtpClient;
 use rstest::*;
 
 #[fixture]
-pub async fn alix() -> FullXmtpClient {
-    ClientBuilder::new_test_client_vanilla(&generate_local_wallet()).await
+pub async fn alix() -> ClientTester {
+    Tester::builder().with_name("alix").build().await
 }
 
 #[fixture]
-pub async fn bo() -> FullXmtpClient {
-    ClientBuilder::new_test_client_vanilla(&generate_local_wallet()).await
+pub async fn bo() -> ClientTester {
+    Tester::builder().with_name("bo").build().await
 }
 
 #[fixture]
-pub async fn caro() -> FullXmtpClient {
-    ClientBuilder::new_test_client_vanilla(&generate_local_wallet()).await
+pub async fn bola() -> ClientTester {
+    Tester::builder().with_name("bo").build().await
 }
 
 #[fixture]
-pub async fn eve() -> FullXmtpClient {
-    ClientBuilder::new_test_client_vanilla(&generate_local_wallet()).await
+pub async fn caro() -> ClientTester {
+    Tester::builder().with_name("caro").build().await
+}
+
+#[fixture]
+pub async fn eve() -> ClientTester {
+    Tester::builder().with_name("eve").build().await
 }
 
 #[fixture]
