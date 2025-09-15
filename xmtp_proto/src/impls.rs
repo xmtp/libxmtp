2cdce3b2ce4d602c005bf2b8cf4fd1aea980f54b
Tyler Hawkes <tyler.hawkes@gmail.com>
2025-09-15 11:48:41 -0600

    welcome pointer v3 impl

--- xmtp_proto/src/impls.rs
+++ xmtp_proto/src/impls.rs
@@ -1,4 +1,8 @@
-/// Std implementations for some generated types
+/// implementations for some generated types
+use crate::xmtp::mls::api::v1::welcome_message::Version;
+use crate::xmtp::mls::message_contents::{
+    WelcomePointeeEncryptionAeadType, WelcomePointeeEncryptionAeadTypesExtension,
+};
 use crate::xmtp::xmtpv4::envelopes::client_envelope::Payload;
 
 impl std::fmt::Display for Payload {
@@ -42,6 +46,35 @@ xmtp_common::if_test! {
                 sender_hmac: xmtp_common::rand_vec::<6>(),
                 should_push: false,
             }
+        }
+    }
+}
+
+impl Version {
+    pub fn id(&self) -> u64 {
+        match self {
+            Version::V1(v1) => v1.id,
+            Version::WelcomePointer(w) => w.id,
+        }
+    }
+    pub fn created_ns(&self) -> u64 {
+        match self {
+            Version::V1(v1) => v1.created_ns,
+            Version::WelcomePointer(w) => w.created_ns,
+        }
+    }
+    pub fn installation_key(&self) -> &[u8] {
+        match self {
+            Version::V1(v1) => v1.installation_key.as_slice(),
+            Version::WelcomePointer(w) => w.installation_key.as_slice(),
+        }
+    }
+}
+
+impl WelcomePointeeEncryptionAeadTypesExtension {
+    pub fn available_types() -> Self {
+        Self {
+            supported_aead_types: vec![WelcomePointeeEncryptionAeadType::Chacha20Poly1305.into()],
         }
     }
 }
