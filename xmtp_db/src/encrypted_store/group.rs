95f98619311906b4e58a30724545f835d2cc5448
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2025-09-22 17:36:41 -0700

    Fix merge conflicts and lint issues

--- xmtp_db/src/encrypted_store/group.rs
+++ xmtp_db/src/encrypted_store/group.rs
@@ -130,13 +130,6 @@ pub enum GroupQueryOrderBy {
     LastActivity,
 }
 
-#[derive(Debug, Clone, Default)]
-pub enum GroupQueryOrderBy {
-    #[default]
-    CreatedAt,
-    LastActivity,
-}
-
 #[derive(Debug, Default, Clone)]
 pub struct GroupQueryArgs {
     pub allowed_states: Option<Vec<GroupMembershipState>>,
