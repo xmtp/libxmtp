7af1923d02772a2054039c1251b4a3c8824cc179
Borja Aranda <borja@ephemerahq.com>
2025-10-02 18:31:26 +0200

    Select fastest node

--- xmtp_api_d14n/src/clients/aggregate_client.rs
+++ xmtp_api_d14n/src/clients/aggregate_client.rs
@@ -1,64 +1,40 @@
-use crate::d14n::GetNodes;
+use crate::d14n::{GetNodes, HealthCheck};
+use futures::future::join_all;
 use prost::bytes::Bytes;
+use std::collections::HashMap;
+use xmtp_api_grpc::GrpcClient;
+use xmtp_common::time::{Duration, Instant};
 use xmtp_proto::{
-    traits::{Client, Query},
-    traits::ApiClientError,
     prelude::ApiBuilder,
+    traits::{ApiClientError, Client, Query},
 };
-use xmtp_api_grpc::{
-    {GrpcClient}
-};
-
 
 pub struct AggregateClient<C>
 where
     C: Client + Sync + Send,
 {
-    // Do we want to store node_id : client map?
-    nodes: Vec<C>,
     inner: C,
 }
 
 impl AggregateClient<GrpcClient> {
-    pub async fn new(gateway_client: &GrpcClient) -> Self {
-        let nodes = Self::build_inner_clients(gateway_client).await.unwrap();
+    pub async fn new(
+        gateway_client: &GrpcClient,
+        timeout: Duration,
+    ) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
+        let nodes = get_nodes(gateway_client).await?;
+        let selected = get_fastest_node(nodes, timeout).await?;
 
-        // TODO: Select best
-        let selected = nodes.first().unwrap().clone();
-
-        Self {
-            nodes,
-            inner: selected,
-        }
+        Ok(Self { inner: selected })
     }
 
-    pub async fn build_inner_clients(gateway_client: &GrpcClient) -> Result<Vec<GrpcClient>, Box<dyn std::error::Error + Send + Sync>> {
-        // TODO: Cleanup unwrap
-        let endpoint = GetNodes::builder().build().unwrap();
-        let response = endpoint.query(gateway_client).await?;
-        let mut inner_clients = Vec::new();
-
-        for node_url in response.nodes {
-            // Save latency somewhere
-            let mut client_builder = GrpcClient::builder();
-            let use_tls = node_url.starts_with("https://");
-
-            client_builder.set_tls(use_tls);
-            client_builder.set_host(node_url);
-            
-            let client = client_builder.build().await?;
-            inner_clients.push(client);
-        }
-
-        Ok(inner_clients)
-    }
-
-    pub fn get_inner_client(&self) -> &GrpcClient {
-        &self.inner
-    }
-
-    pub fn set_inner_client(&mut self, client: &GrpcClient) {
-        self.inner = client.clone();
+    pub async fn refresh(
+        &mut self,
+        timeout: Duration,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let nodes = get_nodes(&self.inner).await?;
+        let selected = get_fastest_node(nodes, timeout).await?;
+        self.inner = selected;
+        Ok(())
     }
 }
 
@@ -88,4 +64,50 @@ where
     ) -> Result<http::Response<Self::Stream>, ApiClientError<Self::Error>> {
         self.inner.stream(request, path, body).await
     }
+}
+
+async fn get_nodes(
+    gateway_client: &GrpcClient,
+) -> Result<HashMap<u32, GrpcClient>, Box<dyn std::error::Error + Send + Sync>> {
+    let endpoint = GetNodes::builder().build()?;
+    let response = endpoint.query(gateway_client).await?;
+
+    let mut clients = HashMap::new();
+    for (node_id, url) in response.nodes {
+        let mut client_builder = GrpcClient::builder();
+
+        client_builder.set_tls(url.starts_with("https://"));
+        client_builder.set_host(url);
+
+        let client = client_builder.build().await?;
+        clients.insert(node_id, client);
+    }
+
+    Ok(clients)
+}
+
+async fn get_fastest_node(
+    clients: HashMap<u32, GrpcClient>,
+    timeout: Duration,
+) -> Result<GrpcClient, Box<dyn std::error::Error + Send + Sync>> {
+    let futures = clients.into_iter().map(|(node_id, client)| async move {
+        let start = Instant::now();
+        let endpoint = HealthCheck::builder().build().ok()?;
+        let result = xmtp_common::time::timeout(timeout, endpoint.query(&client)).await;
+
+        match result {
+            Ok(Ok(_)) => Some((node_id, client, start.elapsed().as_millis() as u64)),
+            _ => None,
+        }
+    });
+
+    let results = join_all(futures).await;
+
+    let fastest_node = results
+        .into_iter()
+        .flatten()
+        .min_by_key(|(_, _, latency)| *latency)
+        .ok_or("No responsive nodes found")?;
+
+    Ok(fastest_node.1)
 }
