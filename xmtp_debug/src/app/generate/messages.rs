263fb2496796804215b16258c3d4d4f4e6ab35e5
Andrew Plaza <github@andrewplaza.dev>
2025-10-20 12:46:11 -0400

    add streams to xmtp-debug

--- xmtp_debug/src/app/generate/messages.rs
+++ xmtp_debug/src/app/generate/messages.rs
@@ -1,8 +1,9 @@
+use crate::app::App;
 use crate::app::identity_lock::get_identity_lock;
 use crate::{
     app::{
         self,
-        store::{Database, GroupStore, IdentityStore, MetadataStore, RandomDatabase},
+        store::{Database, GroupStore, IdentityStore, RandomDatabase},
     },
     args,
 };
@@ -32,18 +33,24 @@ enum MessageSendError {
 }
 
 pub struct GenerateMessages {
-    db: Arc<redb::Database>,
     network: args::BackendOpts,
     opts: args::MessageGenerateOpts,
+    identity_store: IdentityStore<'static>,
+    group_store: GroupStore<'static>,
 }
 
 impl GenerateMessages {
-    pub fn new(
-        db: Arc<redb::Database>,
-        network: args::BackendOpts,
-        opts: args::MessageGenerateOpts,
-    ) -> Self {
-        Self { db, network, opts }
+    pub fn new(network: args::BackendOpts, opts: args::MessageGenerateOpts) -> Result<Self> {
+        let (identity_store, group_store) = {
+            let db = App::readonly_db()?;
+            (db.clone().into(), db.into())
+        };
+        Ok(Self {
+            network,
+            opts,
+            identity_store,
+            group_store,
+        })
     }
 
     pub async fn run(self, n: usize, concurrency: usize) -> Result<()> {
@@ -52,26 +59,19 @@ impl GenerateMessages {
             r#loop, interval, ..
         } = self.opts;
 
-        self.send_many_messages(self.db.clone(), n, concurrency)
-            .await?;
+        self.send_many_messages(n, concurrency).await?;
 
         if r#loop {
             loop {
                 info!(time = ?std::time::Instant::now(), amount = n, "sending messages");
                 tokio::time::sleep(*interval).await;
-                self.send_many_messages(self.db.clone(), n, concurrency)
-                    .await?;
+                self.send_many_messages(n, concurrency).await?;
             }
         }
         Ok(())
     }
 
-    async fn send_many_messages(
-        &self,
-        db: Arc<redb::Database>,
-        n: usize,
-        concurrency: usize,
-    ) -> Result<usize> {
+    async fn send_many_messages(&self, n: usize, concurrency: usize) -> Result<usize> {
         let Self { network, opts, .. } = self;
 
         let style = ProgressStyle::with_template(
@@ -82,15 +82,16 @@ impl GenerateMessages {
         let semaphore = Arc::new(tokio::sync::Semaphore::new(concurrency));
 
         let mut set: tokio::task::JoinSet<Result<(), eyre::Error>> = tokio::task::JoinSet::new();
+        let stores = (self.identity_store.clone(), self.group_store.clone());
         for _ in 0..n {
             let bar_pointer = bar.clone();
-            let d = db.clone();
             let n = network.clone();
             let opts = opts.clone();
+            let (identity, group) = stores.clone();
             let semaphore = semaphore.clone();
             set.spawn(async move {
                 let _permit = semaphore.acquire().await?;
-                Self::send_message(&d.clone().into(), &d.clone().into(), n, opts).await?;
+                Self::send_message(&group, &identity, n, opts).await?;
                 bar_pointer.inc(1);
                 Ok(())
             });
@@ -115,11 +116,6 @@ impl GenerateMessages {
             .into_iter()
             .filter(|r| r.is_ok())
             .collect::<Vec<Result<_, _>>>();
-        let key = crate::meta_key!(network);
-        let meta_db: MetadataStore = db.into();
-        meta_db.modify(key, |meta| {
-            meta.messages += msgs_sent.len() as u32;
-        })?;
         Ok(msgs_sent.len())
     }
 
