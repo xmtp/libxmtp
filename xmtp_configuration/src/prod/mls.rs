19caa26f91fd5f5dc4634ce84c3057d019fbe5fa
richardhuaaa <696206+richardhuaaa@users.noreply.github.com>
2025-10-21 07:36:43 +0000

    Perform readd for forked installations (#2450)
    This PR:
    
    1. Validates incoming readd requests (which come in via Oneshot welcomes), and discards the ones on non-consented conversations, or for which the sender is not a member of the requested group. The bulk of this logic is in `validate_readd_request()`.
    
    2. Validates which installations we have previously received readd requests from, and performs the readd using the API from downstack PR's. The bulk of this logic is in `validate_pending_readds()`.
    
    These changes match the logic described in the [XIP](https://community.xmtp.org/t/xip-68-draft-automated-fork-recovery/951#p-2260-receiving-a-readd-request-13).
    
    It doesn't show up well in the graphite stack, but once done with this PR, please do not miss the top PR of the stack: https://github.com/xmtp/libxmtp/pull/2572

--- xmtp_configuration/src/prod/mls.rs
+++ xmtp_configuration/src/prod/mls.rs
@@ -5,3 +5,4 @@ pub const SYNC_UPDATE_INSTALLATIONS_INTERVAL_NS: i64 = NS_IN_HOUR / 2; // 30 min
 pub const KEYS_EXPIRATION_INTERVAL_NS: i64 = NS_IN_DAY; // 1 day
 
 pub const ENABLE_RECOVERY_REQUESTS: bool = false;
+pub const ENABLE_RECOVERY_RESPONSES: bool = false;
