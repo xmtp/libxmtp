cd387df34d941e0675ededb8934cf304ddc1fc17
Andrew Plaza <github@andrewplaza.dev>
2025-10-30 16:31:16 -0700

    define a ReadWrite client middleware

---
+++ xmtp_api_d14n/src/middleware/read_write_client/test.rs
@@ -0,0 +1,50 @@
+#![allow(clippy::unwrap_used)]
+
+use xmtp_proto::prelude::ApiBuilder;
+
+use super::*;
+
+impl<BRead, BWrite> ApiBuilder for ReadWriteClientBuilder<BRead, BWrite>
+where
+    BRead: ApiBuilder,
+    BWrite: ApiBuilder,
+{
+    type Output = ReadWriteClient<BRead::Output, BWrite::Output>;
+    type Error = ();
+    fn set_libxmtp_version(&mut self, _: String) -> Result<(), Self::Error> {
+        unimplemented!("no way to set host for a client that needs 2 hosts")
+    }
+
+    fn set_app_version(&mut self, _: xmtp_proto::types::AppVersion) -> Result<(), Self::Error> {
+        unimplemented!("no way to set host for a client that needs 2 hosts")
+    }
+
+    fn set_host(&mut self, _host: String) {
+        unimplemented!("no way to set host for a client that needs 2 hosts")
+    }
+
+    fn set_tls(&mut self, _: bool) {
+        unimplemented!("no way to set host for a client that needs 2 hosts")
+    }
+
+    fn rate_per_minute(&mut self, _: u32) {
+        unimplemented!("no way to set host for a client that needs 2 hosts")
+    }
+
+    fn port(&self) -> Result<Option<String>, Self::Error> {
+        unimplemented!("no way to set host for a client that needs 2 hosts")
+    }
+
+    fn host(&self) -> Option<&str> {
+        unimplemented!("no way to set host for a client that needs 2 hosts")
+    }
+
+    fn build(self) -> Result<Self::Output, Self::Error> {
+        let c = self.build_builder().unwrap();
+        Ok(c)
+    }
+
+    fn set_retry(&mut self, _: xmtp_common::Retry) {
+        unimplemented!("no way to set host for a client that needs 2 hosts")
+    }
+}
