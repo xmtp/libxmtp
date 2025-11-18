be2ff831cefa699b7c5ade2f687fe144e4d78246
Ry Racherbaumer <ry@xmtp.com>
2025-11-18 12:32:31 -0600

    RemoteAttachment

--- bindings_node/src/content_types/remote_attachment.rs
+++ bindings_node/src/content_types/remote_attachment.rs
@@ -1,4 +1,10 @@
+use napi::bindgen_prelude::{Result, Uint8Array};
 use napi_derive::napi;
+use prost::Message;
+use xmtp_content_types::{ContentCodec, remote_attachment::RemoteAttachmentCodec};
+use xmtp_proto::xmtp::mls::message_contents::EncodedContent;
+
+use crate::ErrorWrapper;
 
 #[derive(Clone)]
 #[napi(object)]
@@ -26,4 +32,44 @@ impl From<xmtp_content_types::remote_attachment::RemoteAttachment> for RemoteAtt
       filename: remote.filename,
     }
   }
+}
+
+impl From<RemoteAttachment> for xmtp_content_types::remote_attachment::RemoteAttachment {
+  fn from(remote: RemoteAttachment) -> Self {
+    Self {
+      url: remote.url,
+      content_digest: remote.content_digest,
+      secret: remote.secret,
+      salt: remote.salt,
+      nonce: remote.nonce,
+      scheme: remote.scheme,
+      content_length: remote.content_length as usize,
+      filename: remote.filename,
+    }
+  }
+}
+
+#[napi]
+pub fn encode_remote_attachment(remote_attachment: RemoteAttachment) -> Result<Uint8Array> {
+  // Use RemoteAttachmentCodec to encode the remote attachment
+  let encoded =
+    RemoteAttachmentCodec::encode(remote_attachment.into()).map_err(ErrorWrapper::from)?;
+
+  // Encode the EncodedContent to bytes
+  let mut buf = Vec::new();
+  encoded.encode(&mut buf).map_err(ErrorWrapper::from)?;
+
+  Ok(buf.into())
+}
+
+#[napi]
+pub fn decode_remote_attachment(bytes: Uint8Array) -> Result<RemoteAttachment> {
+  // Decode bytes into EncodedContent
+  let encoded_content =
+    EncodedContent::decode(bytes.to_vec().as_slice()).map_err(ErrorWrapper::from)?;
+
+  // Use RemoteAttachmentCodec to decode into RemoteAttachment and convert to RemoteAttachment
+  RemoteAttachmentCodec::decode(encoded_content)
+    .map(Into::into)
+    .map_err(|e| napi::Error::from_reason(e.to_string()))
 }
--- bindings_wasm/src/content_types/remote_attachment.rs
+++ bindings_wasm/src/content_types/remote_attachment.rs
@@ -1,4 +1,9 @@
-use wasm_bindgen::prelude::wasm_bindgen;
+use js_sys::Uint8Array;
+use prost::Message;
+use wasm_bindgen::{JsError, prelude::wasm_bindgen};
+use xmtp_content_types::ContentCodec;
+use xmtp_content_types::remote_attachment::RemoteAttachmentCodec;
+use xmtp_proto::xmtp::mls::message_contents::EncodedContent;
 
 #[wasm_bindgen(getter_with_clone)]
 #[derive(Clone)]
@@ -28,4 +33,48 @@ impl From<xmtp_content_types::remote_attachment::RemoteAttachment> for RemoteAtt
       filename: remote.filename,
     }
   }
+}
+
+impl From<RemoteAttachment> for xmtp_content_types::remote_attachment::RemoteAttachment {
+  fn from(remote: RemoteAttachment) -> Self {
+    Self {
+      url: remote.url,
+      content_digest: remote.content_digest,
+      secret: remote.secret,
+      salt: remote.salt,
+      nonce: remote.nonce,
+      scheme: remote.scheme,
+      content_length: remote.content_length as usize,
+      filename: remote.filename,
+    }
+  }
+}
+
+#[wasm_bindgen(js_name = "encodeRemoteAttachment")]
+pub fn encode_remote_attachment(
+  #[wasm_bindgen(js_name = "remoteAttachment")] remote_attachment: RemoteAttachment,
+) -> Result<Uint8Array, JsError> {
+  // Use RemoteAttachmentCodec to encode the remote attachment
+  let encoded = RemoteAttachmentCodec::encode(remote_attachment.into())
+    .map_err(|e| JsError::new(&format!("{}", e)))?;
+
+  // Encode the EncodedContent to bytes
+  let mut buf = Vec::new();
+  encoded
+    .encode(&mut buf)
+    .map_err(|e| JsError::new(&format!("{}", e)))?;
+
+  Ok(Uint8Array::from(buf.as_slice()))
+}
+
+#[wasm_bindgen(js_name = "decodeRemoteAttachment")]
+pub fn decode_remote_attachment(bytes: Uint8Array) -> Result<RemoteAttachment, JsError> {
+  // Decode bytes into EncodedContent
+  let encoded_content = EncodedContent::decode(bytes.to_vec().as_slice())
+    .map_err(|e| JsError::new(&format!("{}", e)))?;
+
+  // Use RemoteAttachmentCodec to decode into RemoteAttachment and convert to RemoteAttachment
+  RemoteAttachmentCodec::decode(encoded_content)
+    .map(Into::into)
+    .map_err(|e| JsError::new(&format!("{}", e)))
 }
