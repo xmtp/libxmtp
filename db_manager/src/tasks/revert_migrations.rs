9eaca819d9b27093d3495765afa609222d6e6dc4
Dakota Brink <git@kota.is>
2025-11-21 10:57:58 -0500

    cleanup

--- db_manager/src/tasks/revert_migrations.rs
+++ db_manager/src/tasks/revert_migrations.rs
@@ -1,7 +1,10 @@
 use anyhow::Result;
 use diesel_migrations::MigrationHarness;
 use tracing::info;
-use xmtp_db::{EncryptedMessageStore, NativeDb, diesel::migration::MigrationVersion};
+use xmtp_db::{
+    EncryptedMessageStore, NativeDb,
+    diesel::{migration::MigrationVersion, result::Error as DieselError},
+};
 
 use crate::confirm_destructive;
 
@@ -12,7 +15,9 @@ pub fn revert_migrations(store: &EncryptedMessageStore<NativeDb>, target: &str) 
     while let Some(version) = applied_migrations(store)?.first() {
         if version.to_string() > target {
             let result = store.db().raw_query_write(|conn| {
-                Ok(conn.revert_last_migration(xmtp_db::MIGRATIONS).unwrap())
+                Ok(conn
+                    .revert_last_migration(xmtp_db::MIGRATIONS)
+                    .map_err(DieselError::QueryBuilderError))
             });
             if let Err(err) = result {
                 tracing::warn!("{err:?}");
@@ -30,8 +35,9 @@ pub fn revert_migrations(store: &EncryptedMessageStore<NativeDb>, target: &str) 
 fn applied_migrations(
     store: &EncryptedMessageStore<NativeDb>,
 ) -> Result<Vec<MigrationVersion<'static>>> {
-    let applied_migrations: Vec<MigrationVersion<'static>> = store
-        .db()
-        .raw_query_read(|conn| Ok(conn.applied_migrations().unwrap()))?;
+    let applied_migrations: Vec<MigrationVersion<'static>> = store.db().raw_query_read(|conn| {
+        conn.applied_migrations()
+            .map_err(DieselError::QueryBuilderError)
+    })?;
     Ok(applied_migrations)
 }
