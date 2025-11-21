f9d8c3a79d0b0abb636d46a78b7adbbad6a298ef
Dakota Brink <git@kota.is>
2025-11-21 09:03:13 -0500

    group management

--- manager/src/tasks/clear_messages.rs
+++ manager/src/tasks/clear_messages.rs
@@ -1,4 +1,4 @@
-use anyhow::{Result, bail};
+use anyhow::Result;
 use xmtp_db::{
     ConnectionExt, EncryptedMessageStore, NativeDb,
     diesel::{self, ExpressionMethods, QueryDsl, RunQueryDsl},
--- manager/src/tasks/mod.rs
+++ manager/src/tasks/mod.rs
@@ -1,9 +1,11 @@
 mod clear_messages;
 mod db_bench;
+mod group_management;
 mod revert_migrations;
 mod snapshot_to_persistent;
 
 pub use clear_messages::*;
 pub use db_bench::*;
+pub use group_management::*;
 pub use revert_migrations::*;
 pub use snapshot_to_persistent::*;
