5394a155f11d0ebf145405fcc54287b8187443fa
Andrew Plaza <github@andrewplaza.dev>
2025-11-21 21:18:48 +0000

    fixup wasm ci script

--- nix/lib/packages/wasm-bindgen-cli.nix
+++ nix/lib/packages/wasm-bindgen-cli.nix
@@ -2,14 +2,14 @@
 let
   src = fetchCrate {
     pname = "wasm-bindgen-cli";
-    version = "0.2.104";
-    hash = "sha256-9kW+a7IreBcZ3dlUdsXjTKnclVW1C1TocYfY8gUgewE=";
+    version = "0.2.105";
+    hash = "sha256-zLPFFgnqAWq5R2KkaTGAYqVQswfBEYm9x3OPjx8DJRY=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit src;
     inherit (src) pname version;
-    hash = "sha256-V0AV5jkve37a5B/UvJ9B3kwOW72vWblST8Zxs8oDctE=";
+    hash = "sha256-a2X9bzwnMWNt0fTf30qAiJ4noal/ET1jEtf5fBFj5OU=";
   };
 in
 rustPlatform.buildRustPackage {
