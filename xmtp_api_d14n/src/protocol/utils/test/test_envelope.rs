debc116d019c588a64ba1afafd5f66941b466b46
Andrew Plaza <github@andrewplaza.dev>
2025-12-01 17:19:15 -0500

    add icebox to ordering impl

--- xmtp_api_d14n/src/protocol/utils/test/test_envelope.rs
+++ xmtp_api_d14n/src/protocol/utils/test/test_envelope.rs
@@ -2,23 +2,46 @@ use crate::protocol::{Envelope, EnvelopeError};
 use chrono::Utc;
 use std::sync::LazyLock;
 use xmtp_proto::types::{Cursor, GlobalCursor, Topic, TopicKind};
+use xmtp_proto::xmtp::xmtpv4;
 use xmtp_proto::xmtp::xmtpv4::envelopes::ClientEnvelope;
 use xmtp_proto::xmtp::xmtpv4::envelopes::client_envelope::Payload;
 
 static TOPIC: LazyLock<Topic> =
     LazyLock::new(|| Topic::new(TopicKind::GroupMessagesV1, vec![0, 1, 2]));
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, PartialEq, prost::Message)]
 pub struct TestEnvelope {
-    pub cursor: Cursor,
-    pub depends_on: GlobalCursor,
+    #[prost(uint64, tag = "1")]
+    sequence_id: u64,
+    #[prost(uint32, tag = "2")]
+    originator_id: u32,
+    #[prost(message, tag = "3")]
+    depends_on: Option<xmtpv4::envelopes::Cursor>,
+}
+
+impl TestEnvelope {
+    pub fn new(sequence_id: u64, originator_id: u32, depends_on: GlobalCursor) -> Self {
+        Self {
+            sequence_id,
+            originator_id,
+            depends_on: Some(depends_on.into()),
+        }
+    }
+
+    pub fn cursor(&self) -> Cursor {
+        Cursor::new(self.sequence_id, self.originator_id)
+    }
+
+    pub fn depends_on(&self) -> GlobalCursor {
+        self.depends_on.clone().unwrap().into()
+    }
 }
 
 impl TestEnvelope {
     pub fn has_dependency_on(&self, other: &TestEnvelope) -> bool {
-        let originator = other.cursor.originator_id;
-        let depends_on_sid = self.depends_on.get(&originator);
-        depends_on_sid == other.cursor.sequence_id
+        let originator = other.cursor().originator_id;
+        let depends_on_sid = self.depends_on().get(&originator);
+        depends_on_sid == other.cursor().sequence_id
     }
 
     pub fn has_dependency_on_any(&self, other: &[TestEnvelope]) -> bool {
@@ -28,7 +51,12 @@ impl TestEnvelope {
 
 impl std::fmt::Display for TestEnvelope {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "cursor {} depends on {}", &self.cursor, &self.depends_on)
+        write!(
+            f,
+            "cursor {} depends on {}",
+            &self.cursor(),
+            &self.depends_on()
+        )
     }
 }
 
@@ -44,8 +72,8 @@ impl Envelope<'_> for TestEnvelope {
     fn timestamp(&self) -> Option<chrono::DateTime<Utc>> {
         // Create a deterministic timestamp based on originator_id and sequence_id
         // This ensures envelopes can be sorted by timestamp in tests
-        let nanos = (self.cursor.originator_id as i64 * 1_000_000_000)
-            + (self.cursor.sequence_id as i64 * 1000);
+        let nanos = (self.cursor().originator_id as i64 * 1_000_000_000)
+            + (self.cursor().sequence_id as i64 * 1000);
         chrono::DateTime::from_timestamp_nanos(nanos).into()
     }
 
@@ -65,22 +93,12 @@ impl Envelope<'_> for TestEnvelope {
         unreachable!()
     }
 
-    fn consume<E>(&self, _extractor: E) -> Result<E::Output, crate::protocol::EnvelopeError>
-    where
-        Self: Sized,
-        for<'a> crate::protocol::EnvelopeError:
-            From<<E as crate::protocol::EnvelopeVisitor<'a>>::Error>,
-        for<'a> E: crate::protocol::EnvelopeVisitor<'a> + crate::protocol::Extractor,
-    {
-        unreachable!()
-    }
-
     fn cursor(&self) -> Result<xmtp_proto::types::Cursor, EnvelopeError> {
-        Ok(self.cursor)
+        Ok(self.cursor())
     }
 
     fn depends_on(&self) -> Result<Option<xmtp_proto::types::GlobalCursor>, EnvelopeError> {
-        Ok(Some(self.depends_on.clone()))
+        Ok(Some(self.depends_on.clone().unwrap().into()))
     }
 
     fn sha256_hash(&self) -> Result<Vec<u8>, EnvelopeError> {
