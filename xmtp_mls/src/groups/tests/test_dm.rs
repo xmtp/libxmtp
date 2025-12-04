0cf3c5bd3234e2dab86ecf057e5df77d97a06f4e
Dakota Brink <git@kota.is>
2025-12-04 15:40:14 -0500

    revert

--- xmtp_mls/src/groups/tests/test_dm.rs
+++ xmtp_mls/src/groups/tests/test_dm.rs
@@ -1,11 +1,5 @@
-use std::time::Duration;
-
-use tokio_stream::StreamExt;
-use xmtp_common::NS_IN_DAY;
-use xmtp_common::time::now_ns;
 use xmtp_db::consent_record::StoredConsentRecord;
 use xmtp_db::consent_record::{ConsentState, ConsentType};
-use xmtp_db::group::{ConversationType, GroupQueryArgs};
 use xmtp_db::prelude::*;
 
 use crate::tester;
@@ -71,48 +65,4 @@ async fn test_dm_welcome_with_preexisting_consent() {
             .group_id,
         a_group.group_id
     );
-}
-
-#[xmtp_common::test(unwrap_try = true)]
-async fn test_old_msgs() {
-    tester!(alix);
-    tester!(bo);
-
-    let dm_bo_alix = bo
-        .find_or_create_dm_by_inbox_id(alix.inbox_id(), None)
-        .await?;
-    dm_bo_alix.send_message(b"Hi", Default::default()).await?;
-
-    alix.sync_all_welcomes_and_groups(None).await?;
-    let mut stream = alix
-        .stream_all_messages_owned_with_stats(None, None)
-        .await?;
-
-    xmtp_common::spawn(None, async move {
-        tokio::time::sleep(Duration::from_secs(1)).await;
-        dm_bo_alix
-            .send_message(b"Hi again", Default::default())
-            .await?;
-    });
-
-    while let Some(msg) = stream.next().await {
-        let msg = msg?;
-        let txt = String::from_utf8_lossy(&msg.decrypted_message_bytes);
-        tracing::error!("{txt}");
-    }
-}
-
-#[xmtp_common::test(unwrap_try = true)]
-async fn test_gm() {
-    tester!(gm, snapshot_file: "../dev.db3", dev);
-
-    tracing::info!("inbox {}", gm.inbox_id());
-
-    gm.sync_all_welcomes_and_groups(None).await?;
-    let mut stream = gm.stream_all_messages(None, None).await?;
-
-    while let Some(v) = stream.next().await {
-        let msg = String::from_utf8_lossy(&v.unwrap().decrypted_message_bytes).to_string();
-        tracing::info!("{msg}");
-    }
 }
