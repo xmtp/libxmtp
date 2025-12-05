7eec7fa2249dba27de81aaab45e1ecf039a51c45
Andrew Plaza <github@andrewplaza.dev>
2025-12-04 19:41:54 -0500

    test ordering implementation with icebox

--- xmtp_proto/src/types/orphaned_envelope.rs
+++ xmtp_proto/src/types/orphaned_envelope.rs
@@ -3,9 +3,10 @@ use derive_builder::Builder;
 use crate::ConversionError;
 use crate::types::{Cursor, GlobalCursor, GroupId};
 use bytes::Bytes;
+use std::hash::Hash;
 
 /// An envelope whose parent dependencies have not yet been seen
-#[derive(Builder, Clone, Debug, Hash, PartialEq, Eq)]
+#[derive(Builder, Clone, Debug)]
 #[builder(setter(into), build_fn(error = "ConversionError"))]
 pub struct OrphanedEnvelope {
     // the cursor of this envelope
@@ -18,6 +19,28 @@ pub struct OrphanedEnvelope {
     /// the group this orphan belongs to
     pub group_id: GroupId,
 }
+
+// prost grpc encoding is _not_ deterministic.
+// https://github.com/tokio-rs/prost/issues/965
+// so we ned to write a custom Hash implementation to
+// ignore the payload field
+impl Hash for OrphanedEnvelope {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.cursor.hash(state);
+        self.group_id.hash(state);
+    }
+}
+
+// needed for parity with Hash impl
+impl PartialEq<OrphanedEnvelope> for OrphanedEnvelope {
+    fn eq(&self, other: &OrphanedEnvelope) -> bool {
+        self.cursor == other.cursor
+            && self.depends_on == other.depends_on
+            && self.group_id == other.group_id
+    }
+}
+
+impl Eq for OrphanedEnvelope {}
 
 impl OrphanedEnvelope {
     pub fn builder() -> OrphanedEnvelopeBuilder {
