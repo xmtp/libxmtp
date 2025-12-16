6b0d5238db259cf1504e5d25743e91ed189f53ec
Dakota Brink <git@kota.is>
2025-12-16 14:29:29 -0500

    revert

--- xmtp_mls/src/utils/test/mod.rs
+++ xmtp_mls/src/utils/test/mod.rs
@@ -61,7 +61,7 @@ impl<A, S> ClientBuilder<A, S> {
 
 impl<Api, Storage, Db> ClientBuilder<Api, Storage, Db>
 where
-    Api: XmtpApi + XmtpQuery + Clone + 'static,
+    Api: XmtpApi + XmtpQuery + 'static,
     Storage: XmtpMlsStorageProvider + 'static,
     Db: xmtp_db::XmtpDb + 'static,
 {
