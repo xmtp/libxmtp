26a2cd5a485ea79273648959bf0f94a949fad2e6
Dakota Brink <git@kota.is>
2025-12-16 14:25:31 -0500

    test snaps

--- xmtp_mls/src/tests/test_data_migration.rs
+++ xmtp_mls/src/tests/test_data_migration.rs
@@ -1,5 +1,8 @@
 use crate::tester;
 
+const ALIX_DB: &str = "tests/assets/alix.xmtp";
+const BO_DB: &str = "tests/assets/bo.xmtp";
+
 #[xmtp_common::test(unwrap_try = true)]
 async fn setup_migration_test() {
     tester!(alix);
@@ -9,14 +12,14 @@ async fn setup_migration_test() {
     bo.test_talk_in_dm_with(&alix).await?;
     alix.test_talk_in_new_group_with(&bo).await?;
 
-    alix.save_snapshot_to_file("alix.xmtp");
-    bo.save_snapshot_to_file("bo.xmtp");
+    alix.save_snapshot_to_file(ALIX_DB);
+    bo.save_snapshot_to_file(BO_DB);
 }
 
 #[xmtp_common::test(unwrap_try = true)]
 async fn test_existing_client_db() {
-    tester!(alix, snapshot_file: "alix.xmtp");
-    tester!(bo, snapshot_file: "bo.xmtp");
+    tester!(alix, snapshot_file: ALIX_DB);
+    tester!(bo, snapshot_file: BO_DB);
 
     alix.test_talk_in_dm_with(&bo).await?;
     bo.test_talk_in_dm_with(&alix).await?;
