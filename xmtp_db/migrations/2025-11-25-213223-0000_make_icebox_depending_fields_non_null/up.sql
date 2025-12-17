a896a4e39c8b984930dae17b6bf3470e5b6a893b
Andrew Plaza <github.tech@liquidthink.net>
2025-12-17 14:22:50 -0500

    fix originator sequence id order in icebox (#2968)
    on diesel selects, the sequence and originator ids would be swapped

--- xmtp_db/migrations/2025-11-25-213223-0000_make_icebox_depending_fields_non_null/up.sql
+++ xmtp_db/migrations/2025-11-25-213223-0000_make_icebox_depending_fields_non_null/up.sql
@@ -7,10 +7,10 @@ DELETE FROM icebox;
 
 -- Step 1: Create the new icebox_dependencies table
 CREATE TABLE icebox_dependencies (
-    envelope_sequence_id BIGINT NOT NULL,
     envelope_originator_id BIGINT NOT NULL,
-    dependency_sequence_id BIGINT NOT NULL,
+    envelope_sequence_id BIGINT NOT NULL,
     dependency_originator_id BIGINT NOT NULL,
+    dependency_sequence_id BIGINT NOT NULL,
     PRIMARY KEY (envelope_originator_id, envelope_sequence_id, dependency_originator_id, dependency_sequence_id),
     -- when an envelope is deleted, also delete its dependency records
     FOREIGN KEY (envelope_originator_id, envelope_sequence_id) REFERENCES icebox(originator_id, sequence_id) ON DELETE CASCADE
@@ -23,8 +23,8 @@ DROP TABLE icebox;
 
 -- Step 4: Create new icebox table with group_id
 CREATE TABLE icebox (
-    sequence_id BIGINT NOT NULL,
     originator_id BIGINT NOT NULL,
+    sequence_id BIGINT NOT NULL,
     group_id BLOB NOT NULL,
     envelope_payload BLOB NOT NULL,
     PRIMARY KEY (originator_id, sequence_id),
