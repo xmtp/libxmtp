b330c3ab114a116e16f7408d8718d42024602dee
cameronvoell <cameronvoell@users.noreply.github.com>
2026-01-30 15:25:44 -0800

    reduce MAX_DB_POOL_SIZE from 25 to 12

--- crates/xmtp_configuration/src/common/db.rs
+++ crates/xmtp_configuration/src/common/db.rs
@@ -5,7 +5,7 @@ pub const WASM_VFS_NAME: &str = "opfs-libxmtp";
 /// VFS Directory for OPFS
 pub const WASM_VFS_DIRECTORY: &str = ".opfs-libxmtp-metadata";
 /// Max Size the Database Pool is allowed to grow to
-pub const MAX_DB_POOL_SIZE: u32 = 25;
+pub const MAX_DB_POOL_SIZE: u32 = 12;
 /// max time the database will wait to acquire a lock for a table
 pub const BUSY_TIMEOUT: i32 = 5_000;
 /// Minimum amount of connections to keep open & idle to the database in the pool
