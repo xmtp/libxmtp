0042b39422b9a2b524509ba68a9d659d4b1fdde9
Dakota Brink <git@kota.is>
2026-02-02 10:00:05 -0500

    new queries

--- crates/db_tools/src/tasks/db_bench.rs
+++ crates/db_tools/src/tasks/db_bench.rs
@@ -9,13 +9,20 @@ use xmtp_db::{
     group::{ConversationType, GroupQueryArgs, QueryGroup, StoredGroup},
     group_intent::{IntentKind, IntentState, QueryGroupIntent},
     group_message::{MsgQueryArgs, QueryGroupMessage, RelationQuery},
+    icebox::QueryIcebox,
+    identity::QueryIdentity,
     identity_update::QueryIdentityUpdates,
     key_package_history::QueryKeyPackageHistory,
     local_commit_log::{LocalCommitLogOrder, QueryLocalCommitLog},
+    message_deletion::QueryMessageDeletion,
+    pending_remove::QueryPendingRemove,
+    prelude::{QueryDms, QueryGroupVersion},
     processed_device_sync_messages::QueryDeviceSyncMessages,
     proto::types::{Cursor, GlobalCursor},
+    readd_status::QueryReaddStatus,
     refresh_state::{EntityKind, QueryRefreshState},
     remote_commit_log::{QueryRemoteCommitLog, RemoteCommitLogOrder},
+    tasks::QueryTasks,
 };
 
 macro_rules! bench {
@@ -28,8 +35,10 @@ macro_rules! bench {
 }
 
 pub struct DbBencher<Db> {
+    rand_dm: Option<StoredGroup>,
     rand_group: Option<StoredGroup>,
     rand_inbox_id: Option<String>,
+    rand_installation_id: Option<Vec<u8>>,
     measurements: HashMap<String, f32>,
     store: Db,
 }
@@ -55,13 +64,21 @@ where
         // Try to get a random inbox_id from identity updates or association state
         let rand_inbox_id = groups.first().map(|g| g.added_by_inbox_id.clone());
 
+        // Generate a random installation ID for benchmarks that need it
+        let rand_installation_id = Some(vec![0u8; 32]);
+
         Ok(Self {
-            rand_group: dms.pop(),
+            rand_dm: dms.pop(),
             rand_group: groups.pop(),
             rand_inbox_id,
+            rand_installation_id,
             store,
             measurements: HashMap::default(),
         })
+    }
+
+    fn group_or_dm(&self) -> Option<&StoredGroup> {
+        self.rand_dm.as_ref().or(self.rand_group.as_ref())
     }
 
     fn bench_with_key<T, F>(&mut self, key: &str, mut f: F) -> T
@@ -103,6 +120,15 @@ where
                 results.push(self.bench_key_package_history_queries());
                 results.push(self.bench_conversation_list_queries());
                 results.push(self.bench_commit_log_queries());
+                results.push(self.bench_dm_queries());
+                results.push(self.bench_message_deletion_queries());
+                results.push(self.bench_device_sync_queries());
+                results.push(self.bench_task_queries());
+                results.push(self.bench_icebox_queries());
+                results.push(self.bench_readd_status_queries());
+                results.push(self.bench_pending_remove_queries());
+                results.push(self.bench_identity_queries());
+                results.push(self.bench_group_version_queries());
 
                 Err::<(), xmtp_db::diesel::result::Error>(
                     xmtp_db::diesel::result::Error::RollbackTransaction,
@@ -249,6 +275,10 @@ where
                 get_group_message_by_timestamp(&group.id, message.sent_at_ns)
             )?;
 
+            // Cursor-based query
+            let cursor = Cursor::new(message.sequence_id as u64, message.originator_id as u32);
+            bench!(self, get_group_message_by_cursor(&group.id, cursor))?;
+
             // Delivery status updates (non-destructive operations should be safe to benchmark)
             bench!(
                 self,
@@ -275,6 +305,7 @@ where
             bail!("No groups to run group queries on.");
         };
         bench!(self, get_conversation_ids_for_remote_log_download())?;
+        bench!(self, get_conversation_ids_for_remote_log_publish())?;
         bench!(self, get_conversation_ids_for_fork_check())?;
         bench!(self, get_conversation_ids_for_requesting_readds())?;
         bench!(self, get_conversation_ids_for_responding_readds())?;
@@ -284,9 +315,11 @@ where
         bench!(self, all_sync_groups())?;
         bench!(self, primary_sync_group())?;
         bench!(self, find_group(&group.id))?;
+        bench!(self, find_sync_group(&group.id))?;
         bench!(self, get_rotated_at_ns(group.id.clone()))?;
         bench!(self, update_rotated_at_ns(group.id.clone()))?;
         bench!(self, get_installations_time_checked(group.id.clone()))?;
+        bench!(self, update_installations_time_checked(group.id.clone()))?;
         bench!(
             self,
             update_message_disappearing_from_ns(group.id.clone(), Some(1000))
@@ -296,11 +329,22 @@ where
             update_message_disappearing_in_ns(group.id.clone(), Some(1000))
         )?;
 
+        // Fork status queries
+        bench!(self, get_group_commit_log_forked_status(&group.id))?;
+        bench!(self, get_groups_have_pending_leave_request())?;
+
+        // Check for duplicate DM
+        bench!(self, has_duplicate_dm(&group.id))?;
+
+        // Find group by sequence ID (using a dummy cursor)
+        let cursor = Cursor::new(0, 0u32);
+        bench!(self, find_group_by_sequence_id(cursor))?;
+
         Ok(())
     }
 
     fn bench_consent_queries(&mut self) -> Result<()> {
-        let Some(group) = &self.rand_group else {
+        let Some(group) = self.group_or_dm() else {
             bail!("No group to lookup DMs on");
         };
         let group_id = hex::encode(&group.id);
@@ -327,11 +371,10 @@ where
             self,
             maybe_insert_consent_record_return_existing(&new_consent.clone())
         )?;
-        if let Some(dm) = &self.rand_group {
-            let Some(dm_id) = dm.dm_id.clone() else {
-                bail!("Unexpected: DM does not have a dm_id");
-            };
-            bench!(self, find_consent_by_dm_id(&dm_id))?;
+        if let Some(dm) = &self.rand_dm {
+            if let Some(dm_id) = dm.dm_id.clone() {
+                bench!(self, find_consent_by_dm_id(&dm_id))?;
+            }
         }
 
         Ok(())
@@ -369,6 +412,9 @@ where
         let inbox_ids = vec![inbox_id.as_str()];
         bench!(self, get_latest_sequence_id(&inbox_ids))?;
 
+        // Count inbox updates
+        bench!(self, count_inbox_updates(&inbox_ids))?;
+
         Ok(())
     }
 
@@ -399,6 +445,10 @@ where
         // Find intent by payload hash (with a dummy hash that likely doesn't exist)
         let dummy_hash = vec![0u8; 32];
         bench!(self, find_group_intent_by_payload_hash(&dummy_hash))?;
+
+        // Find dependant commits
+        let payload_hashes: Vec<Vec<u8>> = vec![dummy_hash.clone()];
+        bench!(self, find_dependant_commits(&payload_hashes))?;
 
         Ok(())
     }
@@ -488,7 +538,7 @@ where
     }
 
     fn bench_commit_log_queries(&mut self) -> Result<()> {
-        let Some(group) = self.rand_group.clone() else {
+        let Some(group) = self.group_or_dm().cloned() else {
             bail!("No group to run commit log queries on.");
         };
 
@@ -511,6 +561,151 @@ where
                 RemoteCommitLogOrder::AscendingByRowid
             )
         )?;
+
+        Ok(())
+    }
+
+    fn bench_dm_queries(&mut self) -> Result<()> {
+        let Some(dm) = self.rand_dm.clone() else {
+            bail!("No DM available for DM queries.");
+        };
+
+        // Fetch stitched DM
+        bench!(self, fetch_stitched(&dm.id))?;
+
+        // Load other DMs stitched into this group
+        bench!(self, other_dms(&dm.id))?;
+
+        // Find active DM group (using dm_id if available)
+        if let Some(dm_id) = &dm.dm_id {
+            let dm_id_clone = dm_id.clone();
+            bench!(self, find_active_dm_group(&dm_id_clone))?;
+        }
+
+        Ok(())
+    }
+
+    fn bench_message_deletion_queries(&mut self) -> Result<()> {
+        let Some(group) = self.rand_group.clone() else {
+            bail!("No group to run message deletion queries on.");
+        };
+
+        // Get group deletions
+        bench!(self, get_group_deletions(&group.id))?;
+
+        // Try to get a message ID for deletion queries
+        let messages = self
+            .store
+            .db()
+            .get_group_messages(&group.id, &MsgQueryArgs::default())?;
+
+        if let Some(message) = messages.first() {
+            let message_id = message.id.clone();
+
+            // Get message deletion by ID
+            bench!(self, get_message_deletion(&message_id))?;
+
+            // Get deletion by deleted message ID
+            bench!(self, get_deletion_by_deleted_message_id(&message_id))?;
+
+            // Check if message is deleted
+            bench!(self, is_message_deleted(&message_id))?;
+
+            // Get deletions for multiple messages
+            let message_ids = vec![message_id.clone()];
+            bench!(self, get_deletions_for_messages(message_ids.clone()))?;
+        }
+
+        Ok(())
+    }
+
+    fn bench_device_sync_queries(&mut self) -> Result<()> {
+        // Unprocessed sync group messages
+        bench!(self, unprocessed_sync_group_messages())?;
+
+        // Sync group messages paged (already covered but adding here for completeness)
+        bench!(self, sync_group_messages_paged(0, 100))?;
+
+        Ok(())
+    }
+
+    fn bench_task_queries(&mut self) -> Result<()> {
+        // Get all tasks
+        bench!(self, get_tasks())?;
+
+        // Get next task
+        bench!(self, get_next_task())?;
+
+        Ok(())
+    }
+
+    fn bench_icebox_queries(&mut self) -> Result<()> {
+        // Past dependents with empty cursors
+        let empty_cursors: Vec<Cursor> = vec![];
+        bench!(self, past_dependents(&empty_cursors))?;
+
+        // Future dependents with empty cursors
+        bench!(self, future_dependents(&empty_cursors))?;
+
+        // Prune icebox
+        bench!(self, prune_icebox())?;
+
+        Ok(())
+    }
+
+    fn bench_readd_status_queries(&mut self) -> Result<()> {
+        let Some(group) = self.rand_group.clone() else {
+            bail!("No group to run readd status queries on.");
+        };
+
+        let Some(installation_id) = self.rand_installation_id.clone() else {
+            bail!("No installation_id available for readd status queries.");
+        };
+
+        // Get readd status
+        bench!(self, get_readd_status(&group.id, &installation_id))?;
+
+        // Check if awaiting readd
+        bench!(self, is_awaiting_readd(&group.id, &installation_id))?;
+
+        // Get readds awaiting response
+        bench!(
+            self,
+            get_readds_awaiting_response(&group.id, &installation_id)
+        )?;
+
+        Ok(())
+    }
+
+    fn bench_pending_remove_queries(&mut self) -> Result<()> {
+        let Some(group) = self.rand_group.clone() else {
+            bail!("No group to run pending remove queries on.");
+        };
+
+        // Get pending remove users
+        bench!(self, get_pending_remove_users(&group.id))?;
+
+        // Get user pending remove status
+        let inbox_id = self.rand_inbox_id.clone().unwrap_or_default();
+        bench!(self, get_user_pending_remove_status(&group.id, &inbox_id))?;
+
+        Ok(())
+    }
+
+    fn bench_identity_queries(&mut self) -> Result<()> {
+        // Check if identity needs rotation
+        bench!(self, is_identity_needs_rotation())?;
+
+        Ok(())
+    }
+
+    fn bench_group_version_queries(&mut self) -> Result<()> {
+        let Some(group) = self.rand_group.clone() else {
+            bail!("No group to run group version queries on.");
+        };
+
+        // Get group paused version
+        bench!(self, get_group_paused_version(&group.id))?;
 
         Ok(())
     }
