35ce76d7a1d60cf1764938090540de478cc15bf2
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2026-02-05 08:18:36 -0800

    Revert "Squashed 'sdks/android/' content from commit 6e9d4e56a"
    This reverts commit ec4e6136aed16b331fb890e6431bde15d8e2ce32.

--- library/src/main/java/org/xmtp/android/library/SignedData.kt
+++
@@ -1,8 +0,0 @@
-package org.xmtp.android.library
-
-data class SignedData(
-    val rawData: ByteArray,
-    val publicKey: ByteArray? = null, // Used for Passkeys
-    val authenticatorData: ByteArray? = null, // WebAuthn metadata
-    val clientDataJson: ByteArray? = null, // WebAuthn metadata
-)
