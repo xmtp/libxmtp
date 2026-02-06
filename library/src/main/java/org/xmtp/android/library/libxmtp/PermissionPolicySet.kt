47a1e5a4726a0ad40f46fa17e6f3fca94f5078f4
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2026-02-05 17:30:14 -0800

    Squashed 'sdks/android/' content from commit 6e9d4e56a
    git-subtree-dir: sdks/android
    git-subtree-split: 6e9d4e56a2e80c2b6df00fced8e87af6405be661

---
+++ library/src/main/java/org/xmtp/android/library/libxmtp/PermissionPolicySet.kt
@@ -0,0 +1,122 @@
+package org.xmtp.android.library.libxmtp
+
+import uniffi.xmtpv3.FfiGroupPermissionsOptions
+import uniffi.xmtpv3.FfiPermissionPolicy
+import uniffi.xmtpv3.FfiPermissionPolicySet
+
+enum class PermissionOption {
+    Allow,
+    Deny,
+    Admin,
+    SuperAdmin,
+    Unknown,
+    ;
+
+    companion object {
+        fun toFfiPermissionPolicy(option: PermissionOption): FfiPermissionPolicy =
+            when (option) {
+                Allow -> FfiPermissionPolicy.ALLOW
+                Deny -> FfiPermissionPolicy.DENY
+                Admin -> FfiPermissionPolicy.ADMIN
+                SuperAdmin -> FfiPermissionPolicy.SUPER_ADMIN
+                Unknown -> FfiPermissionPolicy.OTHER
+            }
+
+        fun fromFfiPermissionPolicy(ffiPolicy: FfiPermissionPolicy): PermissionOption =
+            when (ffiPolicy) {
+                FfiPermissionPolicy.ALLOW -> Allow
+                FfiPermissionPolicy.DENY -> Deny
+                FfiPermissionPolicy.ADMIN -> Admin
+                FfiPermissionPolicy.SUPER_ADMIN -> SuperAdmin
+                FfiPermissionPolicy.DOES_NOT_EXIST -> Unknown
+                FfiPermissionPolicy.OTHER -> Unknown
+            }
+    }
+}
+
+enum class GroupPermissionPreconfiguration {
+    ALL_MEMBERS,
+    ADMIN_ONLY,
+    ;
+
+    companion object {
+        fun toFfiGroupPermissionOptions(option: GroupPermissionPreconfiguration): FfiGroupPermissionsOptions =
+            when (option) {
+                ALL_MEMBERS -> FfiGroupPermissionsOptions.DEFAULT
+                ADMIN_ONLY -> FfiGroupPermissionsOptions.ADMIN_ONLY
+            }
+    }
+}
+
+data class PermissionPolicySet(
+    val addMemberPolicy: PermissionOption,
+    val removeMemberPolicy: PermissionOption,
+    val addAdminPolicy: PermissionOption,
+    val removeAdminPolicy: PermissionOption,
+    val updateGroupNamePolicy: PermissionOption,
+    val updateGroupDescriptionPolicy: PermissionOption,
+    val updateGroupImagePolicy: PermissionOption,
+    val updateMessageDisappearingPolicy: PermissionOption,
+    val updateAppDataPolicy: PermissionOption,
+) {
+    companion object {
+        fun toFfiPermissionPolicySet(permissionPolicySet: PermissionPolicySet): FfiPermissionPolicySet =
+            FfiPermissionPolicySet(
+                addMemberPolicy = PermissionOption.toFfiPermissionPolicy(permissionPolicySet.addMemberPolicy),
+                removeMemberPolicy = PermissionOption.toFfiPermissionPolicy(permissionPolicySet.removeMemberPolicy),
+                addAdminPolicy = PermissionOption.toFfiPermissionPolicy(permissionPolicySet.addAdminPolicy),
+                removeAdminPolicy = PermissionOption.toFfiPermissionPolicy(permissionPolicySet.removeAdminPolicy),
+                updateGroupNamePolicy =
+                    PermissionOption.toFfiPermissionPolicy(
+                        permissionPolicySet.updateGroupNamePolicy,
+                    ),
+                updateGroupDescriptionPolicy =
+                    PermissionOption.toFfiPermissionPolicy(
+                        permissionPolicySet.updateGroupDescriptionPolicy,
+                    ),
+                updateGroupImageUrlSquarePolicy =
+                    PermissionOption.toFfiPermissionPolicy(
+                        permissionPolicySet.updateGroupImagePolicy,
+                    ),
+                updateMessageDisappearingPolicy =
+                    PermissionOption.toFfiPermissionPolicy(
+                        permissionPolicySet.updateMessageDisappearingPolicy,
+                    ),
+                updateAppDataPolicy =
+                    PermissionOption.toFfiPermissionPolicy(
+                        permissionPolicySet.updateAppDataPolicy,
+                    ),
+            )
+
+        fun fromFfiPermissionPolicySet(ffiPermissionPolicySet: FfiPermissionPolicySet): PermissionPolicySet =
+            PermissionPolicySet(
+                addMemberPolicy = PermissionOption.fromFfiPermissionPolicy(ffiPermissionPolicySet.addMemberPolicy),
+                removeMemberPolicy =
+                    PermissionOption.fromFfiPermissionPolicy(
+                        ffiPermissionPolicySet.removeMemberPolicy,
+                    ),
+                addAdminPolicy = PermissionOption.fromFfiPermissionPolicy(ffiPermissionPolicySet.addAdminPolicy),
+                removeAdminPolicy = PermissionOption.fromFfiPermissionPolicy(ffiPermissionPolicySet.removeAdminPolicy),
+                updateGroupNamePolicy =
+                    PermissionOption.fromFfiPermissionPolicy(
+                        ffiPermissionPolicySet.updateGroupNamePolicy,
+                    ),
+                updateGroupDescriptionPolicy =
+                    PermissionOption.fromFfiPermissionPolicy(
+                        ffiPermissionPolicySet.updateGroupDescriptionPolicy,
+                    ),
+                updateGroupImagePolicy =
+                    PermissionOption.fromFfiPermissionPolicy(
+                        ffiPermissionPolicySet.updateGroupImageUrlSquarePolicy,
+                    ),
+                updateMessageDisappearingPolicy =
+                    PermissionOption.fromFfiPermissionPolicy(
+                        ffiPermissionPolicySet.updateMessageDisappearingPolicy,
+                    ),
+                updateAppDataPolicy =
+                    PermissionOption.fromFfiPermissionPolicy(
+                        ffiPermissionPolicySet.updateAppDataPolicy,
+                    ),
+            )
+    }
+}
