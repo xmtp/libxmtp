fe9bb668ffcdb558154ec742be671bebe7aabac6
Dakota Brink <git@kota.is>
2026-02-09 14:15:22 -0500

    no longer necessary

--- crates/xmtp_mls/src/groups/device_sync/worker.rs
+++ crates/xmtp_mls/src/groups/device_sync/worker.rs
@@ -123,10 +123,6 @@ where
     Context: XmtpSharedContext + 'static,
 {
     async fn run(&mut self) -> Result<(), DeviceSyncError> {
-        // Wait for the identity to be ready & verified before doing anything
-        while !self.client.context.identity().is_ready() {
-            xmtp_common::task::yield_now().await
-        }
         self.sync_init().await?;
         self.metrics.increment_metric(SyncMetric::Init);
 
