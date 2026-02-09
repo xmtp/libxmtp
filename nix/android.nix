d75316c8c15955c3d965c31117c35a931205f333
neekolas <iam+git@nicholasmolnar.com>
2026-02-09 23:30:43 +0000

    Android Builds (#3127)
    Add Nix-based build infrastructure for Android SDK:
    
    - Create nix/package/android.nix using cargo-ndk for cross-compilation
    - Create nix/lib/mobile-common.nix for shared iOS/Android config
    - Add android-libs package to flake.nix
    - Add sdks/android/dev/ scripts (bindings, build)
    - Update sdks/android/library/build.gradle with sourceSets
    - Update nix/android.nix to include SDK 35 and build-tools 35.0.0
    - Remove legacy dev/release-kotlin and sdks/android/script/
    
    ## Issues
    
    https://github.com/xmtp/libxmtp/issues/3084

--- nix/android.nix
+++ nix/android.nix
@@ -15,74 +15,51 @@
 , xmtp
 }:
 let
-  inherit (androidComposition) androidsdk;
+  # Shared Android environment configuration
+  androidEnv = import ./lib/android-env.nix { inherit lib androidenv; };
 
-  android = {
-    platforms = [ "33" "34" ];
-    platformTools = "35.0.2";
-    buildTools = [ "30.0.3" ];
-  };
+  # Use dev composition (includes emulator)
+  androidComposition = androidEnv.composeDevPackages;
+  androidPaths = androidEnv.mkAndroidPaths androidComposition;
 
-  androidTargets = [
-    "aarch64-linux-android"
-    "armv7-linux-androideabi"
-    "x86_64-linux-android"
-    "i686-linux-android"
-  ];
+  # Rust toolchain with Android cross-compilation targets
+  rust-android-toolchain = xmtp.mkToolchain androidEnv.androidTargets [ "clippy-preview" "rustfmt-preview" ];
 
-  # Pinned Rust Version
-  rust-android-toolchain = xmtp.mkToolchain androidTargets [ "clippy-preview" "rustfmt-preview" ];
-  sdkArgs = {
-    platformVersions = android.platforms;
-    platformToolsVersion = android.platformTools;
-    buildToolsVersions = android.buildTools;
-    emulatorVersion = "34.1.19";
-    systemImageTypes = [ "google_apis_playstore" "default" ];
-    abiVersions = [ "x86_64" ];
-    includeNDK = true;
-    includeEmulator = true;
-    includeSystemImages = true;
-  };
-
-  # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/android.section.md
-  androidHome = "${androidComposition.androidsdk}/libexec/android-sdk";
-  androidComposition = androidenv.composeAndroidPackages sdkArgs;
+  # Emulator for local testing
   androidEmulator = androidenv.emulateApp {
     name = "libxmtp-emulator-34";
     platformVersion = "34";
-    abiVersion = "x86_64"; # armeabi-v7a, mips, x86_64
+    abiVersion = "x86_64";
     systemImageType = "default";
   };
 in
 mkShell {
   meta.description = "Android Development environment for Android SDK and Emulator";
+
+  XMTP_DEV_SHELL = "android";
   OPENSSL_DIR = "${openssl.dev}";
-  ANDROID_HOME = androidHome;
-  NDK_HOME = "${androidComposition.androidsdk}/libexec/android-sdk/ndk/${builtins.head (lib.lists.reverseList (builtins.split "-" "${androidComposition.ndk-bundle}"))}";
-  ANDROID_SDK_ROOT = androidHome; # ANDROID_SDK_ROOT is deprecated, but some tools may still use it;
-  ANDROID_NDK_ROOT = "${androidHome}/ndk-bundle";
+  ANDROID_HOME = androidPaths.home;
+  ANDROID_SDK_ROOT = androidPaths.home;
+  ANDROID_NDK_HOME = androidPaths.ndkHome;
+  ANDROID_NDK_ROOT = androidPaths.ndkHome;
+  NDK_HOME = androidPaths.ndkHome;
   EMULATOR = "${androidEmulator}";
 
-  # Packages available to flake while building the environment
   nativeBuildInputs = [ pkg-config ];
 
   buildInputs = [
     rust-android-toolchain
     kotlin
     ktlint
-    androidsdk
+    androidComposition.androidsdk
     jdk17
     cargo-ndk
     androidEmulator
-    gnused # for ./dev/release-kotlin
+    gnused
     perl
-
-    # System Libraries
     sqlite
     openssl
   ] ++ lib.optionals stdenv.isDarwin [
-    # optional packages if on darwin, in order to check if build passes locally
     darwin.cctools
   ];
 }
-
