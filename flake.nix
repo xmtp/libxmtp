4f5c3eaa9dd19d907b81b5d62b9576c5d1e6fbd8
Cameron Voell <1103838+cameronvoell@users.noreply.github.com>
2026-02-11 14:49:28 -0800

    Improvements around toggling nix and direnv on/off (#3169)
    Co-authored-by: cameronvoell <cameronvoell@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -18,16 +18,6 @@
     };
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = [
-      "xmtp.cachix.org-1:nFPFrqLQ9kjYQKiWL7gKq6llcNEeaV4iI+Ka1F+Tmq0="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    extra-substituters = [
-      "https://xmtp.cachix.org"
-      "https://nix-community.cachix.org"
-    ];
-  };
 
   outputs =
     inputs @ { flake-parts, self, ... }:
