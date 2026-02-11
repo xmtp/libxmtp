30aaee00742d40bca4fb9f4e40cf5b0e63362510
Dakota Brink <git@kota.is>
2026-02-11 15:16:22 -0500

    switch to gix

--- apps/mls_validation_service/build.rs
+++ apps/mls_validation_service/build.rs
@@ -1,9 +1,9 @@
 use std::error::Error;
-use vergen_git2::{BuildBuilder, Emitter, Git2Builder};
+use vergen_gix::{BuildBuilder, Emitter, GixBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let build = BuildBuilder::all_build()?;
-    let git = Git2Builder::default().branch(true).sha(true).build()?;
+    let git = GixBuilder::default().branch(true).sha(true).build()?;
     Emitter::default()
         .add_instructions(&build)?
         .add_instructions(&git)?
