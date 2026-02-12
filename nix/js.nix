15604e9102a66f0293e0bff806d4ee9ab8fee3ff
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2026-02-12 15:17:41 -0800

    Add nix formatter

--- nix/js.nix
+++ nix/js.nix
@@ -1,15 +1,16 @@
-{ stdenv
-, darwin
-, mktemp
-, buf
-, curl
-, geckodriver
-, corepack
-, pkg-config
-, playwright-driver
-, playwright
-, lib
-, mkShell
+{
+  stdenv,
+  darwin,
+  mktemp,
+  buf,
+  curl,
+  geckodriver,
+  corepack,
+  pkg-config,
+  playwright-driver,
+  playwright,
+  lib,
+  mkShell,
 }:
 let
   inherit (darwin.apple_sdk) frameworks;
@@ -21,18 +22,17 @@ mkShell {
   PLAYWRIGHT_VERSION = "${playwright.version}";
   name = "xmtp-js environment";
   nativeBuildInputs = [ pkg-config ];
-  buildInputs =
-    [
-      mktemp
-      buf
-      curl
-      geckodriver
-      playwright
-      playwright-driver.browsers
-      corepack
-    ]
-    ++ lib.optionals stdenv.isDarwin [
-      darwin.cctools
-    ];
+  buildInputs = [
+    mktemp
+    buf
+    curl
+    geckodriver
+    playwright
+    playwright-driver.browsers
+    corepack
+  ]
+  ++ lib.optionals stdenv.isDarwin [
+    darwin.cctools
+  ];
   VITE_PROJECT_ID = "2ca676e2e5e9322c40c68f10dca637e5";
 }
--- nix/lib/mkToolchain.nix
+++ nix/lib/mkToolchain.nix
@@ -1,9 +1,17 @@
-{ inputs, lib, fenix, ... }:
+{
+  inputs,
+  lib,
+  fenix,
+  ...
+}:
 let
   toolchain = (fenix.fromManifestFile inputs.rust-manifest).defaultToolchain;
 in
-targets: components: fenix.combine [
+targets: components:
+fenix.combine [
   toolchain
-  (lib.forEach targets (target: (fenix.targets."${target}".fromManifestFile inputs.rust-manifest).rust-std))
+  (lib.forEach targets (
+    target: (fenix.targets."${target}".fromManifestFile inputs.rust-manifest).rust-std
+  ))
   (lib.forEach components (component: (fenix.fromManifestFile inputs.rust-manifest)."${component}"))
 ]
