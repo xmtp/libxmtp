97b6804fa5259eae8a9a1f07d25c814df5a8bb65
Andrew Plaza <github@andrewplaza.dev>
2026-02-13 08:58:08 -0500

    aarch64

--- nix/musl-docker.nix
+++ nix/musl-docker.nix
@@ -9,23 +9,32 @@ _: {
       ...
     }:
     let
+      muslPkgs = pkgs.pkgsCross.musl64;
       muslEnv = old: {
         CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
         CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        depsBuildBuild = (old.depsBuildBuild or [ ]) ++ [ muslPkgs.stdenv.cc ];
+        buildInputs = (old.buildInputs or [ ]) ++ [
+          muslPkgs.pkgsStatic.openssl.dev
+        ];
+        CC_x86_64_unknown_linux_musl = "${muslPkgs.stdenv.cc}/bin/${muslPkgs.stdenv.cc.targetPrefix}cc";
+        CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${muslPkgs.stdenv.cc}/bin/${muslPkgs.stdenv.cc.targetPrefix}cc";
+        OPENSSL_STATIC = "true";
+        OPENSSL_LIB_DIR = "${muslPkgs.pkgsStatic.openssl.out}/lib";
+        OPENSSL_INCLUDE_DIR = "${muslPkgs.pkgsStatic.openssl.dev}/include";
+        OPENSSL_NO_VENDOR = "1";
+        PKG_CONFIG_ALL_STATIC = "true";
+        PKG_CONFIG_PATH = "${muslPkgs.pkgsStatic.openssl.dev}/lib/pkgconfig";
       };
     in
     {
-      # musl-mls_validation_service builds just fine on darwin,
-      # it just takes forever b/c it compiles the musl gcc for aarch64 darwin from scratch
-      # (need to add pkgs.pkgsCross.musl64 versions of `buildInputs` on darwin, & point to right CC)
-      # restrict build to linux only to save some CI time
-      # can still build on darwin via Qemu
-      packages = lib.optionalAttrs pkgs.stdenv.isLinux {
-        musl-mls_validation_service =
+      packages = {
+
+        mls-validation-service-musl64 =
           config.rust-project.crates.mls_validation_service.crane.outputs.drv.crate.overrideAttrs
             (old: old // (muslEnv old));
         # a very small docker build with anvil pre-populated for use in CI
-        docker-mls_validation_service = pkgs.dockerTools.buildLayeredImage {
+        validation-service-image-musl64 = pkgs.dockerTools.buildLayeredImage {
           name = "ghcr.io/xmtp/mls-validation-service"; # override ghcr images
           tag = "main";
           created = "now";
@@ -33,7 +42,7 @@ _: {
             Env = [
               "ANVIL_URL=http://anvil:8545"
             ];
-            entrypoint = [ "${self'.packages.musl-mls_validation_service}/bin/mls-validation-service" ];
+            entrypoint = [ "${self'.packages.mls-validation-service-musl64}/bin/mls-validation-service" ];
           };
         };
       };
