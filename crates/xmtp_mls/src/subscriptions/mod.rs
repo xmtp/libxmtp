95dad9d4011b85bdcb0a14d1c4a218bcf6444218
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2026-09-23 12:29:17 -0700

    Remove obsolete message stream callback adapters (#4208)

--- crates/xmtp_mls/src/subscriptions/mod.rs
+++ crates/xmtp_mls/src/subscriptions/mod.rs
@@ -481,29 +481,6 @@ where
         StreamAllMessages::new_owned(self.context.clone(), conversation_type, consent_state).await
     }
 
-    pub fn stream_all_messages_with_callback(
-        context: Context,
-        conversation_type: Option<ConversationType>,
-        consent_state: Option<Vec<ConsentState>>,
-        callback: impl FnMut(Result<StoredGroupMessage>) + MaybeSend + 'static,
-        on_close: impl FnOnce() + MaybeSend + 'static,
-    ) -> impl StreamHandle<StreamOutput = Result<()>> {
-        let cancel = watchdog::StreamCancel::new(&context);
-        watchdog::spawn_watchdog_stream(
-            cancel,
-            "stream_all_messages",
-            move || {
-                let context = context.clone();
-                let consent_state = consent_state.clone();
-                async move {
-                    StreamAllMessages::new_owned(context, conversation_type, consent_state).await
-                }
-            },
-            callback,
-            on_close,
-        )
-    }
-
     pub fn stream_consent_with_callback(
         client: Arc<Client<Context>>,
         mut callback: impl FnMut(Result<Vec<StoredConsentRecord>>) + MaybeSend + 'static,
